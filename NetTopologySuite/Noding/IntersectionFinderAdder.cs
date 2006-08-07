65cd3598ec0d5f75285ec3c9ac9b8de370e972d3
DGuidi <diegoguidi@gmail.com>
2006-08-07 19:10:00 +0000

    git-svn-id: http://nettopologysuite.googlecode.com/svn/trunk@27 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

---
+++ NetTopologySuite/Noding/IntersectionFinderAdder.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Text;
+
+using GisSharpBlog.NetTopologySuite.Algorithm;
+using GisSharpBlog.NetTopologySuite.Geometries;
+
+namespace GisSharpBlog.NetTopologySuite.Noding
+{
+
+    /// <summary>
+    /// Finds proper and interior intersections in a set of <see cref="SegmentString" />s,
+    /// and adds them as nodes.
+    /// </summary>
+    public class IntersectionFinderAdder : ISegmentIntersector
+    {
+
+        private LineIntersector li = null;
+        private readonly IList interiorIntersections = null;
+
+        /// <summary>
+        /// Creates an intersection finder which finds all proper intersections.
+        /// </summary>
+        /// <param name="li">The <see cref="LineIntersector" /> to use.</param>
+        public IntersectionFinderAdder(LineIntersector li)
+        {
+            this.li = li;
+            interiorIntersections = new ArrayList();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public IList InteriorIntersections
+        {
+            get
+            {
+                return interiorIntersections;
+            }
+        }
+   
+        /// <summary>
+        /// This method is called by clients
+        /// of the <see cref="SegmentIntersector" /> class to process
+        /// intersections for two segments of the <see cref="SegmentString" />s being intersected.
+        /// Note that some clients (such as <see cref="MonotoneChain" />s) may optimize away
+        /// this call for segment pairs which they have determined do not intersect
+        /// (e.g. by an disjoint envelope test).
+        /// </summary>
+        /// <param name="e0"></param>
+        /// <param name="segIndex0"></param>
+        /// <param name="e1"></param>
+        /// <param name="segIndex1"></param>
+        public void ProcessIntersections(SegmentString e0, int segIndex0, SegmentString e1, int segIndex1 )
+        {
+            // don't bother intersecting a segment with itself
+            if (e0 == e1 && segIndex0 == segIndex1) 
+                return;
+
+            Coordinate p00 = e0.Coordinates[segIndex0];
+            Coordinate p01 = e0.Coordinates[segIndex0 + 1];
+            Coordinate p10 = e1.Coordinates[segIndex1];
+            Coordinate p11 = e1.Coordinates[segIndex1 + 1];
+
+            li.ComputeIntersection(p00, p01, p10, p11);            
+
+            if (li.HasIntersection)
+            {
+                if (li.IsInteriorIntersection())
+                {
+                    for(int intIndex = 0; intIndex < li.IntersectionNum; intIndex++)
+                        interiorIntersections.Add(li.GetIntersection(intIndex));
+                    
+                    e0.AddIntersections(li, segIndex0, 0);
+                    e1.AddIntersections(li, segIndex1, 1);
+                }
+            }
+        }
+
+    }
+}
---
+++ NetTopologySuite/Noding/IteratedNoder.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections;
+using System.Text;
+
+using GisSharpBlog.NetTopologySuite.Geometries;
+using GisSharpBlog.NetTopologySuite.Algorithm;
+
+namespace GisSharpBlog.NetTopologySuite.Noding
+{
+
+    /// <summary>
+    /// Nodes a set of <see cref="SegmentString" />s completely.
+    /// The set of <see cref="SegmentString" />s is fully noded;
+    /// i.e. noding is repeated until no further intersections are detected.
+    /// <para>
+    /// Iterated noding using a <see cref="PrecisionModels.Floating" /> precision model is not guaranteed to converge,
+    /// due to roundoff error. This problem is detected and an exception is thrown.
+    /// Clients can choose to rerun the noding using a lower precision model.
+    /// </para>
+    /// </summary>
+    public class IteratedNoder : Noder
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const int MaxIterations = 5;
+
+        private LineIntersector li = null;
+        private IList nodedSegStrings = null;
+        private int maxIter = MaxIterations;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IteratedNoder"/> class.
+        /// </summary>
+        /// <param name="pm"></param>
+        public IteratedNoder(PrecisionModel pm)
+        {
+            li = new RobustLineIntersector();
+            li.PrecisionModel = pm;
+        }
+
+        /// <summary>
+        /// Gets/Sets the maximum number of noding iterations performed before
+        /// the noding is aborted. Experience suggests that this should rarely need to be changed
+        /// from the default. The default is <see cref="MaxIterations" />.
+        /// </summary>
+        public int MaximumIterations
+        {
+            get
+            {
+                return maxIter;
+            }
+            set
+            {
+                maxIter = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns a <see cref="IList"/> of fully noded <see cref="SegmentString"/>s.
+        /// The <see cref="SegmentString"/>s have the same context as their parent.
+        /// </summary>
+        /// <returns></returns>
+        public IList GetNodedSubstrings() 
+        { 
+            return nodedSegStrings; 
+        }
+
+        /// <summary>
+        /// Fully nodes a list of <see cref="SegmentString" />s, i.e. peforms noding iteratively
+        /// until no intersections are found between segments.
+        /// Maintains labelling of edges correctly through the noding.
+        /// </summary>
+        /// <param name="segStrings">A collection of SegmentStrings to be noded.</param>
+        /// <exception cref="TopologyException">If the iterated noding fails to converge.</exception>
+        public void ComputeNodes(IList segStrings)    
+        {
+            int[] numInteriorIntersections = new int[1];
+            nodedSegStrings = segStrings;
+            int nodingIterationCount = 0;
+            int lastNodesCreated = -1;
+            do 
+            {
+              Node(nodedSegStrings, numInteriorIntersections);
+              nodingIterationCount++;
+              int nodesCreated = numInteriorIntersections[0];
+
+              /**
+               * Fail if the number of nodes created is not declining.
+               * However, allow a few iterations at least before doing this
+               */       
+              if (lastNodesCreated > 0
+                  && nodesCreated >= lastNodesCreated
+                  && nodingIterationCount > maxIter) 
+                throw new TopologyException("Iterated noding failed to converge after "
+                                            + nodingIterationCount + " iterations");              
+              lastNodesCreated = nodesCreated;
+
+            } 
+            while (lastNodesCreated > 0);        
+        }
+
+        /// <summary>
+        /// Node the input segment strings once
+        /// and create the split edges between the nodes.
+        /// </summary>
+        /// <param name="segStrings"></param>
+        /// <param name="numInteriorIntersections"></param>
+        private void Node(IList segStrings, int[] numInteriorIntersections)
+        {
+            IntersectionAdder si = new IntersectionAdder(li);
+            MCIndexNoder noder = new MCIndexNoder(si);            
+            noder.ComputeNodes(segStrings);
+            nodedSegStrings = noder.GetNodedSubstrings();
+            numInteriorIntersections[0] = si.NumInteriorIntersections;            
+        }
+
+    }
+}
