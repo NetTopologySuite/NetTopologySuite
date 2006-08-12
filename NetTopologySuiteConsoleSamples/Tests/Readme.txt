313bec6b99cd110199082c363edafc5d5ca20a3a
DGuidi <diegoguidi@gmail.com>
2006-08-12 16:35:35 +0000

    git-svn-id: http://nettopologysuite.googlecode.com/svn/trunk@57 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

--- NetTopologySuiteConsoleSamples/Tests/Readme.txt
+++ NetTopologySuiteConsoleSamples/Tests/Readme.txt
@@ -1,3 +1,4 @@
 Run this tests with TestDriven.NET plugin for VS2005!
 Simply install the plugin @ http://www.testdriven.net/,
 then click with the mouse right-button and ... launch test!
+Could be more easier than this? :)
--- NetTopologySuiteConsoleSamples/Tests/Various/OGCTests.cs
+++ NetTopologySuiteConsoleSamples/Tests/Various/OGCTests.cs
@@ -19,11 +19,17 @@ namespace GisSharpBlog.NetTopologySuite.Samples.Tests.Various
     [TestFixture]
     public class OGCTests : BaseSamples
     {
-
+        private Geometry blueLake = null;
+        private Geometry ashton = null;
+ 
         /// <summary>
         /// 
         /// </summary>
-        public OGCTests() : base(GeometryFactory.Fixed) { }
+        public OGCTests() : base(GeometryFactory.Fixed) 
+        {
+            blueLake = Reader.Read("POLYGON((52 18,66 23,73 9,48 6,52 18),(59 18,67 18,67 13,59 13,59 18))");
+            ashton = Reader.Read("POLYGON(( 62 48, 84 48, 84 30, 56 30, 56 34, 62 48))");
+        }
 
 
         /// <summary>
@@ -32,14 +38,28 @@ namespace GisSharpBlog.NetTopologySuite.Samples.Tests.Various
         [Test]
         public void OGCUnionTest()
         {                        
-            Geometry blueLake = Reader.Read("POLYGON((52 18,66 23,73 9,48 6,52 18),(59 18,67 18,67 13,59 13,59 18))");
-            Assert.IsNotNull(blueLake);
-
-            Geometry ashton = Reader.Read("POLYGON(( 62 48, 84 48, 84 30, 56 30, 56 34, 62 48))");
+            Assert.IsNotNull(blueLake);            
             Assert.IsNotNull(ashton);
 
             Geometry expected = Reader.Read("POLYGON((52 18,66 23,73 9,48 6,52 18))");
             Geometry result = blueLake.Union(ashton);
+
+            Debug.WriteLine(result);
+            Assert.IsTrue(result.EqualsExact(expected));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public void OGCSymDifferenceTest()
+        {
+            Assert.IsNotNull(blueLake);
+            Assert.IsNotNull(ashton);
+
+            Geometry expected = Reader.Read("POLYGON((52 18,66 23,73 9,48 6,52 18))");
+            Geometry result = blueLake.SymmetricDifference(ashton);
+
             Debug.WriteLine(result);
             Assert.IsTrue(result.EqualsExact(expected));
         }
