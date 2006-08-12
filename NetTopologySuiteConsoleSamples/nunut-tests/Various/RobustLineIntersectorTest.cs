562d208e65515a7ddb169e9252a306879d0c2816
DGuidi <diegoguidi@gmail.com>
2006-08-12 15:20:05 +0000

    git-svn-id: http://nettopologysuite.googlecode.com/svn/trunk@48 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

---
+++ NetTopologySuiteConsoleSamples/nunut-tests/Various/RobustLineIntersectorTest.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+
+using GisSharpBlog.NetTopologySuite.Geometries;
+using GisSharpBlog.NetTopologySuite.IO;
+
+using GisSharpBlog.NetTopologySuite.Samples.SimpleTests;
+
+using NUnit.Framework;
+
+namespace GisSharpBlog.NetTopologySuite.Samples.nunut_tests
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [TestFixture]
+    public class RobustLineIntersectorTest : BaseSamples
+    {
+        
+        private const string lineString1 = "LINESTRING (2089426.5233462777 1180182.3877339689, 2085646.6891757075 1195618.7333999649)";
+        private const string lineString2 = "LINESTRING (1889281.8148903656 1997547.0560044837, 2259977.3672235999 483675.17050843034)";
+        private const string point1 = "POINT (2097408.2633752143,1144595.8008114607)";
+
+        private WKTReader reader = null;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RobustLineIntersectorTest"/> class.
+        /// </summary>
+        public RobustLineIntersectorTest() : base()
+        {
+            reader = new WKTReader(Factory);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public void IntersectionTest()
+        {
+            Geometry g1 = reader.Read(lineString1);
+            Geometry g2 = reader.Read(lineString2);
+            Geometry p1 = reader.Read(point1);
+
+            Geometry result = g1.Intersection(g2);            
+            
+            Debug.WriteLine(result);
+            Assert.IsNotNull(result);            
+        }
+
+    }
+}
