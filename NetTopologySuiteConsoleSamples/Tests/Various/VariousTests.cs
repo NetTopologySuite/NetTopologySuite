58405a94e8b0e79e6b8f4f826c253e85d48992ee
DGuidi <diegoguidi@gmail.com>
2006-08-12 16:02:02 +0000

    git-svn-id: http://nettopologysuite.googlecode.com/svn/trunk@54 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

---
+++ NetTopologySuiteConsoleSamples/Tests/Various/VariousTests.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+
+using GisSharpBlog.NetTopologySuite.Geometries;
+using GisSharpBlog.NetTopologySuite.IO;
+
+using GisSharpBlog.NetTopologySuite.Samples.SimpleTests;
+
+using NUnit.Framework;
+
+namespace GisSharpBlog.NetTopologySuite.Samples.Tests.Various
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [TestFixture]
+    public class VariousTests : BaseSamples
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public VariousTests() : base(GeometryFactory.Fixed) { }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public void Test1()
+        {                        
+            Geometry blueLake = Reader.Read("POLYGON((52 18,66 23,73 9,48 6,52 18),(59 18,67 18,67 13,59 13,59 18))");
+            Assert.IsNotNull(blueLake);
+
+            Geometry ashton = Reader.Read("POLYGON(( 62 48, 84 48, 84 30, 56 30, 56 34, 62 48))");
+            Assert.IsNotNull(ashton);
+
+            Geometry expected = Reader.Read("POLYGON((52 18,66 23,73 9,48 6,52 18))");
+            Geometry result = blueLake.Union(ashton);
+            Debug.WriteLine(result);
+            Assert.IsTrue(result.EqualsExact(expected));
+        }
+
+    }
+}
