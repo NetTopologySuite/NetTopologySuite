3dce2b4cba993a3fa4a3b1a3d2e0f933eaa4fba8
DGuidi <diegoguidi@gmail.com>
2007-05-29 08:51:27 +0000

    IGeometryFactory created
    git-svn-id: http://nettopologysuite.googlecode.com/svn/trunk@158 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

---
+++ NetTopologySuite/Geometries/IGeometryFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+using GeoAPI.Geometries;
+
+namespace GisSharpBlog.NetTopologySuite.Geometries
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    interface IGeometryFactory
+    {
+        ICoordinateSequenceFactory CoordinateSequenceFactory { get; }
+
+        int SRID { get; }
+        PrecisionModel PrecisionModel { get; }
+                
+        IGeometry BuildGeometry(ICollection geomList);
+        IGeometry CreateGeometry(IGeometry g);
+        
+        Point CreatePoint(ICoordinate coordinate);
+        Point CreatePoint(ICoordinateSequence coordinates);        
+
+        ILineString CreateLineString(ICoordinate[] coordinates);
+        ILineString CreateLineString(ICoordinateSequence coordinates);
+
+        ILinearRing CreateLinearRing(ICoordinate[] coordinates);
+        ILinearRing CreateLinearRing(ICoordinateSequence coordinates);
+
+        IPolygon CreatePolygon(ILinearRing shell, ILinearRing[] holes);
+
+        IMultiPoint CreateMultiPoint(ICoordinate[] coordinates);
+        IMultiPoint CreateMultiPoint(IPoint[] point);
+        IMultiPoint CreateMultiPoint(ICoordinateSequence coordinates);
+
+        IMultiLineString CreateMultiLineString(ILineString[] lineStrings);
+        
+        IMultiPolygon CreateMultiPolygon(IPolygon[] polygons);
+        
+        IGeometryCollection CreateGeometryCollection(IGeometry[] geometries);
+    }
+}
