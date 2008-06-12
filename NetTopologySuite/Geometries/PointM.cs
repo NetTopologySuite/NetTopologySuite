71237496eb6a550b6e07a25f03b8b03a885aadcc
codekaizen <codekaizen@gmail.com>
2008-06-12 20:20:36 +0000

    Adding PointM
    git-svn-id: http://nettopologysuite.googlecode.com/svn/branches/v2.0@321 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

---
+++ NetTopologySuite/Geometries/PointM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using GeoAPI.Coordinates;
+using GeoAPI.Geometries;
+using NPack.Interfaces;
+
+namespace GisSharpBlog.NetTopologySuite.Geometries
+{
+    public class PointM<TCoordinate> : Point<TCoordinate>, IPoint2DM, IPoint3DM
+        where TCoordinate : ICoordinate<TCoordinate>, IEquatable<TCoordinate>,
+                            IComparable<TCoordinate>, IConvertible,
+                            IComputable<Double, TCoordinate>
+    {
+        private readonly Double _measure = Double.NaN;
+
+        /// <summary>
+        /// Constructs a <see cref="Point{TCoordinate}"/> with the given coordinate.
+        /// </summary>
+        /// <param name="coordinate">
+        /// Contains the single coordinate on which to base this <see cref="Point{TCoordinate}"/>,
+        /// or <see langword="null" /> to create the empty point.
+        /// </param>
+        public PointM(TCoordinate coordinate, Double measure, IGeometryFactory<TCoordinate> factory)
+            : base(coordinate, factory)
+        {
+            _measure = measure;
+        }
+
+        #region IPoint2DM Members
+
+        public Double M
+        {
+            get { return _measure; }
+        }
+
+        #endregion
+    }
+}
