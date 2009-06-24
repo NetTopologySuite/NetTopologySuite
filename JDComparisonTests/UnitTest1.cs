030f3a794fb10ba47e0e073507236a50a255f832
john.diss <john.diss@newgrove.com>
2009-06-24 15:30:37 +0000

    v2: fix to SegmentNode invalid comparison of coord with NodedSegment yielded invalid IsInterior value SegmentNodeList change to ensure previousNode was updated also change to createSplitEdge where previously Slice was incorrectly used rather than ICoordinateSequence.Append / Prepend Segment string Count modifed - removed -1 which was giving different count to 1.7.x
    Buffer appears to be fixed
    
    git-svn-id: http://nettopologysuite.googlecode.com/svn/trunk@442 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

---
+++ JDComparisonTests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using GeoAPI.Geometries;
+using GisSharpBlog.NetTopologySuite.IO;
+using GisSharpBlog.NetTopologySuite.Operation.Buffer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SharpMap.Data.Providers.PostGis.Tests
+{
+
+    [TestClass]
+    public class UnrelatedTests
+    {
+
+        public TestContext TestContext { get; set; }
+
+        [ClassInitialize]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+        }
+
+
+        public static void MyClassCleanup()
+        {
+        }
+
+
+
+        [TestMethod]
+        public void Test_T97Buffer()
+        {
+            ILineString ls = new WKTReader().Read("LINESTRING(0 0, 100 100)") as ILineString;
+            Assert.IsNotNull(ls);
+
+            BufferBuilder bb =
+                new BufferBuilder();
+            IGeometry res = bb.Buffer(ls, 10);
+            Assert.IsNotNull(res);
+        }
+    }
+}
