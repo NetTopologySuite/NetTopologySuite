1cdcbe605cb82b3c36077f97ce2aa960d39b32fe
FObermaier <felix.obermaier@netcologne.de>
2010-11-25 12:56:52 +0000

    [branches/v2.11] - Added VS2010 solution file, hence the *.csproj changes
    git-svn-id: http://nettopologysuite.googlecode.com/svn/branches/v2.11@534 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

--- NetTopologySuite/Precision2/CommonBitsRemover.cs
+++ NetTopologySuite/Precision2/CommonBitsRemover.cs
@@ -140,10 +140,10 @@ namespace GisSharpBlog.NetTopologySuite.Precision
                     return _geomFact.CreatePoint(AddTrans(geom.Coordinates[0]));
 
                 if ( geom is IMultiPoint<TCoordinate>)
-                    return _geomFact.CreateMultiPoint(Processor.Transform(geom.Coordinates, AddTrans));
+                    return _geomFact.CreateMultiPoint(Processor.Transform<TCoordinate>(geom.Coordinates, AddTrans));
 
                 if ( geom is ILineString<TCoordinate> )
-                    return _geomFact.CreateLineString(Processor.Transform(geom.Coordinates, AddTrans));
+                    return _geomFact.CreateLineString(Processor.Transform<TCoordinate>(geom.Coordinates, AddTrans));
 
                 if (geom is IMultiLineString<TCoordinate>)
                 {
@@ -159,7 +159,7 @@ namespace GisSharpBlog.NetTopologySuite.Precision
                     IPolygon<TCoordinate> poly =
                         _geomFact.CreatePolygon(
                             _geomFact.CreateLinearRing(
-                                Processor.Transform(polyInput.ExteriorRing.Coordinates, AddTrans)),
+                                Processor.Transform<TCoordinate>(polyInput.ExteriorRing.Coordinates, AddTrans)),
                                 Processor.Transform<ILineString<TCoordinate>, ILinearRing<TCoordinate>>(polyInput.InteriorRings, AddTrans));
 
                     return poly;
@@ -191,7 +191,7 @@ namespace GisSharpBlog.NetTopologySuite.Precision
 
             private ILinearRing<TCoordinate> AddTrans(ILineString<TCoordinate> input)
             {
-                return _geomFact.CreateLinearRing(Processor.Transform(input.Coordinates, AddTrans));
+                return _geomFact.CreateLinearRing(Processor.Transform<TCoordinate>(input.Coordinates, AddTrans));
             }
 
             private class AddDeltaOperation : GeometryEditor<TCoordinate>.CoordinateOperation
