25120ca5bfd8e3b9393f9b1adb3bcaa60386b5ec
john.diss <john.diss@newgrove.com>
2011-03-25 10:33:57 +0000

    trunk: adding missing file to NetTopologySuite.Silverlight
    git-svn-id: http://nettopologysuite.googlecode.com/svn/trunk@547 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

---
+++ NetTopologySuite.Silverlight/IO/GeoTools/StringToGeometryConverter.cs
@@ -0,0 +1,25 @@
+﻿using GeoAPI.Geometries;
+using GisSharpBlog.NetTopologySuite.Data;
+using GisSharpBlog.NetTopologySuite.Geometries;
+
+namespace GisSharpBlog.NetTopologySuite.IO.GeoTools
+{
+    public class StringToGeometryConverter : CustomConverterBase<string, IGeometry>
+    {
+        private readonly WKTReader _reader;
+
+        public StringToGeometryConverter() : this(new GeometryFactory())
+        {
+        }
+
+        public StringToGeometryConverter(IGeometryFactory geometryFactory)
+        {
+            _reader = new WKTReader(geometryFactory);
+        }
+
+        public override IGeometry Convert(string source)
+        {
+            return _reader.Read(source);
+        }
+    }
+}
