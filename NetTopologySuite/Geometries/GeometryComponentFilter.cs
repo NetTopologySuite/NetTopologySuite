647ff6173fa160f376cf202ce1970de30fbb9e57
FObermaier <felix.obermaier@netcologne.de>
2011-10-19 07:53:18 +0000

    [branches/v1.12] - added missing file Issue: 89
    git-svn-id: http://nettopologysuite.googlecode.com/svn/branches/v1.12@756 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

---
+++ NetTopologySuite/Geometries/GeometryComponentFilter.cs
@@ -0,0 +1,33 @@
+﻿using GeoAPI.Geometries;
+
+namespace NetTopologySuite.Geometries
+{
+    /// <summary>
+    /// Delegate function declaration to handle filter operation
+    /// </summary>
+    /// <param name="geom">The geometry to filter</param>
+    public delegate void FilterMethod(IGeometry geom);
+
+    /// <summary>
+    /// An <see cref="IGeometryComponentFilter"/> implementation that applies filtering with the provided <see cref="FilterMethod"/>
+    /// </summary>
+    public class GeometryComponentFilter : IGeometryComponentFilter
+    {
+        private readonly FilterMethod _do;
+
+        /// <summary>
+        /// Creates an instance of this class
+        /// </summary>
+        /// <param name="filterMethod">The filter method to be used</param>
+        public GeometryComponentFilter(FilterMethod filterMethod)
+        {
+            //Assert.IsTrue(filterMethod != null);
+            _do = filterMethod;
+        }
+
+        public void Filter(IGeometry geom)
+        {
+            _do(geom);
+        }
+    }
+}
