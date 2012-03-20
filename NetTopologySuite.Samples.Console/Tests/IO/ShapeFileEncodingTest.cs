c30abdf96d8d6c5f3f69da9d76e37f35a5d331dc
DGuidi <diegoguidi@gmail.com>
2012-03-20 09:55:13 +0000

    fixed previous commit
    git-svn-id: http://nettopologysuite.googlecode.com/svn/trunk@826 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

---
+++ NetTopologySuite.Samples.Console/Tests/IO/ShapeFileEncodingTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using NetTopologySuite.Features;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+using NUnit.Framework;
+
+namespace NetTopologySuite.Tests.IO
+{
+    [TestFixture]
+    public class ShapeFileEncodingTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            ShapefileDataWriter sfdr = new ShapefileDataWriter("encoding_sample");
+            DbaseFileHeader h = new DbaseFileHeader();
+            h.AddColumn("id", 'n', 8, 0);
+            h.AddColumn("Test", 'C', 15, 0);
+            h.AddColumn("Ålder", 'N', 8, 0);
+            h.AddColumn("Ödestext", 'C', 255, 0);
+            h.NumRecords = 1;
+            sfdr.Header = h;
+
+            List<Feature> feats = new List<Feature>();
+            AttributesTable at = new AttributesTable();
+            at.AddAttribute("id", "0");
+            at.AddAttribute("Test", "Testar");
+            at.AddAttribute("Ålder", 10);
+            at.AddAttribute("Ödestext", "Lång text med åäö etc");
+            feats.Add(new Feature(new Point(0, 0), at));
+            sfdr.Write(feats);      
+        }
+
+        [Test]
+        public void TestLoadShapeFileWithEncoding()
+        {
+            ShapefileDataReader reader = new ShapefileDataReader("encoding_sample.shp", GeometryFactory.Default);
+            DbaseFileHeader header = reader.DbaseHeader;
+            Assert.AreEqual(header.Encoding.WindowsCodePage, 1252, "Invalid encoding!");
+
+            Assert.AreEqual(header.Fields[1].Name, "Test");
+            Assert.AreEqual(header.Fields[2].Name, "Ålder");
+            Assert.AreEqual(header.Fields[3].Name, "Ödestext");
+
+            Assert.IsTrue(reader.Read(), "Error reading file");
+            Assert.AreEqual(reader["Test"], "Testar");
+            Assert.AreEqual(reader["Ödestext"], "Lång text med åäö etc");
+        }
+    }
+}
