1d99d6d26c6715611d7c1527b35d17f3e1e48632
FObermaier <felix.obermaier@netcologne.de>
2012-05-08 15:32:22 +0000

    [trunk] - Adding PostGis2 Reader and Writer class This is untested and not at all ready for use!
    git-svn-id: http://nettopologysuite.googlecode.com/svn/trunk@836 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

---
+++ NetTopologySuite.IO/NetTopologySuite.IO.PostGis2/PostGis2GeometryHeader.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using GeoAPI;
+using GeoAPI.Geometries;
+
+namespace NetTopologySuite.IO
+{
+    public class PostGis2GeometryHeader
+    {
+        /// <summary>
+        /// Three unused bits, followed by ReadOnly, Geodetic, HasBBox, HasM and HasZ flags.
+        /// [---RGBMZ]
+        /// </summary>
+        [Flags]
+        private enum PostGis2Flags : uint
+        {
+            HasZ = 1 << 24,
+            HasM = 1 << 25,
+            HasZM = HasZ | HasM,
+            HasBoundingBox = 1 << 26,
+            IsGeodetic = 1 << 27,
+            IsReadonly = 1 << 28
+        }
+///**
+//* Macros for manipulating the 'flags' byte. A uint8_t used as follows: 
+//* ---RGBMZ
+//* Three unused bits, followed by ReadOnly, Geodetic, HasBBox, HasM and HasZ flags.
+//*/
+//#define FLAGS_GET_Z(flags) ((flags) & 0x01)
+//#define FLAGS_GET_M(flags) (((flags) & 0x02)>>1)
+//#define FLAGS_GET_BBOX(flags) (((flags) & 0x04)>>2)
+//#define FLAGS_GET_GEODETIC(flags) (((flags) & 0x08)>>3)
+//#define FLAGS_GET_READONLY(flags) (((flags) & 0x10)>>4)
+//#define FLAGS_GET_SOLID(flags) (((flags) & 0x20)>>5)
+//#define FLAGS_SET_Z(flags, value) ((flags) = (value) ? ((flags) | 0x01) : ((flags) & 0xFE))
+//#define FLAGS_SET_M(flags, value) ((flags) = (value) ? ((flags) | 0x02) : ((flags) & 0xFD))
+//#define FLAGS_SET_BBOX(flags, value) ((flags) = (value) ? ((flags) | 0x04) : ((flags) & 0xFB))
+//#define FLAGS_SET_GEODETIC(flags, value) ((flags) = (value) ? ((flags) | 0x08) : ((flags) & 0xF7))
+//#define FLAGS_SET_READONLY(flags, value) ((flags) = (value) ? ((flags) | 0x10) : ((flags) & 0xEF))
+//#define FLAGS_SET_SOLID(flags, value) ((flags) = (value) ? ((flags) | 0x20) : ((flags) & 0xDF))
+//#define FLAGS_NDIMS(flags) (2 + FLAGS_GET_Z(flags) + FLAGS_GET_M(flags))
+//#define FLAGS_GET_ZM(flags) (FLAGS_GET_M(flags) + FLAGS_GET_Z(flags) * 2)
+//#define FLAGS_NDIMS_BOX(flags) (FLAGS_GET_GEODETIC(flags) ? 3 : FLAGS_NDIMS(flags))
+
+        
+        #region fields
+
+        public PostGis2GeometryHeader(BinaryReader reader)
+{
+    _size = reader.ReadUInt32();
+    _sridFlags = reader.ReadUInt32();
+
+    if (!HasBoundingBox) return;
+    _envelope = new Envelope(reader.ReadDouble(), reader.ReadDouble(),
+                             reader.ReadDouble(), reader.ReadDouble());
+    if (HasZ || IsGeodetic)
+        _zInterval = new[] {reader.ReadDouble(), reader.ReadDouble()};
+
+    if (HasM)
+        _mInterval = new[] {reader.ReadDouble(), reader.ReadDouble()};
+}
+
+        public PostGis2GeometryHeader(IGeometry geometry)
+            : this(geometry, Ordinates.XY)
+        { }
+
+        public PostGis2GeometryHeader(IGeometry geometry, Ordinates handleOrdinates)
+            : this(geometry, handleOrdinates, false)
+        {}
+
+        public PostGis2GeometryHeader(IGeometry geometry, Ordinates handleOrdinates, bool isGeodetic)
+        {
+            Srid = geometry.SRID;
+
+            HasM = (handleOrdinates & Ordinates.Z) == Ordinates.Z; // geometry.HasM();
+            HasZ = (handleOrdinates & Ordinates.M) == Ordinates.M; // geometry.HasZ();
+
+            if (geometry.OgcGeometryType != OgcGeometryType.Point)
+            {
+                HasBoundingBox = true;
+                _envelope = geometry.EnvelopeInternal;
+                if (HasM)
+                    _mInterval = geometry.GetMRange();
+                if (HasZ | isGeodetic)
+                    _zInterval = geometry.GetZRange();
+            }
+
+            ComputeSize(geometry);
+            _factory = geometry.Factory;
+        }
+
+        private uint ComputeSize(IGeometry geometry)
+        {
+            // We start with header size
+            uint geometrySize = 8;
+
+            if (HasBoundingBox)
+                geometrySize += 2*Dimension*8;
+            
+            switch (geometry.OgcGeometryType)
+            {
+                case OgcGeometryType.Point:
+                    geometrySize += 4 + 4 + Dimension*8;
+                    break;
+                case OgcGeometryType.LineString:
+                case OgcGeometryType.CircularString:
+                    geometrySize += 4 + 4 + Dimension * 8 * (uint)geometry.NumPoints;
+                    break;
+                case OgcGeometryType.Polygon:
+                    geometrySize += 4 + 4 + ComputePolygonSize((IPolygon) geometry);
+                    break;
+                default:
+                    for (var i = 0; i < geometry.NumGeometries; i++)
+                        geometrySize += ComputeSize(geometry.GetGeometryN(i));
+                    break;
+            }
+            return geometrySize;
+        }
+
+        private uint ComputePolygonSize(IPolygon polygon)
+        {
+            if (polygon.NumPoints == 0)
+                return 0;
+
+            var rings = 1;
+            var points = polygon.Shell.NumPoints;
+
+            for (var i = 0; i < polygon.NumInteriorRings; i++)
+            {
+                rings += 1;
+                points += polygon.GetInteriorRingN(i).NumPoints;
+            }
+
+            if ((rings % 2) != 0) rings++;
+            return (uint)(4*rings + points*Dimension*8);
+        }
+
+        private readonly uint _size;
+        private uint _sridFlags;
+
+        private Envelope _envelope;
+        private double[] _zInterval;
+        private double[] _mInterval;
+
+        private IGeometryFactory _factory;
+        
+        public Ordinates Ordinates
+        {
+            get
+            {
+                var res = Ordinates.XY;
+                if (HasZ) res |= Ordinates.Z;
+                if (HasM) res |= Ordinates.M;
+                
+                return res;
+            }
+        }
+
+        public Ordinate[] OrdinateIndices
+        {
+            get 
+            { 
+                var lst = new List<Ordinate>(new[] {Ordinate.X, Ordinate.Y,});
+                
+                if (HasZ) lst.Add(Ordinate.Z);
+                if (HasM) lst.Add(Ordinate.M);
+
+                return lst.ToArray();
+            }
+        }
+        
+        #endregion
+        
+        public uint Dimension
+        {
+            get
+            {
+                return IsGeodetic
+                           ? 3
+                           : (uint)(2 + (HasZ ? 1 : 0) + (HasM ? 1 : 0));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the spatial reference id
+        /// </summary>
+        public int Srid
+        {
+            get { return (int) (_sridFlags & 0xffffff); }
+            set
+            {
+                if (value != Srid)
+                    _sridFlags = (uint) value | (uint)Flags; 
+            }
+        }
+
+        private PostGis2Flags Flags { get { return (PostGis2Flags)(_sridFlags & 0xff000000); } }
+
+        public void Lock()
+        {
+            _sridFlags = _sridFlags | (uint) PostGis2Flags.IsReadonly;
+        }
+
+        public void UnLock()
+        {
+            _sridFlags = _sridFlags & ~(uint)PostGis2Flags.IsReadonly;
+        }
+
+        public bool IsReadonly
+        {
+            get { return (Flags | PostGis2Flags.IsReadonly) == PostGis2Flags.IsReadonly; }
+        }
+
+        public bool HasBoundingBox
+        {
+            get { return (Flags | PostGis2Flags.HasBoundingBox) == PostGis2Flags.HasBoundingBox; }
+            set 
+            {
+                if (IsReadonly)
+                    throw new InvalidOperationException("IsReadonly");
+
+                _sridFlags = _sridFlags & ~(uint)PostGis2Flags.HasBoundingBox;
+                if (value)
+                    _sridFlags = _sridFlags | (uint)PostGis2Flags.HasBoundingBox;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets whether M ordinates are present
+        /// </summary>
+        public bool HasM
+        {
+            get { return (Flags | PostGis2Flags.HasM) == PostGis2Flags.HasM; }
+            set
+            {
+                if (IsReadonly)
+                    throw new InvalidOperationException("IsReadonly");
+
+                _sridFlags = _sridFlags & ~(uint)PostGis2Flags.HasM;
+                if (value)
+                    _sridFlags = _sridFlags | (uint)PostGis2Flags.HasM;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets whether Z ordinates are present
+        /// </summary>
+        public bool HasZ
+        {
+            get { return (Flags | PostGis2Flags.HasZ) == PostGis2Flags.HasZ; }
+            set
+            {
+                if (IsReadonly)
+                    throw new InvalidOperationException("IsReadonly");
+
+                _sridFlags = _sridFlags & ~(uint)PostGis2Flags.HasZ;
+                if (value)
+                    _sridFlags = _sridFlags | (uint)PostGis2Flags.HasZ;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets whether Z and M ordinates are present
+        /// </summary>
+        public bool HasZM
+        {
+            get { return (Flags | PostGis2Flags.HasZM) == PostGis2Flags.HasZM; }
+            set
+            {
+                if (IsReadonly)
+                    throw new InvalidOperationException("IsReadonly");
+
+                _sridFlags = _sridFlags & ~(uint)PostGis2Flags.HasZM;
+                if (value)
+                    _sridFlags = _sridFlags | (uint)PostGis2Flags.HasZM;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets whether geometry is geodetic
+        /// </summary>
+        public bool IsGeodetic
+        {
+            get { return (Flags | PostGis2Flags.IsGeodetic) == PostGis2Flags.IsGeodetic; }
+            set
+            {
+                if (IsReadonly)
+                    throw new InvalidOperationException("IsReadonly");
+
+                _sridFlags = _sridFlags & ~(uint)PostGis2Flags.HasZM;
+                if (value)
+                    _sridFlags = _sridFlags | (uint)PostGis2Flags.HasZM;
+            }
+        }
+
+        //private void Read(BinaryReader reader)
+        //{
+        //    _size = reader.ReadUInt32();
+        //    _sridFlags = reader.ReadUInt32();
+
+        //    if (HasBoundingBox)
+        //    {
+        //        _envelope = new Envelope(reader.ReadDouble(), reader.ReadDouble(), 
+        //                                 reader.ReadDouble(), reader.ReadDouble());
+        //        if (HasZ || IsGeodetic)
+        //            _zInterval = new [] { reader.ReadDouble(), reader.ReadDouble() };
+
+        //        if (HasM)
+        //            _mInterval = new [] { reader.ReadDouble(), reader.ReadDouble() };
+        //    }
+        //}
+
+        public void Write(BinaryWriter writer)
+        {
+            writer.Write(_size);
+            writer.Write(_sridFlags);
+            if (!HasBoundingBox) return;
+            
+            // xy
+            writer.Write(_envelope.MinX);
+            writer.Write(_envelope.MaxX);
+            writer.Write(_envelope.MinY);
+            writer.Write(_envelope.MaxY);
+
+            // z
+            if (HasZ || IsGeodetic)
+            {
+                writer.Write(_zInterval[0]);
+                writer.Write(_zInterval[0]);
+            }
+
+            // m
+            if (HasM)
+            {
+                writer.Write(_mInterval[0]);
+                writer.Write(_mInterval[0]);
+            }
+        }
+
+        private static PostGis2Flags Not(PostGis2Flags flags)
+        {
+            return (PostGis2Flags) ~((uint) flags);
+        }
+
+        public IGeometryFactory Factory
+        {
+            get { return _factory ?? (_factory = GeometryServiceProvider.Instance.CreateGeometryFactory(Srid)); }
+        }
+    }
+}
---
+++ NetTopologySuite.IO/NetTopologySuite.IO.PostGis2/PostGis2Writer.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.IO;
+using GeoAPI.Geometries;
+using GeoAPI.IO;
+
+namespace NetTopologySuite.IO
+{
+    public class PostGis2Writer : IBinaryGeometryWriter
+    {
+        private Ordinates _handleOrdinates;
+
+        /// <summary>
+        /// Gets or sets whether the geometry notation should be geodetic
+        /// </summary>
+        public bool IsGeodetic { get; set; }
+        
+        #region Implementation of IGeometryIOSettings
+
+        /// <summary>
+        /// Gets or sets whether the SpatialReference ID must be handled.
+        /// </summary>
+        public bool HandleSRID
+        {
+            get
+            {
+                //Postgis 2 always has SRID
+                return true;
+            }
+            set { }
+        }
+
+        /// <summary>
+        /// Gets and <see cref="Ordinates"/> flag that indicate which ordinates can be handled.
+        /// </summary>
+        /// <remarks>
+        /// This flag must always return at least <see cref="Ordinates.XY"/>.
+        /// </remarks>
+        public Ordinates AllowedOrdinates
+        {
+            get
+            {
+                return IsGeodetic ? Ordinates.XYZ : Ordinates.XYZM;
+            }
+        }
+
+        /// <summary>
+        /// Gets and sets <see cref="Ordinates"/> flag that indicate which ordinates shall be handled.
+        /// </summary>
+        /// <remarks>
+        /// No matter which <see cref="Ordinates"/> flag you supply, <see cref="Ordinates.XY"/> are always processed,
+        /// the rest is binary and 'ed with <see cref="IGeometryIOSettings.AllowedOrdinates"/>.
+        /// </remarks>
+        public Ordinates HandleOrdinates
+        {
+            get { return _handleOrdinates; }
+            set
+            {
+                if (IsGeodetic)
+                {
+                    _handleOrdinates = Ordinates.XYZ;
+                    return;
+                } 
+                _handleOrdinates = Ordinates.XY | value;
+            }
+        }
+
+        #endregion
+
+        #region Implementation of IGeometryWriter<byte[]>
+
+        /// <summary>
+        /// Writes a binary representation of a given geometry.
+        /// </summary>
+        /// <param name="geometry">The geometry</param>
+        /// <returns>The binary representation of <paramref name="geometry"/></returns>
+        public byte[] Write(IGeometry geometry)
+        {
+            using (var ms = new MemoryStream())
+            {
+                Write(geometry, ms);
+                return ms.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Writes a binary representation of a given geometry.
+        /// </summary>
+        /// <param name="geometry"></param>
+        /// <param name="stream"></param>
+        public void Write(IGeometry geometry, Stream stream)
+        {
+            using (var writer = CreateWriter(stream))
+            {
+                Write(geometry, writer, 
+                    AllowedOrdinates & HandleOrdinates, 
+                    IsGeodetic);
+            }
+        }
+
+        private static void Write(IGeometry geometry, BinaryWriter writer, Ordinates handleOrdinates, bool isGeodetic)
+        {
+            var pgh = new PostGis2GeometryHeader(geometry, handleOrdinates) {IsGeodetic = isGeodetic};
+            pgh.Write(writer);
+
+            switch (geometry.OgcGeometryType)
+            {
+                case OgcGeometryType.Point:
+                    WritePoint((IPoint) geometry, pgh, writer);
+                    break;
+                case OgcGeometryType.LineString:
+                    WriteLineString((ILineString)geometry, pgh, writer);
+                    break;
+                case OgcGeometryType.Polygon:
+                    WritePolygon((IPolygon)geometry, pgh, writer);
+                    break;
+                case OgcGeometryType.MultiPoint:
+                case OgcGeometryType.MultiLineString:
+                case OgcGeometryType.MultiPolygon:
+                case OgcGeometryType.GeometryCollection:
+                    WriteCollection((IGeometryCollection) geometry, pgh, writer);
+                    break;
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        private static void WriteSequence(ICoordinateSequence sequence, PostGis2GeometryHeader pgh, BinaryWriter writer, int numPoints)
+        {
+            if (numPoints == -1)
+            {
+                numPoints = sequence.Count;
+                writer.Write(numPoints);
+            }
+            
+            var ordinateIndices = pgh.OrdinateIndices;
+            for (var i = 0; i < numPoints; i++)
+            {
+                foreach (var ordinateIndex in ordinateIndices)
+                    writer.Write(sequence.GetOrdinate(i, ordinateIndex));
+            }
+        }
+
+        private static void WritePoint(IPoint geometry, PostGis2GeometryHeader pgh, BinaryWriter writer)
+        {
+            writer.Write((uint)PostGis2GeometryType.Point);
+            WriteSequence(geometry.CoordinateSequence, pgh, writer, -1);
+        }
+
+        private static void WriteLineString(ILineString geometry, PostGis2GeometryHeader pgh, BinaryWriter writer)
+        {
+            writer.Write((uint)PostGis2GeometryType.LineString);
+            WriteSequence(geometry.CoordinateSequence, pgh, writer, -1);
+        }
+
+        private static void WritePolygon(IPolygon geometry, PostGis2GeometryHeader pgh, BinaryWriter writer)
+        {
+            writer.Write((uint)PostGis2GeometryType.Polygon);
+            if (geometry.IsEmpty)
+            {
+                writer.Write(0);
+                return;
+            }
+
+            var numRings = 1 + geometry.NumInteriorRings;
+            
+            //shell
+            writer.Write(numRings);
+            writer.Write(geometry.Shell.NumPoints);
+
+            //holes
+            for (var i = 0; i < geometry.NumInteriorRings; i++ )
+                writer.Write(geometry.GetInteriorRingN(i).NumPoints);
+            
+            //pad
+            if (numRings % 2 != 0)
+                writer.Write(0);
+
+            WriteSequence(geometry.Shell.CoordinateSequence, pgh, writer, geometry.Shell.NumPoints);
+            for (var i = 0; i < geometry.NumInteriorRings; i++ )
+            {
+                var sequence = geometry.GetInteriorRingN(i).CoordinateSequence;
+                WriteSequence(sequence, pgh, writer, sequence.Count);
+            }
+        }
+
+        private static void WriteCollection(IGeometryCollection geometry, PostGis2GeometryHeader pgh, BinaryWriter writer)
+        {
+            writer.Write((uint)geometry.OgcGeometryType.ToPostGis2());
+            if (geometry.IsEmpty)
+            {
+                writer.Write(0);
+                return;
+            }
+
+            for (var i = 0; i < geometry.NumGeometries; i++)
+                Write(geometry.GetGeometryN(i), writer, pgh.Ordinates, pgh.IsGeodetic);
+        }
+
+        #endregion
+
+        #region Implementation of IBinaryGeometryWriter
+
+        /// <summary>
+        /// Gets or sets the desired <see cref="IBinaryGeometryWriter.ByteOrder"/>
+        /// </summary>
+        public ByteOrder ByteOrder { get; set; }
+
+        private BinaryWriter CreateWriter(Stream stream)
+        {
+            return (ByteOrder == ByteOrder.LittleEndian)
+                ? new BinaryWriter(stream)
+                : new BEBinaryWriter(stream);
+        }
+
+        #endregion
+    }
+}
