4878280748654d5690ba9503ad750244ef1ff728
DGuidi <diegoguidi@gmail.com>
2012-07-02 09:56:36 +0000

    fixed gmlnamespace, as suggested here: http://code.google.com/p/nettopologysuite/issues/detail?id=116 Issue: 116
    git-svn-id: http://nettopologysuite.googlecode.com/svn/trunk@844 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

--- NetTopologySuite/IO/GML2/GMLElements.cs
+++ NetTopologySuite/IO/GML2/GMLElements.cs
@@ -6,7 +6,7 @@ namespace NetTopologySuite.IO.GML2
     internal static class GMLElements
     {
         internal const string gmlPrefix = "gml";
-        internal const string gmlNS = "urn:gml";
+        internal const string gmlNS = "http://www.opengis.net/gml";
         internal const string gmlNSDecl = " xmlns:gml=\"" + gmlNS + "\"";
     }
 }
