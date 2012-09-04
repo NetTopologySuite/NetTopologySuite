7e391abae39f78a16ae4a3ae0ac8834efb5a8199
FObermaier <felix.obermaier@netcologne.de>
2012-09-04 09:10:57 +0000

    [trunk] JTS' rev. 649 - Added unit test for Contains predicate failure case
    git-svn-id: http://nettopologysuite.googlecode.com/svn/trunk@882 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

---
+++ NetTopologySuite.Tests.NUnit/Operation/Relate/ContainsTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using GeoAPI.Geometries;
+using NUnit.Framework;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+
+namespace NetTopologySuite.Tests.NUnit.Operation.Relate
+{
+public class ContainsTest
+{
+  private readonly WKTReader _reader = new WKTReader(new GeometryFactory());
+
+  /**
+   * From GEOS #572
+   * 
+   * The cause is that the longer line nodes the single-segment line.
+   * The node then tests as not lying precisely on the original longer line.
+   * 
+   * The solution is to change the relate algorithm so that it never computes
+   * new intersection points, only ones which occur at existing vertices.
+   * (The topology of the implicit intersections can still be computed
+   * to contribute to the intersection matrix result).
+   * This will require a complete reworking of the relate algorithm. 
+   */
+    [Test]
+    public void TestContainsIncorrect()
+    {
+        var a = _reader.Read("LINESTRING (1 0, 0 2, 0 0, 2 2)");
+        var b = _reader.Read("LINESTRING (0 0, 2 2)");
+
+        // actual matrix is 001F001F2
+        Assert.IsTrue(a.Contains(b));
+    }
+}}
---
+++ NetTopologySuite.Tests.NUnit/Operation/Relate/RelateTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using GeoAPI.Geometries;
+using NUnit.Framework;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+using NetTopologySuite.Operation.Relate;
+
+namespace NetTopologySuite.Tests.NUnit.Operation.Relate
+{
+/**
+ * Tests {@link Geometry#relate}.
+ *
+ * @author Martin Davis
+ * @version 1.7
+ */
+public class RelateTest
+{
+  private static readonly WKTReader Reader = new WKTReader(new GeometryFactory());
+
+  /**
+   * From GEOS #572
+   * 
+   * The cause is that the longer line nodes the single-segment line.
+   * The node then tests as not lying precisely on the original longer line.
+   * 
+   * @throws Exception
+   */
+    [Test]
+  public void TestContainsIncorrectIntersectionMatrix()
+  {
+    var a = "LINESTRING (1 0, 0 2, 0 0, 2 2)";
+    var b = "LINESTRING (0 0, 2 2)";
+
+    // actual matrix is 001F001F2
+    RunRelateTest(a, b,  "101F00FF2"    );
+  }
+
+  private static void RunRelateTest(String wkt1, String wkt2, String expectedIM)
+  {
+    var g1 = Reader.Read(wkt1);
+    var g2 = Reader.Read(wkt2);
+    var im = RelateOp.Relate(g1, g2);
+    var imStr = im.ToString();
+    Console.WriteLine("expected: {0}", expectedIM);
+    Console.WriteLine("result:   {0}", imStr);
+    Assert.IsTrue(im.Matches(expectedIM));
+  }
+}}
