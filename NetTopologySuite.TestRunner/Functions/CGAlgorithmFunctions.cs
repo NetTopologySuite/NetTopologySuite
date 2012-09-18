4560dd13b5260c75e48325c70d732cf18cfbd557
FObermaier <felix.obermaier@netcologne.de>
2012-09-18 12:18:04 +0000

    [trunk] JTS' rev 661 - Added CGAlgorithms functions
    git-svn-id: http://nettopologysuite.googlecode.com/svn/trunk@900 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

---
+++ NetTopologySuite.TestRunner/Functions/CGAlgorithmFunctions.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using GeoAPI.Geometries;
+using NetTopologySuite.Algorithm;
+
+namespace Open.Topology.TestRunner.Functions
+{
+    public class CGAlgorithmFunctions
+    {
+        public static int orientationIndex(IGeometry segment, IGeometry ptGeom)
+        {
+            if (segment.NumPoints != 2 || ptGeom.NumPoints != 1)
+            {
+                throw new ArgumentException("A must have two points and B must have one");
+            }
+            Coordinate[] segPt = segment.Coordinates;
+
+            Coordinate p = ptGeom.Coordinate;
+            int index = CGAlgorithms.OrientationIndex(segPt[0], segPt[1], p);
+            return index;
+        }
+
+        public static int orientationIndexDD(IGeometry segment, IGeometry ptGeom)
+        {
+            if (segment.NumPoints != 2 || ptGeom.NumPoints != 1)
+            {
+                throw new ArgumentException("A must have two points and B must have one");
+            }
+            Coordinate[] segPt = segment.Coordinates;
+
+            Coordinate p = ptGeom.Coordinate;
+            int index = CGAlgorithmsDD.OrientationIndex(segPt[0], segPt[1], p);
+            return index;
+        }
+
+        public static bool segmentIntersects(IGeometry g1, IGeometry g2)
+        {
+            Coordinate[] pt1 = g1.Coordinates;
+            Coordinate[] pt2 = g2.Coordinates;
+            RobustLineIntersector ri = new RobustLineIntersector();
+            ri.ComputeIntersection(pt1[0], pt1[1], pt2[0], pt2[1]);
+            return ri.HasIntersection;
+        }
+
+        public static IGeometry segmentIntersection(IGeometry g1, IGeometry g2)
+        {
+            Coordinate[] pt1 = g1.Coordinates;
+            Coordinate[] pt2 = g2.Coordinates;
+            RobustLineIntersector ri = new RobustLineIntersector();
+            ri.ComputeIntersection(pt1[0], pt1[1], pt2[0], pt2[1]);
+            switch (ri.IntersectionNum)
+            {
+                case 0:
+                    // no intersection => return empty point
+                    return g1.Factory.CreatePoint((Coordinate)null);
+                case 1:
+                    // return point
+                    return g1.Factory.CreatePoint(ri.GetIntersection(0));
+                case 2:
+                    // return line
+                    return g1.Factory.CreateLineString(
+                        new Coordinate[] {
+              ri.GetIntersection(0),
+              ri.GetIntersection(1)
+          });
+            }
+            return null;
+        }
+
+        public static IGeometry segmentIntersectionDD(IGeometry g1, IGeometry g2)
+        {
+            Coordinate[] pt1 = g1.Coordinates;
+            Coordinate[] pt2 = g2.Coordinates;
+
+            // first check if there actually is an intersection
+            RobustLineIntersector ri = new RobustLineIntersector();
+            ri.ComputeIntersection(pt1[0], pt1[1], pt2[0], pt2[1]);
+            if (!ri.HasIntersection)
+            {
+                // no intersection => return empty point
+                return g1.Factory.CreatePoint((Coordinate)null);
+            }
+
+            Coordinate intPt = CGAlgorithmsDD.Intersection(pt1[0], pt1[1], pt2[0], pt2[1]);
+            return g1.Factory.CreatePoint(intPt);
+        }
+
+
+    }
+}
