71d0ac520ba40342fb56cb5c423cacb18ba57b72
FObermaier <felix.obermaier@netcologne.de>
2013-01-16 21:45:05 +0000

    [trunk] - patches for MonoDroid by kjeremy. Thanks
    git-svn-id: http://nettopologysuite.googlecode.com/svn/trunk@998 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

---
+++ MonoDroid/NetTopologySuite/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+//using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NetTopologySuite.MonoDroid")]
+[assembly: AssemblyDescription("A .NET library for GIS operations, direct porting of JTS Topology Suite 1.13 library")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Stable")]
+#endif
+[assembly: AssemblyCompany("NetTopologySuite Team")]
+[assembly: AssemblyProduct("NTS Topology Suite")]
+[assembly: AssemblyCopyright("Copyright © 2006 - 2013 Diego Guidi, John Diss (www.newgrove.com), Felix Obermaier (www.ivv-aachen.de), Todd Jackson")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.13.0")]
+[assembly: AssemblyFileVersion("1.13.0")]
