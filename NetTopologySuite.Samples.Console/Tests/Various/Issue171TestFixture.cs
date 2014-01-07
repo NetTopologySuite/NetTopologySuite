0bc1fb04e15ef74a95c042ecc313543dc89d9c54
DGuidi <diegoguidi@gmail.com>
2014-01-07 14:35:20 +0000

    added specific test that mimics the WKTWriter behavior Issue: 171
    git-svn-id: http://nettopologysuite.googlecode.com/svn/trunk@1136 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

---
+++ NetTopologySuite.Samples.Console/Tests/Various/Issue171TestFixture.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using GeoAPI.Geometries;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+using NUnit.Framework;
+
+namespace NetTopologySuite.Samples.Tests.Various
+{
+    // see https://code.google.com/p/nettopologysuite/issues/detail?id=171
+    [TestFixture]
+    public class Issue171TestFixture
+    {
+        [Test]
+        public void large_numbers_should_be_formatted_properly()
+        {
+            const string expected = "123456789012345680";
+            const double d = 123456789012345680d;
+
+            IPrecisionModel precisionModel = new PrecisionModel(1E9);
+            NumberFormatInfo formatter = CreateFormatter(precisionModel);
+            string format = "0." + StringOfChar('#', formatter.NumberDecimalDigits);
+            string actual = d.ToString(format, formatter);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        // same code used in WKTWriter
+        private static NumberFormatInfo CreateFormatter(IPrecisionModel precisionModel)
+        {
+            int digits = precisionModel.MaximumSignificantDigits;
+            int decimalPlaces = Math.Max(0, digits);
+            NumberFormatInfo nfi = new NumberFormatInfo
+            {
+                NumberDecimalSeparator = ".",
+                NumberDecimalDigits = decimalPlaces,
+                NumberGroupSeparator = String.Empty,
+                NumberGroupSizes = new int[] { }
+            };
+            return nfi;
+        }
+
+        // same code used in WKTWriter
+        private static string StringOfChar(char ch, int count)
+        {
+            StringBuilder buf = new StringBuilder();
+            for (int i = 0; i < count; i++)
+                buf.Append(ch);
+            return buf.ToString();
+        }
+    }
+}
