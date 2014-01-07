3947d7e0cd85c74cdb25d18639c16bc46a398924
DGuidi <diegoguidi@gmail.com>
2014-01-07 15:07:20 +0000

    using DoubleConverter class the test works Issue: 171
    git-svn-id: http://nettopologysuite.googlecode.com/svn/trunk@1139 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

--- NetTopologySuite.Samples.Console/Tests/Various/Issue171TestFixture.cs
+++ NetTopologySuite.Samples.Console/Tests/Various/Issue171TestFixture.cs
@@ -28,25 +28,40 @@ namespace NetTopologySuite.Samples.Tests.Various
         public void large_decimals_are_formatted_properly()
         {
             const string expected = "123456789012345680";
-            const decimal l = 123456789012345680;
+            const decimal m = 123456789012345680;
 
             IPrecisionModel precisionModel = new PrecisionModel(1E9);
             NumberFormatInfo formatter = CreateFormatter(precisionModel);
             string format = "0." + StringOfChar('#', formatter.NumberDecimalDigits);
-            string actual = l.ToString(format, formatter);
+            string actual = m.ToString(format, formatter);            
             Assert.That(actual, Is.EqualTo(expected));
         }
 
         [Test]
-        public void large_doubles_should_be_formatted_properly()
+        public void large_doubles_arent_formatted_properly()
         {
+            /*
+             * http://stackoverflow.com/questions/2105096/why-is-tostring-rounding-my-double-value
+             * 
+             * By default, the return value only contains 15 digits of precision although a maximum of 17 digits is maintained internally. 
+             */
             const string expected = "123456789012345680";
             const double d = 123456789012345680;
 
             IPrecisionModel precisionModel = new PrecisionModel(1E9);
             NumberFormatInfo formatter = CreateFormatter(precisionModel);
             string format = "0." + StringOfChar('#', formatter.NumberDecimalDigits);            
-            string actual = d.ToString(format, formatter);
+            string actual = d.ToString(format, formatter);            
+            Assert.That(actual, Is.Not.EqualTo(expected));
+        }
+
+        [Test]
+        public void large_doubles_are_formatted_properly_using_doubleconverter()
+        {
+            // see http://www.yoda.arachsys.com/csharp/DoubleConverter.cs
+            const string expected = "123456789012345680";
+            const double d = 123456789012345680;
+            string actual = DoubleConverter.ToExactString(d);
             Assert.That(actual, Is.EqualTo(expected));
         }
 
