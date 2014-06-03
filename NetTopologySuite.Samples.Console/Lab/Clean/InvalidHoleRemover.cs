16ed8757cd5f08a38ee3f631472adce2d788cedc
DGuidi <diegoguidi@gmail.com>
2014-06-03 15:20:05 +0000

    added jts code for rev. 910 http://sourceforge.net/p/jts-topo-suite/code/910 NOTE: placed "jts-lab" code in a folder inside a test project
    git-svn-id: http://nettopologysuite.googlecode.com/svn/trunk@1234 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

---
+++ NetTopologySuite.Samples.Console/Lab/Clean/InvalidHoleRemover.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using GeoAPI.Geometries;
+using GeoAPI.Geometries.Prepared;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.Geometries.Prepared;
+using NetTopologySuite.Geometries.Utilities;
+
+namespace NetTopologySuite.Samples.Lab.Clean
+{
+    /// <summary>
+    /// Removes holes which are invalid due to not being wholly covered by the parent shell.
+    /// </summary>
+    /// <remarks>
+    /// Does not remove holes which are invalid due to touching other rings at more than one point
+    /// </remarks>
+    /// <author>Martin Davis</author>
+    public class InvalidHoleRemover
+    {
+        /// <summary>
+        /// Removes invalid holes from the polygons in a geometry.
+        /// </summary>
+        /// <param name="geom">The geometry to clean</param>
+        /// <returns>The geometry with invalid holes removed</returns>
+        public static IGeometry Clean(IGeometry geom)
+        {
+            InvalidHoleRemover pihr = new InvalidHoleRemover(geom);
+            return pihr.GetResult();
+        }
+
+        private readonly IGeometry _geom;
+
+        /// <summary>
+        /// Creates a new invalid hole remover instance.
+        /// </summary>
+        /// <param name="geom">The geometry to process</param>
+        public InvalidHoleRemover(IGeometry geom)
+        {
+            _geom = geom;
+        }
+
+        /// <summary>
+        /// Gets the cleaned geometry.
+        /// </summary>
+        /// <returns>The geometry with invalid holes removed.</returns>
+        public IGeometry GetResult()
+        {
+            return GeometryMapper.Map(_geom, new InvalidHoleRemoverMapOp());
+        }
+
+        private class InvalidHoleRemoverMapOp : GeometryMapper.IMapOp
+        {
+            public IGeometry Map(IGeometry geom)
+            {
+                if (geom is IPolygon)
+                {
+                    IPolygon poly = (IPolygon)geom;
+                    return PolygonInvalidHoleRemover.Clean(poly);
+                }
+                return geom;
+            }
+        }
+
+        private class PolygonInvalidHoleRemover
+        {
+            public static IPolygon Clean(IPolygon poly)
+            {
+                PolygonInvalidHoleRemover pihr = new PolygonInvalidHoleRemover(poly);
+                return pihr.GetResult();
+            }
+
+            private readonly IPolygon _poly;
+
+            private PolygonInvalidHoleRemover(IPolygon poly)
+            {
+                _poly = poly;
+            }
+
+            private IPolygon GetResult()
+            {
+                IGeometryFactory gf = _poly.Factory;
+                ILinearRing shell = (ILinearRing)_poly.ExteriorRing;
+                IPreparedGeometry shellPrep = PreparedGeometryFactory.Prepare(gf.CreatePolygon(shell));
+
+                IList<IGeometry> holes = new List<IGeometry>();
+                for (int i = 0; i < _poly.NumInteriorRings; i++)
+                {
+                    IGeometry hole = _poly.GetInteriorRingN(i);
+                    if (shellPrep.Covers(hole))
+                        holes.Add(hole);
+                }
+                // all holes valid, so return original
+                if (holes.Count == _poly.NumInteriorRings)
+                    return _poly;
+
+                // return new polygon with covered holes only
+                ILinearRing[] arr = GeometryFactory.ToLinearRingArray(holes);
+                IPolygon result = gf.CreatePolygon(shell, arr);
+                return result;
+            }
+        }
+    }
+}
---
+++ NetTopologySuite.Samples.Console/Lab/Clean/InvalidHoleRemoverTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using GeoAPI.Geometries;
+using NetTopologySuite.IO;
+using NUnit.Framework;
+
+namespace NetTopologySuite.Samples.Lab.Clean
+{
+    [TestFixture]
+    public class InvalidHoleRemoverTest
+    {
+        [Test]
+        public void NoHole()
+        {
+            CheckHolesRemoved("POLYGON ((1 9, 9 9, 9 1, 1 1, 1 9))",
+                "POLYGON ((1 9, 9 9, 9 1, 1 1, 1 9))");
+        }
+
+        [Test]
+        public void OneValid()
+        {
+            CheckHolesRemoved("POLYGON ((1 9, 9 9, 9 1, 1 1, 1 9), (5 5, 5 2, 8 2, 5 5))",
+                "POLYGON ((1 9, 9 9, 9 1, 1 1, 1 9), (5 5, 5 2, 8 2, 5 5))");
+        }
+
+        [Test]
+        public void OneOutside()
+        {
+            CheckHolesRemoved("POLYGON ((1 9, 9 9, 9 1, 1 1, 1 9), (11 5, 11 2, 14 2, 11 5))",
+                "POLYGON ((1 9, 9 9, 9 1, 1 1, 1 9))");
+        }
+
+        [Test]
+        public void OneValidOneOutside()
+        {
+            CheckHolesRemoved("POLYGON ((1 9, 9 9, 9 1, 1 1, 1 9), (11 5, 11 2, 14 2, 11 5), (2 5, 2 2, 5 2, 2 5))",
+                "POLYGON ((1 1, 1 9, 9 9, 9 1, 1 1), (2 2, 5 2, 2 5, 2 2))");
+        }
+
+        [Test]
+        public void OneOverlapping()
+        {
+            CheckHolesRemoved("POLYGON ((1 9, 9 9, 9 1, 1 1, 1 9), (2 5, 2 2, 12 2, 2 5))",
+                "POLYGON ((1 9, 9 9, 9 1, 1 1, 1 9))");
+        }
+
+        [Test]
+        public void OneOverlappingMP()
+        {
+            CheckHolesRemoved("MULTIPOLYGON (((1 9, 9 9, 9 1, 1 1, 1 9), (2 5, 2 2, 12 2, 2 5)), ((21 9, 25 9, 25 5, 21 5, 21 9)))",
+                "MULTIPOLYGON (((1 9, 9 9, 9 1, 1 1, 1 9)), ((21 9, 25 9, 25 5, 21 5, 21 9)))");
+        }
+
+        [Test]
+        public void OneOverlappingGC()
+        {
+            CheckHolesRemoved("GEOMETRYCOLLECTION (POLYGON ((1 9, 9 9, 9 1, 1 1, 1 9), (2 5, 2 2, 12 2, 2 5)), LINESTRING (15 9, 19 5))",
+                "GEOMETRYCOLLECTION (POLYGON ((1 9, 9 9, 9 1, 1 1, 1 9)), LINESTRING (15 9, 19 5))");
+        }
+
+        [Test]
+        private void CheckHolesRemoved(string inputWKT, string expectedWKT)
+        {
+            IGeometry input = read(inputWKT);
+            IGeometry expected = read(expectedWKT);
+
+            IGeometry actual = InvalidHoleRemover.Clean(input);
+            CheckEqual(expected, actual);
+        }
+
+        [Test]
+        private void CheckEqual(IGeometry expected, IGeometry actual)
+        {
+            IGeometry actualNorm = actual.Normalized();
+            bool equal = actualNorm.EqualsExact(expected.Normalized());
+            if (!equal)
+            {
+                Console.WriteLine("FAIL - Expected = " + expected
+                    + " actual = " + actual.Normalized());
+            }
+            Assert.That(equal, Is.True);
+        }
+
+        private IGeometry read(string wkt)
+        {
+            WKTReader reader = new WKTReader();
+            return reader.Read(wkt);
+        }
+    }
+}
