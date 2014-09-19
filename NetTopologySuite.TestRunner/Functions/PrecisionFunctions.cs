3375645da63b00c127ef73dc5ca87b61c89b90e7
Diego Guidi <diegoguidi@gmail.com>
2014-09-19 15:45:38 +0200

    applied changes from jts r919

--- NetTopologySuite.TestRunner/Functions/PrecisionFunctions.cs
+++ NetTopologySuite.TestRunner/Functions/PrecisionFunctions.cs
@@ -6,12 +6,19 @@ namespace Open.Topology.TestRunner.Functions
 {
     public static class PrecisionFunctions
     {
+        /*
+        public static IGeometry OLDReducePrecisionPointwise(IGeometry geom, double scaleFactor)
+        {
+            PrecisionModel pm = new PrecisionModel(scaleFactor);
+            IGeometry reducedGeom = SimpleGeometryPrecisionReducer.Reduce(geom, pm);
+            return reducedGeom;
+        }
+        */
+
         public static IGeometry ReducePrecisionPointwise(IGeometry geom, double scaleFactor)
         {
-            var pm = new PrecisionModel(scaleFactor);
-
-            var reducedGeom = SimpleGeometryPrecisionReducer.Reduce(geom, pm);
-
+            PrecisionModel pm = new PrecisionModel(scaleFactor);
+            IGeometry reducedGeom = GeometryPrecisionReducer.Reduce(geom, pm);
             return reducedGeom;
         }
     }
