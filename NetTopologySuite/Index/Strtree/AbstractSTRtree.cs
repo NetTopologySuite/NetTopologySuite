NetTopologySuite/NetTopologySuite

commit de89f7c678eea7ba936a297a76b2accb6cf4eb93

[trunk] - Generics on NTS.Index.StrTree and related - fixing project file
git-svn-id: http://nettopologysuite.googlecode.com/svn/trunk@990 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

NetTopologySuite/Index/Strtree/AbstractSTRtree.cs
@@ -1,4 +1,5 @@
 using System;
+using GeoAPI.Geometries;
 using IList = System.Collections.Generic.IList<object>;
 using System.Collections.Generic;
 using NetTopologySuite.Utilities;
@@ -10,13 +11,14 @@ namespace NetTopologySuite.Index.Strtree
     /// P. Rigaux, Michel Scholl and Agnes Voisard. <i>Spatial Databases With
     /// Application To GIS</i>. Morgan Kaufmann, San Francisco, 2002.
     /// <para>
-    /// This implementation is based on <see cref="IBoundable"/>s rather than just <see cref="AbstractNode"/>s,
+    /// This implementation is based on <see cref="IBoundable{T, TItem}"/>s rather than just <see cref="AbstractNode{T, TItem}"/>s,
     /// because the STR algorithm operates on both nodes and
-    /// data, both of which are treated as <see cref="IBoundable"/>s.
+    /// data, both of which are treated as <see cref="IBoundable{T, TItem}"/>s.
     /// </para>
     /// </summary>
     [Serializable]
-    public abstract class AbstractSTRtree
+    public abstract class AbstractSTRtree<T, TItem>
+        where T: IIntersectable<T>, IExpandable<T>
     {
         /// <returns>
         /// A test for intersection between two bounds, necessary because subclasses
@@ -32,19 +34,19 @@ namespace NetTopologySuite.Index.Strtree
             /// <param name="aBounds">The bounds of one spatial object.</param>
             /// <param name="bBounds">The bounds of another spatial object.</param>                        
             /// <returns>Whether the two bounds intersect.</returns>
-            bool Intersects(object aBounds, object bBounds);
+            bool Intersects(T aBounds, T bBounds);
         }
 
         /// <summary>
         /// 
         /// </summary>
         private readonly object _buildLock = new object();
-        private volatile AbstractNode _root;
+        private volatile AbstractNode<T, TItem> _root;
         private volatile bool _built, _building;
         /**
          * Set to <tt>null</tt> when index is built, to avoid retaining memory.
          */
-        private List<object> _itemBoundables = new List<object>();
+        private List<IBoundable<T, TItem>> _itemBoundables = new List<IBoundable<T,TItem>>();
         private readonly int _nodeCapacity;
 
         /// <summary> 
@@ -89,7 +91,7 @@ namespace NetTopologySuite.Index.Strtree
         /// </summary>
         /// <param name="level"></param>
         /// <returns></returns>
-        protected abstract AbstractNode CreateNode(int level);
+        protected abstract AbstractNode<T, TItem> CreateNode(int level);
 
         /// <summary>
         /// Sorts the childBoundables then divides them into groups of size M, where
@@ -97,16 +99,16 @@ namespace NetTopologySuite.Index.Strtree
         /// </summary>
         /// <param name="childBoundables"></param>
         /// <param name="newLevel"></param>
-        protected virtual IList CreateParentBoundables(IList childBoundables, int newLevel)
+        protected virtual IList<IBoundable<T, TItem>> CreateParentBoundables(IList<IBoundable<T, TItem>> childBoundables, int newLevel)
         {
             Assert.IsTrue(childBoundables.Count != 0);
-            var parentBoundables = new List<object>();
+            var parentBoundables = new List<IBoundable<T, TItem>>();
             parentBoundables.Add(CreateNode(newLevel));
             var castedChildBoundables = PlatformUtilityEx.CastPlatform(childBoundables);
-            var sortedChildBoundables = 
-                new Wintellect.PowerCollections.BigList<object>(castedChildBoundables);
+            var sortedChildBoundables =
+                new Wintellect.PowerCollections.BigList<IBoundable<T, TItem>>(castedChildBoundables);
             sortedChildBoundables.Sort(GetComparer());
-            foreach (IBoundable childBoundable in sortedChildBoundables)
+            foreach (IBoundable<T, TItem> childBoundable in sortedChildBoundables)
             {
                 if (LastNode(parentBoundables).ChildBoundables.Count == NodeCapacity)
                     parentBoundables.Add(CreateNode(newLevel));
@@ -115,9 +117,9 @@ namespace NetTopologySuite.Index.Strtree
             return parentBoundables;
         }
 
-        protected AbstractNode LastNode(IList nodes)
+        protected AbstractNode<T, TItem> LastNode(IList<IBoundable<T, TItem>>  nodes)
         {
-            return (AbstractNode)nodes[nodes.Count - 1];
+            return (AbstractNode<T, TItem>)nodes[nodes.Count - 1];
         }
 
         protected static int CompareDoubles(double a, double b)
@@ -133,16 +135,16 @@ namespace NetTopologySuite.Index.Strtree
         /// <param name="level">the level of the Boundables, or -1 if the boundables are item
         /// boundables (that is, below level 0).</param>
         /// <returns>The root, which may be a ParentNode or a LeafNode.</returns>
-        private AbstractNode CreateHigherLevels(IList boundablesOfALevel, int level)
+        private AbstractNode<T, TItem> CreateHigherLevels(IList<IBoundable<T, TItem>>  boundablesOfALevel, int level)
         {
             Assert.IsTrue(boundablesOfALevel.Count != 0);
             var parentBoundables = CreateParentBoundables(boundablesOfALevel, level + 1);
             if (parentBoundables.Count == 1)
-                return (AbstractNode)parentBoundables[0];
+                return (AbstractNode<T, TItem>)parentBoundables[0];
             return CreateHigherLevels(parentBoundables, level + 1);
         }
 
-        protected AbstractNode Root
+        protected AbstractNode<T, TItem> Root
         {
             get
             {
@@ -186,15 +188,14 @@ namespace NetTopologySuite.Index.Strtree
             }
         }
 
-        protected int GetSize(AbstractNode node)
+        protected int GetSize(AbstractNode<T, TItem> node)
         {
             var size = 0;
-            for (var i = node.ChildBoundables.GetEnumerator(); i.MoveNext(); )
+            foreach (var childBoundable in node.ChildBoundables)
             {
-                var childBoundable = (IBoundable)i.Current;
-                if (childBoundable is AbstractNode)
-                    size += GetSize((AbstractNode)childBoundable);
-                else if (childBoundable is ItemBoundable)
+                if (childBoundable is AbstractNode<T, TItem>)
+                    size += GetSize((AbstractNode<T, TItem>)childBoundable);
+                else if (childBoundable is ItemBoundable<T, TItem>)
                     size += 1;
             }
             return size;
@@ -214,36 +215,35 @@ namespace NetTopologySuite.Index.Strtree
             }
         }
 
-        protected int GetDepth(AbstractNode node)
+        protected int GetDepth(AbstractNode<T, TItem> node)
         {
             var maxChildDepth = 0;
-            for (var i = node.ChildBoundables.GetEnumerator(); i.MoveNext(); )
+            foreach (var childBoundable in node.ChildBoundables)
             {
-                var childBoundable = (IBoundable)i.Current;
-                if (!(childBoundable is AbstractNode))
+                if (!(childBoundable is AbstractNode<T, TItem>))
                     continue;
-                var childDepth = GetDepth((AbstractNode)childBoundable);
+                var childDepth = GetDepth((AbstractNode<T, TItem>)childBoundable);
                 if (childDepth > maxChildDepth)
                     maxChildDepth = childDepth;
             }
             return maxChildDepth + 1;
         }
 
-        protected void Insert(object bounds, object item)
+        protected void Insert(T bounds, TItem item)
         {
             Assert.IsTrue(!(_built || _building), "Cannot insert items into an STR packed R-tree after it has been built.");
-            _itemBoundables.Add(new ItemBoundable(bounds, item));
+            _itemBoundables.Add(new ItemBoundable<T, TItem>(bounds, item));
         }
 
         /// <summary>
         /// Also builds the tree, if necessary.
         /// </summary>
         /// <param name="searchBounds"></param>
-        protected IList Query(object searchBounds)
+        protected IList<TItem> Query(T searchBounds)
         {
             Build();
 
-            var matches = new List<object>();
+            var matches = new List<TItem>();
             if (IsEmpty)
                 return matches;
             
@@ -258,7 +258,7 @@ namespace NetTopologySuite.Index.Strtree
             return matches;
         }
 
-        protected void Query(Object searchBounds, IItemVisitor<object> visitor)
+        protected void Query(T searchBounds, IItemVisitor<TItem> visitor)
         {
             Build();
 
@@ -276,33 +276,31 @@ namespace NetTopologySuite.Index.Strtree
                 Query(searchBounds, _root, visitor);
         }
 
-        private void Query(object searchBounds, AbstractNode node, IList matches)
+        private void Query(T searchBounds, AbstractNode<T, TItem> node, IList<TItem> matches)
         {
-            foreach (var obj in node.ChildBoundables)
+            foreach (var childBoundable in node.ChildBoundables)
             {
-                var childBoundable = (IBoundable)obj;
                 if (!IntersectsOp.Intersects(childBoundable.Bounds, searchBounds))
                     continue;
 
-                if (childBoundable is AbstractNode)
-                    Query(searchBounds, (AbstractNode)childBoundable, matches);
-                else if (childBoundable is ItemBoundable)
-                    matches.Add(((ItemBoundable)childBoundable).Item);
+                if (childBoundable is AbstractNode<T, TItem>)
+                    Query(searchBounds, (AbstractNode<T, TItem>)childBoundable, matches);
+                else if (childBoundable is ItemBoundable<T, TItem>)
+                    matches.Add(((ItemBoundable<T, TItem>)childBoundable).Item);
                 else Assert.ShouldNeverReachHere();
             }
         }
 
-        private void Query(object searchBounds, AbstractNode node, IItemVisitor<object> visitor)
+        private void Query(T searchBounds, AbstractNode<T, TItem> node, IItemVisitor<TItem> visitor)
         {
-            foreach (var obj in node.ChildBoundables)
+            foreach (var childBoundable in node.ChildBoundables)
             {
-                var childBoundable = (IBoundable)obj;
                 if (!IntersectsOp.Intersects(childBoundable.Bounds, searchBounds))
                     continue;
-                if (childBoundable is AbstractNode)
-                    Query(searchBounds, (AbstractNode)childBoundable, visitor);
-                else if (childBoundable is ItemBoundable)
-                    visitor.VisitItem(((ItemBoundable)childBoundable).Item);
+                if (childBoundable is AbstractNode<T, TItem>)
+                    Query(searchBounds, (AbstractNode<T, TItem>)childBoundable, visitor);
+                else if (childBoundable is ItemBoundable<T, TItem>)
+                    visitor.VisitItem(((ItemBoundable<T, TItem>)childBoundable).Item);
                 else Assert.ShouldNeverReachHere();
             }
         }
@@ -316,29 +314,29 @@ namespace NetTopologySuite.Index.Strtree
         /// Builds the tree if necessary.
         /// </summary>
         /// <returns>a List of items and/or Lists</returns>
-        public IList ItemsTree()
+        public IList<TItem> ItemsTree()
         {
             Build();
 
             var valuesTree = ItemsTree(_root);
-            return valuesTree ?? new List<object>();
+            return valuesTree ?? new List<TItem>();
         }
 
-        private static IList ItemsTree(AbstractNode node)
+        private static IList<TItem> ItemsTree(AbstractNode<T, TItem> node)
         {
-            var valuesTreeForNode = new List<object>();
+            var valuesTreeForNode = new List<TItem>();
 
-            foreach (IBoundable childBoundable in node.ChildBoundables)
+            foreach (var childBoundable in node.ChildBoundables)
             {
-                if (childBoundable is AbstractNode)
+                if (childBoundable is AbstractNode<T, TItem>)
                 {
-                    var valuesTreeForChild = ItemsTree((AbstractNode)childBoundable);
+                    var valuesTreeForChild = ItemsTree((AbstractNode<T, TItem>)childBoundable);
                     // only add if not null (which indicates an item somewhere in this tree
                     if (valuesTreeForChild != null)
-                        valuesTreeForNode.Add(valuesTreeForChild);
+                        valuesTreeForNode.AddRange(valuesTreeForChild);
                 }
-                else if (childBoundable is ItemBoundable)
-                    valuesTreeForNode.Add(((ItemBoundable) childBoundable).Item);
+                else if (childBoundable is ItemBoundable<T, TItem>)
+                    valuesTreeForNode.Add(((ItemBoundable<T, TItem>)childBoundable).Item);
                 else Assert.ShouldNeverReachHere();
             }
             
@@ -356,7 +354,7 @@ namespace NetTopologySuite.Index.Strtree
         /// Removes an item from the tree.
         /// (Builds the tree, if necessary.)
         /// </summary>
-        protected bool Remove(object searchBounds, object item)
+        protected bool Remove(T searchBounds, TItem item)
         {
             Build();
             if (_itemBoundables.Count == 0)
@@ -364,14 +362,13 @@ namespace NetTopologySuite.Index.Strtree
             return IntersectsOp.Intersects(_root.Bounds, searchBounds) && Remove(searchBounds, _root, item);
         }
 
-        private bool RemoveItem(AbstractNode node, object item)
+        private bool RemoveItem(AbstractNode<T, TItem> node, TItem item)
         {
-            IBoundable childToRemove = null;
+            IBoundable<T, TItem> childToRemove = null;
             for (var i = node.ChildBoundables.GetEnumerator(); i.MoveNext(); )
             {
-                var childBoundable = (IBoundable)i.Current;
-                if (childBoundable is ItemBoundable)
-                    if (((ItemBoundable)childBoundable).Item == item)
+                var childBoundable = i.Current as ItemBoundable<T, TItem>;
+                if (childBoundable != null && ReferenceEquals(childBoundable.Item, item))
                         childToRemove = childBoundable;
             }
             if (childToRemove != null)
@@ -382,26 +379,25 @@ namespace NetTopologySuite.Index.Strtree
             return false;
         }
 
-        private bool Remove(object searchBounds, AbstractNode node, object item)
+        private bool Remove(T searchBounds, AbstractNode<T, TItem> node, TItem item)
         {
             // first try removing item from this node
             var found = RemoveItem(node, item);
             if (found)
                 return true;
-            AbstractNode childToPrune = null;
+            AbstractNode<T, TItem> childToPrune = null;
             // next try removing item from lower nodes
-            for (var i = node.ChildBoundables.GetEnumerator(); i.MoveNext(); )
+            foreach (var childBoundable in node.ChildBoundables)
             {
-                var childBoundable = (IBoundable)i.Current;
                 if (!IntersectsOp.Intersects(childBoundable.Bounds, searchBounds))
                     continue;
-                if (!(childBoundable is AbstractNode))
+                if (!(childBoundable is AbstractNode<T, TItem>))
                     continue;
-                found = Remove(searchBounds, (AbstractNode)childBoundable, item);
+                found = Remove(searchBounds, (AbstractNode<T, TItem>)childBoundable, item);
                 // if found, record child for pruning and exit
                 if (!found)
                     continue;
-                childToPrune = (AbstractNode)childBoundable;
+                childToPrune = (AbstractNode<T, TItem>)childBoundable;
                 break;
             }
             // prune child if possible
@@ -411,14 +407,14 @@ namespace NetTopologySuite.Index.Strtree
             return found;
         }
 
-        protected IList BoundablesAtLevel(int level)
+        protected IList<IBoundable<T, TItem>> BoundablesAtLevel(int level)
         {
-            IList boundables = new List<object>();
-            BoundablesAtLevel(level, _root, ref boundables);
+            var boundables = new List<IBoundable<T, TItem>>();
+            BoundablesAtLevel(level, _root, boundables);
             return boundables;
         }
 
-        private void BoundablesAtLevel(int level, AbstractNode top, ref IList boundables)
+        private static void BoundablesAtLevel(int level, AbstractNode<T, TItem> top, ICollection<IBoundable<T, TItem>>  boundables)
         {
             Assert.IsTrue(level > -2);
             if (top.Level == level)
@@ -426,20 +422,19 @@ namespace NetTopologySuite.Index.Strtree
                 boundables.Add(top);
                 return;
             }
-            for (var i = top.ChildBoundables.GetEnumerator(); i.MoveNext(); )
+            foreach (var boundable in top.ChildBoundables )
             {
-                var boundable = (IBoundable)i.Current;
-                if (boundable is AbstractNode)
-                    BoundablesAtLevel(level, (AbstractNode)boundable, ref boundables);
+                if (boundable is AbstractNode<T, TItem>)
+                    BoundablesAtLevel(level, (AbstractNode<T, TItem>)boundable, boundables);
                 else
                 {
-                    Assert.IsTrue(boundable is ItemBoundable);
+                    Assert.IsTrue(boundable is ItemBoundable<T, TItem>);
                     if (level == -1)
                         boundables.Add(boundable);
                 }
             }
         }
-        protected abstract IComparer<object> GetComparer();
+        protected abstract IComparer<IBoundable<T, TItem>> GetComparer();
 
     }
 }

NetTopologySuite/Operation/Union/CascadedPolygonUnion.cs
@@ -101,7 +101,7 @@ namespace NetTopologySuite.Operation.Union
              * This makes unioning more efficient, since vertices are more likely 
              * to be eliminated on each round.
              */
-            var index = new STRtree(StrtreeNodeCapacity);
+            var index = new STRtree<object>(StrtreeNodeCapacity);
             foreach (IGeometry item in _inputPolys)
                 index.Insert(item.EnvelopeInternal, item);
 
@@ -275,7 +275,7 @@ namespace NetTopologySuite.Operation.Union
             foreach (var o in geomTree)
             {
                 IGeometry geom = null;
-                if (o is IList)
+                if (o is IList<IGeometry>)
                     geom = UnionTree((IList) o);
                 else if (o is IGeometry)
                     geom = (IGeometry) o;

NetTopologySuite.IO/NetTopologySuite.IO.GDB/GDBReader.cs
@@ -1,8 +1,6 @@
 using System;
 using System.IO;
 using GeoAPI.Geometries;
-using GeoAPI.IO;
-using NetTopologySuite.Geometries;
 
 namespace NetTopologySuite.IO
 {    
@@ -35,6 +33,41 @@ namespace NetTopologySuite.IO
                 return Read(reader);
         }
 
+        private static Ordinates GetOrdinatesFromShapeGeometryType(ShapeGeometryType sgt)
+        {
+            switch (sgt)
+            {
+                case ShapeGeometryType.Point:
+                case ShapeGeometryType.MultiPoint:
+                case ShapeGeometryType.LineString:
+                case ShapeGeometryType.Polygon:
+                    return Ordinates.XY;
+
+                case ShapeGeometryType.PointZ:
+                case ShapeGeometryType.MultiPointZ:
+                case ShapeGeometryType.LineStringZ:
+                case ShapeGeometryType.PolygonZ:
+                    return Ordinates.XYZ;
+
+                case ShapeGeometryType.PointM:
+                case ShapeGeometryType.MultiPointM:
+                case ShapeGeometryType.LineStringM:
+                case ShapeGeometryType.PolygonM:
+                    return Ordinates.XYM;
+
+                case ShapeGeometryType.PointZM:
+                case ShapeGeometryType.MultiPointZM:
+                case ShapeGeometryType.LineStringZM:
+                case ShapeGeometryType.PolygonZM:
+                    return Ordinates.XYZM;
+
+                case ShapeGeometryType.MultiPatch:
+                    throw new NotSupportedException("FeatureType " + sgt + " not supported.");
+
+                default:
+                    throw new ArgumentOutOfRangeException("FeatureType " + sgt + " not recognized by the system");
+            }
+        }
         /// <summary>
         /// 
         /// </summary>
@@ -43,6 +76,7 @@ namespace NetTopologySuite.IO
         public IGeometry Read(BinaryReader reader)
         {
             var shapeType = (ShapeGeometryType)reader.ReadInt32();
+            var ordinates = GetOrdinatesFromShapeGeometryType(shapeType);
 
             switch (shapeType)
             {
@@ -50,28 +84,28 @@ namespace NetTopologySuite.IO
                 case ShapeGeometryType.PointM:
                 case ShapeGeometryType.PointZ:
                 case ShapeGeometryType.PointZM:
-                    return ReadPoint(reader);
+                    return ReadPoint(reader, ordinates);
 
                 case ShapeGeometryType.LineString:
                 case ShapeGeometryType.LineStringM:
                 case ShapeGeometryType.LineStringZ:
                 case ShapeGeometryType.LineStringZM:
-                    return ReadLineString(reader);
+                    return ReadLineString(reader, ordinates);
 
                 case ShapeGeometryType.Polygon:
                 case ShapeGeometryType.PolygonM:
                 case ShapeGeometryType.PolygonZ:
                 case ShapeGeometryType.PolygonZM:
-                    return ReadPolygon(reader);
+                    return ReadPolygon(reader, ordinates);
 
                 case ShapeGeometryType.MultiPoint:
                 case ShapeGeometryType.MultiPointM:
                 case ShapeGeometryType.MultiPointZ:
                 case ShapeGeometryType.MultiPointZM:
-                    return ReadMultiPoint(reader);
+                    return ReadMultiPoint(reader, ordinates);
 
                 case ShapeGeometryType.MultiPatch:
-                    throw new NotImplementedException("FeatureType " + shapeType + " not supported.");
+                    throw new NotSupportedException("FeatureType " + shapeType + " not supported.");
 
                 default:
                     throw new ArgumentOutOfRangeException("FeatureType " + shapeType + " not recognized by the system");

NetTopologySuite.IO/NetTopologySuite.IO.Tests/AbstractIOFixture.cs
@@ -1,4 +1,6 @@
-﻿namespace NetTopologySuite.IO.Tests
+﻿using NetTopologySuite.Geometries.Implementation;
+
+namespace NetTopologySuite.IO.Tests
 {
     using System;
     using System.Configuration;
@@ -82,7 +84,11 @@
         {
             AppSettingsReader asr = new AppSettingsReader();
             this.SRID = (int)asr.GetValue("Srid", typeof(int));
-            this.PrecisionModel = new PrecisionModel((PrecisionModels)Enum.Parse(typeof(PrecisionModels), (string)asr.GetValue("PrecisionModel", typeof(string))));
+            var pm = (string) asr.GetValue("PrecisionModel", typeof (string));
+            int scale;
+            this.PrecisionModel = int.TryParse(pm, out scale) 
+                ? new PrecisionModel(scale) 
+                : new PrecisionModel((PrecisionModels)Enum.Parse(typeof(PrecisionModels), pm));
             this.MinX = (double)asr.GetValue("MinX", typeof(double));
             this.MaxX = (double)asr.GetValue("MaxX", typeof(double));
             this.MinY = (double)asr.GetValue("MinY", typeof(double));
@@ -105,11 +111,14 @@
             }
             protected set
             {
-                PrecisionModel oldPM = new PrecisionModel();
+                var oldPM = new PrecisionModel();
                 if (this.RandomGeometryHelper != null)
                     oldPM = (PrecisionModel)this.RandomGeometryHelper.Factory.PrecisionModel;
                 Debug.Assert(this.RandomGeometryHelper != null, "RandomGeometryHelper != null");
-                this.RandomGeometryHelper.Factory = new GeometryFactory(oldPM, value);
+                if (RandomGeometryHelper.Factory is OgcCompliantGeometryFactory)
+                    this.RandomGeometryHelper.Factory = new OgcCompliantGeometryFactory(oldPM, value);
+                else
+                    this.RandomGeometryHelper.Factory = new GeometryFactory(oldPM, value);
             }
         }
 
@@ -117,17 +126,26 @@
         {
             get
             {
-                return (PrecisionModel)this.RandomGeometryHelper.Factory.PrecisionModel;
+                return (PrecisionModel)RandomGeometryHelper.Factory.PrecisionModel;
             }
             protected set
             {
                 if (value == null)
                     return;
 
-                if (value == this.PrecisionModel)
+                if (value == PrecisionModel)
                     return;
 
-                this.RandomGeometryHelper.Factory = new GeometryFactory(value);
+                var factory = RandomGeometryHelper.Factory;
+                var oldSrid = factory != null ? factory.SRID : 0;
+                var oldFactory = factory != null
+                                     ? factory.CoordinateSequenceFactory
+                                     : CoordinateArraySequenceFactory.Instance;
+                
+                if (RandomGeometryHelper.Factory is OgcCompliantGeometryFactory)
+                    RandomGeometryHelper.Factory = new OgcCompliantGeometryFactory(value, oldSrid, oldFactory);
+                else
+                    RandomGeometryHelper.Factory = new GeometryFactory(value, oldSrid, oldFactory);
             }
         }
 
@@ -172,16 +190,20 @@
 
         public void PerformTest(IGeometry gIn)
         {
-            byte[] b = this.Write(gIn);
-            IGeometry gParsed = this.Read(b);
+            var writer = new WKTWriter(2) {EmitSRID = true, MaxCoordinatesPerLine = 3,};
+            byte[] b = null;
+            Assert.DoesNotThrow(() => b = Write(gIn), "Threw exception during write:\n{0}", writer.WriteFormatted(gIn));
 
-            Assert.IsNotNull(gParsed);
-            CheckEquality(gIn, gParsed);
+            IGeometry gParsed = null;
+            Assert.DoesNotThrow(() => gParsed = Read(b), "Threw exception during read:\n{0}", writer.WriteFormatted(gIn));
+
+            Assert.IsNotNull(gParsed, "Could not be parsed\n{0}", gIn);
+            CheckEquality(gIn, gParsed, writer);
         }
 
-        protected virtual void CheckEquality(IGeometry gIn, IGeometry gParsed)
+        protected virtual void CheckEquality(IGeometry gIn, IGeometry gParsed, WKTWriter writer)
         {
-            Assert.IsTrue(gIn.EqualsExact(gParsed));
+            Assert.IsTrue(gIn.EqualsExact(gParsed), "Instances are not equal\n{0}\n\n{1}", gIn, gParsed);
         }
 
         protected abstract IGeometry Read(byte[] b);

commit 2ed48c858c6d453ee30369ed2e5ab6038605eed8

Fixing a somewhat minor performance regression from de89f7c. That commit caused CascadedPolygonUnion to behave slightly differently than it does in JTS. From experimentation, this change reduces the total execution time of polygon unioning by up to 8%.

NetTopologySuite/Index/Strtree/AbstractSTRtree.cs
@@ -317,17 +317,17 @@ namespace NetTopologySuite.Index.Strtree
         /// Builds the tree if necessary.
         /// </summary>
         /// <returns>a List of items and/or Lists</returns>
-        public IList<TItem> ItemsTree()
+        public IList ItemsTree()
         {
             Build();
 
             var valuesTree = ItemsTree(_root);
-            return valuesTree ?? new List<TItem>();
+            return valuesTree ?? new List<object>();
         }
 
-        private static IList<TItem> ItemsTree(AbstractNode<T, TItem> node)
+        private static IList ItemsTree(AbstractNode<T, TItem> node)
         {
-            var valuesTreeForNode = new List<TItem>();
+            var valuesTreeForNode = new List<object>();
 
             foreach (var childBoundable in node.ChildBoundables)
             {
@@ -336,7 +336,7 @@ namespace NetTopologySuite.Index.Strtree
                     var valuesTreeForChild = ItemsTree((AbstractNode<T, TItem>)childBoundable);
                     // only add if not null (which indicates an item somewhere in this tree
                     if (valuesTreeForChild != null)
-                        valuesTreeForNode.AddRange(valuesTreeForChild);
+                        valuesTreeForNode.Add(valuesTreeForChild);
                 }
                 else if (childBoundable is ItemBoundable<T, TItem>)
                     valuesTreeForNode.Add(((ItemBoundable<T, TItem>)childBoundable).Item);

NetTopologySuite/Operation/Union/CascadedPolygonUnion.cs
@@ -287,7 +287,7 @@ namespace NetTopologySuite.Operation.Union
             foreach (var o in geomTree)
             {
                 IGeometry geom = null;
-                if (o is IList<IGeometry>)
+                if (o is IList)
                     geom = UnionTree((IList)o);
                 else if (o is IGeometry)
                     geom = (IGeometry)o;