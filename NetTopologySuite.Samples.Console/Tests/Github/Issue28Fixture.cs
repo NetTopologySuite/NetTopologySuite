44206f8705e4533d165607e9423b775c92f57a91
FObermaier <felix.obermaier@netcologne.de>
2014-12-23 12:41:15 +0100

    Point out difference in test

--- NetTopologySuite.Samples.Console/Tests/Github/Issue28Fixture.cs
+++ NetTopologySuite.Samples.Console/Tests/Github/Issue28Fixture.cs
@@ -1,4 +1,6 @@
+using System;
 using GeoAPI.Geometries;
+using NetTopologySuite.Algorithm.Distance;
 using NetTopologySuite.Geometries;
 using NetTopologySuite.IO;
 using NUnit.Framework;
@@ -11,17 +13,33 @@ namespace NetTopologySuite.Samples.Tests.Github
         [Test]
         public void test_wkt_wkb_result()
         {
-            TestWktWkb(GeometryFactory.Default, Issue27Fixture.Poly1Wkt, Issue27Fixture.Poly1Wkb);
-            TestWktWkb(GeometryFactory.Default, Issue27Fixture.Poly2Wkt, Issue27Fixture.Poly2Wkb);
+            var failed = TestWktWkb(0, GeometryFactory.Default, Issue27Fixture.Poly1Wkt, Issue27Fixture.Poly1Wkb);
+            failed |= TestWktWkb(1, GeometryFactory.Default, Issue27Fixture.Poly2Wkt, Issue27Fixture.Poly2Wkb);
+            Assert.IsFalse(failed);
         }
 
-        private static void TestWktWkb(IGeometryFactory factory, string wkt, string wkb)
+        private static bool TestWktWkb(int number, IGeometryFactory factory, string wkt, string wkb)
         {
             WKTReader r = new WKTReader(factory);
             IGeometry wktGeom = r.Read(wkt);
             WKBReader s = new WKBReader(factory);
             IGeometry wkbGeom = s.Read(WKBReader.HexToBytes(wkb));
-            Assert.IsTrue(wktGeom.EqualsExact(wkbGeom));
+
+            try
+            {
+                Assert.IsTrue(DiscreteHausdorffDistance.Distance(wktGeom, wkbGeom) < 1e-9, number + ": DiscreteHausdorffDistance.Distance(wktGeom, wkbGeom) < 1e-9");
+                if (!wktGeom.EqualsExact(wkbGeom))
+                {
+                    var diff = wkbGeom.Difference(wktGeom);
+                    Assert.IsTrue(false, number + ": wktGeom.EqualsExact(wkbGeom)\n" + diff.AsText());
+                }
+                return false;
+            }
+            catch (AssertionException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return true;
+            }
         }
     }
 }
