e95e5fcf548c82cfa6c1e2b6a5d78395e9d2287f
Felix Obermaier <felix.obermaier@netcologne.de>
2015-08-11 12:56:25 +0200

    #35: JTS' rev. 968: Added kdtree tests, IOUtil

--- NetTopologySuite.Tests.NUnit/Index/KdTree/KdTreeTest.cs
+++ NetTopologySuite.Tests.NUnit/Index/KdTree/KdTreeTest.cs
@@ -2,6 +2,7 @@
 using GeoAPI.Geometries;
 using NUnit.Framework;
 using NetTopologySuite.Index.KdTree;
+using NetTopologySuite.Tests.NUnit.Utilities;
 
 namespace NetTopologySuite.Tests.NUnit.Index.KdTree
 {
@@ -48,5 +49,45 @@ namespace NetTopologySuite.Tests.NUnit.Index.KdTree
             res = kd.NearestNeighbor(new Coordinate(635, 377)); //Should be G
             Assert.AreEqual("G", res.Data);
         }
+
+        [Test]
+        public void TestMultiplePoint()
+        {
+            var index = Build("MULTIPOINT ((1 1), (2 2))");
+
+            var queryEnv = new Envelope(0, 10, 0, 10);
+
+            var result = index.Query(queryEnv);
+            Assert.AreEqual(2, result.Count, "2 != result.Count");
+            var node = result.First();
+            Assert.IsTrue(node.Coordinate.Equals2D(new Coordinate(1, 1)));
+            node = result.Last();
+            Assert.IsTrue(node.Coordinate.Equals2D(new Coordinate(2, 2)));
+        }
+
+        private static KdTree<object> Build(string wkt)
+        {
+            var geom = IOUtil.Read(wkt);
+            var index = new KdTree<object>(.001);
+            geom.Apply(new TestCoordinateFilter<object>(index));
+            return index;
+        }
+
+        private class TestCoordinateFilter<T> : ICoordinateFilter where T : class
+        {
+            private readonly KdTree<T> _index;
+
+            public TestCoordinateFilter(KdTree<T> index)
+            {
+                _index = index;
+            }
+
+            public void Filter(Coordinate coord)
+            {
+                _index.Insert(coord);
+            }
+        }
+
     }
+
 }
