ee5a083bf1517767fe98adc6840e2a68ec1b0718
FObermaier <felix.obermaier@netcologne.de>
2015-08-15 15:13:41 +0200

    #35: JTS' rev. 1010 Added SpatialIndexFunction.kdTreeQueryRepeated (and others)

--- NetTopologySuite.TestRunner/Functions/SpatialIndexFunctions.cs
+++ NetTopologySuite.TestRunner/Functions/SpatialIndexFunctions.cs
@@ -1,19 +1,30 @@
-﻿using GeoAPI.Geometries;
+﻿using System;
+using GeoAPI.Geometries;
 using NetTopologySuite.Index.KdTree;
+using NetTopologySuite.Index.Quadtree;
+using NetTopologySuite.Index.Strtree;
 
 namespace Open.Topology.TestRunner.Functions
 {
     public class SpatialIndexFunctions
     {
-        public static IGeometry KdTree(IGeometry pts, IGeometry query, double tolerance)
+        public static IGeometry KdTreeQuery(IGeometry pts, IGeometry query, double tolerance)
         {
-            var index = Build(pts, tolerance);
+            var index = BuildKdTree(pts, tolerance);
             var result = index.Query(query.EnvelopeInternal);
             var resultCoords = KdTree<object>.ToCoordinates(result);
             return pts.Factory.CreateMultiPoint(resultCoords);
         }
 
-        private static KdTree<object> Build(IGeometry geom, double tolerance)
+        public static IGeometry KdTreeQueryRepeated(IGeometry pts, IGeometry queryEnv, double tolerance)
+        {
+            var index = BuildKdTree(pts, tolerance);
+            var result = index.Query(queryEnv.EnvelopeInternal);
+            var resultCoords = KdTree<object>.ToCoordinates(result, true);
+            return pts.Factory.CreateMultiPoint(resultCoords);
+        }
+
+        private static KdTree<object> BuildKdTree(IGeometry geom, double tolerance)
         {
             var index = new KdTree<object>(tolerance);
             var pt = geom.Coordinates;
@@ -21,6 +32,61 @@ namespace Open.Topology.TestRunner.Functions
             {
                 index.Insert(pt[i]);
             }
+            return index;
+        }
+
+        private class DelegateGeometryFilter : IGeometryFilter
+        {
+            public Action<IGeometry> DoFilter { get; set; }
+
+            public void Filter(IGeometry geom)
+            {
+                DoFilter(geom);
+            }
+        }
+
+        public static IGeometry STRtreeQuery(IGeometry geoms, IGeometry queryEnv)
+        {
+            var index = BuildSTRtree(geoms);
+            var result = index.Query(queryEnv.EnvelopeInternal);
+            return geoms.Factory.BuildGeometry(result);
+        }
+
+        private static STRtree<IGeometry> BuildSTRtree(IGeometry geom)
+        {
+            var index = new STRtree<IGeometry>();
+            geom.Apply(new DelegateGeometryFilter
+            {
+                DoFilter = delegate(IGeometry tmpGeometry)
+                {
+                    // only insert atomic geometries
+                    if (tmpGeometry is IGeometryCollection) return;
+                    index.Insert(tmpGeometry.EnvelopeInternal, tmpGeometry);
+                }
+            });
+
+            return index;
+        }
+
+        public static IGeometry QuadTreeQuery(IGeometry geoms, IGeometry queryEnv)
+        {
+            var index = BuildQuadtree(geoms);
+            var result = index.Query(queryEnv.EnvelopeInternal);
+            return geoms.Factory.BuildGeometry(result);
+        }
+
+        private static Quadtree<IGeometry> BuildQuadtree(IGeometry geom)
+        {
+            var index = new Quadtree<IGeometry>();
+            geom.Apply(new DelegateGeometryFilter()
+            {
+                DoFilter = delegate(IGeometry tmpGeometry)
+                {
+                    // only insert atomic geometries
+                    if (tmpGeometry is IGeometryCollection) return;
+                    index.Insert(tmpGeometry.EnvelopeInternal, tmpGeometry);
+                }
+            });
             return index;
         }
     }
