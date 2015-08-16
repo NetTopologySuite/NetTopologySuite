08e15535e58fb60c364b9795e7fadd3726006fac
FObermaier <felix.obermaier@netcologne.de>
2015-08-16 18:01:35 +0200

    #35: JTS' rev. 1032 Add Concave Hull experimental code

---
+++ NetTopologySuite.Lab/Hull/ConcaveHull.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeoAPI.Geometries;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.Triangulate;
+using NetTopologySuite.Triangulate.QuadEdge;
+
+namespace NetTopologySuite.Hull
+{
+    public class ConcaveHull
+    {
+        private IGeometry geom;
+        private double tolerance;
+
+        public ConcaveHull(IGeometry geom, double tolerance)
+        {
+            this.geom = geom;
+            this.tolerance = tolerance;
+        }
+
+        public IGeometry GetResult()
+        {
+            var subdiv = BuildDelaunay();
+            var tris = ExtractTriangles(subdiv);
+            var hull = ComputeHull(tris);
+            return hull;
+        }
+
+        private IList<QuadEdgeTriangle> ExtractTriangles(QuadEdgeSubdivision subdiv)
+        {
+            return QuadEdgeTriangle.CreateOn(subdiv);
+        }
+
+        private static Geometry ComputeHull(IList<QuadEdgeTriangle> tris)
+        {
+            return null;
+
+        }
+
+        private QuadEdgeSubdivision BuildDelaunay()
+        {
+            var builder = new DelaunayTriangulationBuilder();
+            builder.SetSites(geom);
+            return builder.GetSubdivision();
+        }
+    }
+}
---
+++ NetTopologySuite.Tests.NUnit/Hull/ConcaveHullTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.Hull;
+using NUnit.Framework;
+
+namespace NetTopologySuite.Tests.NUnit.Hull
+{
+    public class ConcaveHullTest : GeometryTestCase
+    {
+
+        [Test]
+        public void TestSimple() {
+            CheckHull(
+                "POLYGON ((100 200, 200 180, 300 200, 200 190, 100 200))",
+                150,
+                "POLYGON ((100 200, 200 180, 300 200, 200 190, 100 200))"
+                );
+        }
+
+        private void CheckHull(string inputWKT, double tolerance, string expectedWKT) {
+            var input = Read(inputWKT);
+            var expected = Read(expectedWKT);
+            var hull = new ConcaveHull(input, tolerance);
+            var actual = hull.GetResult();
+            CheckEqual(expected, actual);
+        }
+    }
+}
