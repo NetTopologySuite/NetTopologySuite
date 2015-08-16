b28d5d8b56aeb89070716cb840a16d51702a831e
FObermaier <felix.obermaier@netcologne.de>
2015-08-16 10:22:05 +0200

    #35: JTS' rev. 1018 Code documentation

--- NetTopologySuite.Tests.NUnit/Precision/CommonBitsOpTest.cs
+++ NetTopologySuite.Tests.NUnit/Precision/CommonBitsOpTest.cs
@@ -7,6 +7,9 @@ namespace NetTopologySuite.Tests.NUnit.Precision
 {
     public class CommonBitsOpTest : GeometryTestCase
     {
+        /// <summary>
+        /// Tests an issue where CommonBitsRemover was not persisting changes to some kinds of CoordinateSequences
+        /// </summary>
         [Test]
         public void TestPackedCoordinateSequence() {
             var pcsFactory = new GeometryFactory(PackedCoordinateSequenceFactory.DoubleFactory);
@@ -15,7 +18,7 @@ namespace NetTopologySuite.Tests.NUnit.Precision
             var cbo = new CommonBitsOp(true);
             var result = cbo.Intersection(geom0, geom1);
             var expected = geom0.Intersection(geom1);
-            ///Geometry expected = read("POLYGON ((220 215, 215 215, 215 220, 220 220, 220 215))");
+            //Geometry expected = read("POLYGON ((220 215, 215 215, 215 220, 220 220, 220 215))");
             CheckEqual(expected, result);
         }
     }
