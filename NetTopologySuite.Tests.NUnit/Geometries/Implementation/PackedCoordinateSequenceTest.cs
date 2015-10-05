b237f97fbdc4b18f755f19287c482a0e62bc643c
Felix Obermaier <felix.obermaier@netcologne.de>
2015-10-05 06:13:18 +0200

    Reenabled test that is now working

--- NetTopologySuite.Tests.NUnit/Geometries/Implementation/PackedCoordinateSequenceTest.cs
+++ NetTopologySuite.Tests.NUnit/Geometries/Implementation/PackedCoordinateSequenceTest.cs
@@ -1,3 +1,4 @@
+using System;
 using GeoAPI.Geometries;
 using NetTopologySuite.Geometries;
 using NetTopologySuite.Geometries.Implementation;
@@ -11,29 +12,6 @@ namespace NetTopologySuite.Tests.NUnit.Geometries.Implementation
         protected override ICoordinateSequenceFactory CsFactory
         {
             get { return new PackedCoordinateSequenceFactory(); }
-        }
-
-        // TODO: This test was overriden because the base class implementation asserts that the Z ordinate is NaN, 
-        ///but in NTS/JTS 1.9 the result of PackedCoordinateSequence.GetCoordinate method is 0.0
-        // This should be removed once the new Coordinate.NULL_ORDINATE result is migrated from JTS
-        [TestAttribute]
-        [Ignore("test fail")]
-        public override void Test2DZOrdinate()
-        {
-            Coordinate[] coords = CreateArray(Size);
-
-            ICoordinateSequence seq = CsFactory.Create(Size, 2);
-            for (int i = 0; i < seq.Count; i++)
-            {
-                seq.SetOrdinate(i, Ordinate.X, coords[i].X);
-                seq.SetOrdinate(i, Ordinate.Y, coords[i].Y);
-            }
-
-            for (int i = 0; i < seq.Count; i++)
-            {
-                Coordinate p = seq.GetCoordinate(i);
-                Assert.IsTrue(p.Z == 0.0);
-            }
         }
 
         [TestAttribute]
