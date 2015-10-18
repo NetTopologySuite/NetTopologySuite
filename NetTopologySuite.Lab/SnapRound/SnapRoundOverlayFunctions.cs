a7831571ca9afd340d0d05ada07f084ab9657ef9
FObermaier <felix.obermaier@netcologne.de>
2015-10-18 15:46:59 +0200

    JTS' rev. 1087 add GeometrySnapRounder TODOs

--- NetTopologySuite.Lab/SnapRound/SnapRoundOverlayFunctions.cs
+++ NetTopologySuite.Lab/SnapRound/SnapRoundOverlayFunctions.cs
@@ -12,39 +12,40 @@ namespace NetTopologySuite.SnapRound
 
         public static IGeometry Intersection(IGeometry geomA, IGeometry geomB, double scaleFactor)
         {
-            IGeometry[] geom = snapClean(geomA, geomB, scaleFactor);
+            IGeometry[] geom = SnapClean(geomA, geomB, scaleFactor);
             return geom[0].Intersection(geom[1]);
         }
 
         public static IGeometry Difference(IGeometry geomA, IGeometry geomB, double scaleFactor)
         {
-            IGeometry[] geom = snapClean(geomA, geomB, scaleFactor);
+            IGeometry[] geom = SnapClean(geomA, geomB, scaleFactor);
             return geom[0].Difference(geom[1]);
         }
 
         public static IGeometry SymmetricDifference(IGeometry geomA, IGeometry geomB, double scaleFactor)
         {
-            IGeometry[] geom = snapClean(geomA, geomB, scaleFactor);
+            IGeometry[] geom = SnapClean(geomA, geomB, scaleFactor);
             return geom[0].SymmetricDifference(geom[1]);
         }
 
         public static IGeometry Union(IGeometry geomA, IGeometry geomB, double scaleFactor)
         {
-            IGeometry[] geom = snapClean(geomA, geomB, scaleFactor);
+            IGeometry[] geom = SnapClean(geomA, geomB, scaleFactor);
             return geom[0].Union(geom[1]);
         }
 
-        private static IGeometry[] snapClean(
+        private static IGeometry[] SnapClean(
             IGeometry geomA, IGeometry geomB,
             double scaleFactor)
         {
             IGeometry snapped = SnapRoundFunctions.SnapRound(geomA, geomB, scaleFactor);
-            IGeometry aSnap = clean(snapped.GetGeometryN(0));
-            IGeometry bSnap = clean(snapped.GetGeometryN(1));
+            // TODO: don't need to clean once GeometrySnapRounder ensures all components are valid
+            IGeometry aSnap = Clean(snapped.GetGeometryN(0));
+            IGeometry bSnap = Clean(snapped.GetGeometryN(1));
             return new IGeometry[] { aSnap, bSnap };
         }
 
-        private static IGeometry clean(IGeometry geom)
+        private static IGeometry Clean(IGeometry geom)
         {
             // TODO: only buffer if it is a polygonal IGeometry
             if (!(geom is IPolygonal) ) return geom;
