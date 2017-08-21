e8e85fbef6d09532dacb77dd254d3719aebb526d
Diego Guidi <diegoguidi@gmail.com>
2017-08-21 12:37:35 +0200

    added a test for #180

---
+++ NetTopologySuite.Samples.Console/Tests/Github/Issue180Fixture.cs
@@ -0,0 +1,33 @@
+using GeoAPI.Geometries;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+using NUnit.Framework;
+
+namespace NetTopologySuite.Samples.Tests.Github
+{
+    [TestFixture]
+    public class Issue180Fixture
+    {
+        /// <summary>
+        /// Same results with JTS, see: https://github.com/NetTopologySuite/NetTopologySuite/issues/180
+        /// </summary>
+        [Test]
+        public void expected_no_interception_point()
+        {
+            IGeometryFactory factory = GeometryFactory.Default;
+            WKTReader reader = new WKTReader(factory);
+            IGeometry g1 = reader.Read(
+                @"LINESTRING (500252.36136968032 3268279.9946693764, 500197.63371806522 3268255.4002767489)");
+            Assert.IsNotNull(g1);
+            Assert.IsTrue(g1.IsValid);
+            IGeometry g2 = reader.Read(
+                @"LINESTRING (499815.091 3269179.8250000011, 500224.99754436983 3268267.6974732862)");
+            Assert.IsNotNull(g2);
+            Assert.IsTrue(g2.IsValid);
+            IGeometry ret = g1.Intersection(g2);
+            Assert.IsNotNull(ret);
+            Assert.IsInstanceOf<ILineString>(ret);
+            Assert.AreEqual("LINESTRING EMPTY", ret.ToString());
+        }
+    }
+}
