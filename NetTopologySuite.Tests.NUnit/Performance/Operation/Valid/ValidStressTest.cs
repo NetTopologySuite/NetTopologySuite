241ee41b565454a4940a6c7ff8b460aea7a05ac9
FObermaier <felix.obermaier@netcologne.de>
2017-10-12 10:09:21 +0200

    Refactored test code JTS' commit 8215e0cf1de7b9fab91017f7233f02c4e7c71d8e

--- NetTopologySuite.Tests.NUnit/Performance/Operation/Valid/ValidStressTest.cs
+++ NetTopologySuite.Tests.NUnit/Performance/Operation/Valid/ValidStressTest.cs
@@ -1,40 +1,48 @@
-﻿
-using System;
+﻿using System;
 using System.Diagnostics;
 using GeoAPI.Geometries;
 using NetTopologySuite.Geometries;
 using NetTopologySuite.Geometries.Utilities;
+using NetTopologySuite.Operation.Valid;
 using NUnit.Framework;
+
 namespace NetTopologySuite.Tests.NUnit.Performance.Operation.Valid
 {
-/**
- * Stress-tests {@link IsValidOp} 
- * on invalid polygons with many intersections.
- * 
- * @author mdavis
- *
- */
-
+    /// <summary>
+    /// Stress-tests <see cref="IsValidOp"/> on invalid polygons with many intersections.
+    /// by running it on an invalid MultiPolygon with many intersections.
+    /// In NTS 1.14 and earlier this takes a very long time to run,
+    /// since all intersections are computed before the invalid result is returned.
+    /// In fact it is only necessary to detect a single intersection in order
+    /// to determine invalidity.
+    /// </summary>
+    /// <author>mdavis</author>
     public class ValidStressTest
     {
 
         public static int SIZE = 10000;
 
+        private static readonly GeometryFactory geomFact = new GeometryFactory();
+
         [Test,Ignore("Runs without end")]
         public void run()
         {
             var env = new Envelope(0, 100, 0, 100);
-            var comb1 = Comb(env, SIZE);
-            var centre = env.Centre;
-            var trans = AffineTransformation.RotationInstance(0.5*Math.PI, centre.X, centre.Y);
-            var comb2 = (IPolygon) trans.Transform(comb1);
-            var mp = GeomFact.CreateMultiPolygon(new[] {comb1, comb2});
+            var mp = CrossingCombs(env);
             //System.out.println(mp);
             var isValid = mp.IsValid;
             Debug.WriteLine("Is Valid = " + isValid);
         }
 
-        static readonly GeometryFactory GeomFact = new GeometryFactory();
+        private IMultiPolygon CrossingCombs(Envelope env)
+        {
+            var comb1 = Comb(env, SIZE);
+            var centre = env.Centre;
+            var trans = AffineTransformation.RotationInstance(0.5 * Math.PI, centre.X, centre.Y);
+            var comb2 = (IPolygon)trans.Transform(comb1);
+            var mp = geomFact.CreateMultiPolygon(new [] { comb1, comb2 });
+            return mp;
+        }
 
         static IPolygon Comb(Envelope env, int nArms)
         {
