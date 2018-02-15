3068dc67116adc71c25a60977cfb12f57182d70e
FObermaier <felix.obermaier@netcologne.de>
2018-02-15 09:51:54 +0100

    Improve handling of Quadtree queries with null Envelope JTS' commit 0f36b3950eac3f9412bd816168f5f289bc5b37c0

---
+++ NetTopologySuite.Tests.NUnit/Index/Quadtree/QuadtreeTest.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Copyright (c) 2016 Martin Davis.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
+
+using GeoAPI.Geometries;
+using NetTopologySuite.Index.Quadtree;
+using NUnit.Framework;
+
+namespace NetTopologySuite.Tests.NUnit.Index.Quadtree
+{
+    public class QuadtreeTest
+    {
+        [Test]
+        public void TestNullQuery()
+        {
+            var qt = new Quadtree<string>();
+            var result1 = qt.Query(null);
+            Assert.That(result1.Count, Is.EqualTo(0));
+
+            qt.Insert(new Envelope(0, 10, 0, 10), "some data");
+            var result2 = qt.Query(null);
+            Assert.That(result2.Count, Is.EqualTo(0));
+        }
+    }
+}
