70054af8452c6bc1d17391a98564012cae878a2a
Joe Amenta <airbreather@linux.com>
2018-05-05 12:05:07 -0400

    Comment to explain this apparently uncalled code

--- NetTopologySuite/NetTopologySuiteBootstrapper.cs
+++ NetTopologySuite/NetTopologySuiteBootstrapper.cs
@@ -19,6 +19,9 @@ namespace GeoAPI
 
         internal static class ModuleInitializer
         {
+            // this is a method with a special name in a class with a special name; we don't call it
+            // directly. Instead, ModuleInit.Fody recognizes this and adds a module initializer that
+            // calls this method when NTS is loaded, before anything else of ours can run.
             public static void Initialize() => GeometryServiceProvider.SetInstanceIfNotAlreadySetDirectly(NetTopologySuite.NtsGeometryServices.Instance);
         }
     }
