f843cb01432988a172f7314dc6d6c652709516ff
Joe Amenta <airbreather@linux.com>
2018-05-06 19:06:03 -0400

    Better ignore reason for these ones.

--- NetTopologySuite.Tests.Vivid.XUnit/XUnitGeneralTests.cs
+++ NetTopologySuite.Tests.Vivid.XUnit/XUnitGeneralTests.cs
@@ -15,49 +15,49 @@ namespace NetTopologySuite.Tests.XUnit
 
         protected override string TestLocation { get { return testLocation; } }
 
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test00() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test01() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test02() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test03() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test04() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test05() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test06() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test07() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test08() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test09() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test10() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test11() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test12() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test13() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test14() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test15() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test16() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test17() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test18() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test19() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void Test20() { }
-        [NUnit.Framework.Ignore(reason: "FIXME: IgnoreAttribute didn't require a reason when this was written, but now it does.")]
+        [NUnit.Framework.Ignore(reason: "There are more than 20 files, so these all get handled by TestAllFiles anyway.")]
         public override void TestCountOk() { }
 
         [NUnit.Framework.Test]
