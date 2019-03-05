7b6fae5c9b7253c277c0cca4f12148a6d3df082e
Joe Amenta <airbreather@linux.com>
2019-03-05 08:57:08 -0500

    Generate assembly attributes via MSBuild. Bump up the version number to 2.x. Include the JTS compatibility level in the assembly informational version. Move AssemblyInfo.cs files out from "Properties" subfolders. Delete things from AssemblyInfo.cs that are now auto-generated. Condense a bit of copy-paste in NetTopologySuite.Samples.Console.csproj.
    Related to #244 and #298

---
+++ NetTopologySuite.Samples.Console/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM componenets.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a3029beb-2e42-4123-ac49-6e099bd6ce55")]
---
+++ NetTopologySuite/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDelaySign(false)]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("6B7EB658-792E-4178-B853-8AEB851513A9")]
+
+[assembly: InternalsVisibleTo("NetTopologySuite.Tests.NUnit, PublicKey=" + Consts.PublicKeyToken)]
