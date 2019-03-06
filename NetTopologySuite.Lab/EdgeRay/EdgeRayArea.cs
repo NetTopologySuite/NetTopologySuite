bafaa7859a17fa9cda7e41095d376c4c2ad4a9d8
Joe Amenta <airbreather@linux.com>
2019-03-06 05:43:47 -0500

    Add Lab EdgeRay package
    JTS' commit
    locationtech/jts@6ffaebd
    
    NTS-specific notes:
    - Some static methods changed from X() to GetX() so that when there's a property-like getter method in JTS, NTS can make it an X property.

---
+++ NetTopologySuite.Lab/EdgeRay/EdgeRayArea.cs
@@ -0,0 +1,51 @@
+﻿using GeoAPI.Geometries;
+using NetTopologySuite.Algorithm;
+
+namespace NetTopologySuite.EdgeRay
+{
+    public class EdgeRayArea
+    {
+        public static double GetArea(IGeometry geom)
+        {
+            var area = new EdgeRayArea(geom);
+            return area.Area;
+        }
+
+        private readonly IGeometry _geom;
+
+        public EdgeRayArea(IGeometry geom)
+        {
+            _geom = geom;
+        }
+
+        public double Area
+        {
+            get
+            {
+                var poly = (IPolygon)_geom;
+                var seq = poly.ExteriorRing.CoordinateSequence;
+                bool isCW = !Orientation.IsCCW(seq);
+
+                // TODO: for now assume poly is CW
+
+                // scan every segment
+                double area = 0;
+                for (int i = 1; i < seq.Count; i++)
+                {
+                    int i0 = i - 1;
+                    int i1 = i;
+                    /*
+                    area += EdgeRay.SreaTermBoth(seq.GetX(i0), seq.GetY(i0),
+                        seq.GetX(i1), seq.GetY(i1));
+                        */
+                    area += EdgeRay.AreaTerm(seq.GetX(i0), seq.GetY(i0),
+                        seq.GetX(i1), seq.GetY(i1), isCW);
+                    area += EdgeRay.AreaTerm(seq.GetX(i1), seq.GetY(i1),
+                        seq.GetX(i0), seq.GetY(i0), !isCW);
+                }
+
+                return area;
+            }
+        }
+    }
+}
---
+++ NetTopologySuite.Lab/EdgeRay/EdgeRayFunctions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using GeoAPI.Geometries;
+
+namespace NetTopologySuite.EdgeRay
+{
+    public static class EdgeRayFunctions
+    {
+        public static double GetArea(IGeometry g)
+        {
+            return EdgeRayArea.GetArea(g);
+        }
+
+        public static double GetIntersectionArea(IGeometry geom0, IGeometry geom1)
+        {
+            var area = new EdgeRayIntersectionArea(geom0, geom1);
+            return area.Area;
+        }
+
+        public static double CheckIntersectionArea(IGeometry geom0, IGeometry geom1)
+        {
+            double intArea = GetIntersectionArea(geom0, geom1);
+
+            double intAreaStd = geom0.Intersection(geom1).Area;
+
+            double diff = Math.Abs(intArea - intAreaStd) / Math.Max(intArea, intAreaStd);
+
+            return diff;
+        }
+    }
+}
