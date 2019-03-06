ffbf59df8597a10175556d71321f3a7cf18c9140
Joe Amenta <airbreather@linux.com>
2019-03-06 07:21:19 -0500

    Add Variable Width Buffer functions in Lab
    JTS' commit
    locationtech/jts@df19fc7

---
+++ NetTopologySuite.Lab/Operation/Buffer/BufferFunctions.cs
@@ -0,0 +1,17 @@
+﻿using GeoAPI.Geometries;
+
+namespace NetTopologySuite.Operation.Buffer
+{
+    public static class BufferFunctions
+    {
+        /// <summary>
+        /// Buffer a line by a width varying along the line.
+        /// </summary>
+        /// <param name="startWidth">Start width.</param>
+        /// <param name="endWidth">End width.</param>
+        public static IGeometry BufferVariableWidth(ILineString line, double startWidth, double endWidth)
+        {
+            return VariableWidthBuffer.Buffer(line, startWidth, endWidth);
+        }
+    }
+}
