b773e84984a8bb7a811b7894efacf72f8648a0d1
Joe Amenta <airbreather@linux.com>
2019-03-07 06:23:35 -0500

    Generate assembly attributes via MSBuild. (#299)
    Bump up the version number to 2.x.
    Include the JTS compatibility level in the assembly informational version.
    Move AssemblyInfo.cs files out from "Properties" subfolders.
    Delete things from AssemblyInfo.cs that are now auto-generated.
    Condense a bit of copy-paste in NetTopologySuite.Samples.Console.csproj.
    
    Related to #244
    Resolves #298

---
+++ NetTopologySuite.Lab/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.InteropServices;
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d446fba3-b6ea-4feb-9615-d5fd5aeb3d23")]
