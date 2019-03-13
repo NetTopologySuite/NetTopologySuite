43c4e5c9f84e269345d548980182ee2433c5caf2
Joe Amenta <airbreather@linux.com>
2019-03-13 08:47:45 -0400

    Add TestBuilder Compute over Collection button
    JTS' commit
    locationtech/jts@869b3b2
    
    NTS notes:
    - This commit was mostly nothing relevant to us, but we do happen to have a home for the functions.

---
+++ NetTopologySuite.TestRunner/Functions/SpreaderGeometryFunction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using GeoAPI.Geometries;
+
+namespace Open.Topology.TestRunner.Functions
+{
+    public class SpreaderGeometryFunction : IGeometryFunction
+    {
+        private readonly IGeometryFunction _fun;
+
+        public SpreaderGeometryFunction(IGeometryFunction fun)
+        {
+            _fun = fun;
+        }
+
+        public string Category => _fun.Category;
+
+        public string Name => _fun.Name;
+
+        public string[] ParameterNames => _fun.ParameterNames;
+
+        public Type[] ParameterTypes => _fun.ParameterTypes;
+
+        public Type ReturnType => _fun.ReturnType;
+
+        public string Signature => _fun.Signature;
+
+        public bool IsBinary => _fun.IsBinary;
+
+        public object Invoke(IGeometry geom, object[] args)
+        {
+            var results = new IGeometry[geom.NumGeometries];
+            for (int i = 0; i < results.Length; i++)
+            {
+                var elt = geom.GetGeometryN(i);
+                var result = (IGeometry)_fun.Invoke(elt, args);
+                ////FunctionsUtil.showIndicator(result);
+                results[i] = result;
+            }
+
+            return geom.Factory.CreateGeometryCollection(results);
+        }
+    }
+}
