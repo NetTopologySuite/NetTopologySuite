818ce13945de4f1ed32997c077fad1f1a763449a
Joe Amenta <airbreather@linux.com>
2019-05-11 11:35:00 -0400

    Add EnvelopeCombiner. This is an analog for MSSQL's EnvelopeAggregate function.
    Part of #246; the rest will come from NetTopologySuite/GeoAPI#66.

---
+++ NetTopologySuite.Tests.NUnit/Geometries/Utility/EnvelopeCombinerTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using NetTopologySuite.Geometries;
+using NetTopologySuite.Geometries.Utilities;
+using NetTopologySuite.Shape.Random;
+
+using NUnit.Framework;
+
+namespace NetTopologySuite.Tests.NUnit.Geometries.Utility
+{
+    public class EnvelopeCombinerTests
+    {
+        private static readonly GeometryFactory Factory = GeometryFactory.Default;
+
+        public static List<Geometry>[] EnvelopeCombinerTestCases =
+        {
+            null,
+
+            new List<Geometry>(),
+
+            new List<Geometry>
+            {
+                null,
+                null,
+                null,
+                null,
+            },
+
+            new List<Geometry>
+            {
+                Factory.CreatePoint(),
+                null,
+                Factory.CreatePolygon(),
+            },
+
+            new List<Geometry>
+            {
+                Factory.CreatePoint(new Coordinate(1, 1)),
+                null,
+                null,
+            },
+
+            new List<Geometry>
+            {
+                null,
+                Factory.ToGeometry(new Envelope(1, 2, 3, 4)),
+                null,
+                null,
+                null,
+                null,
+                null,
+            },
+
+            new List<Geometry>
+            {
+                Factory.ToGeometry(new Envelope(1, 2, 3, 4)),
+                null,
+                Factory.ToGeometry(new Envelope(4, 3, 2, 1)),
+            },
+
+            new List<Geometry>
+            {
+                new RandomPointsBuilder(Factory) { NumPoints = 3 }.GetGeometry(),
+                new RandomPointsBuilder(Factory) { NumPoints = 3 }.GetGeometry(),
+                new RandomPointsBuilder(Factory) { NumPoints = 3 }.GetGeometry(),
+            },
+        };
+
+        [TestCaseSource(nameof(EnvelopeCombinerTestCases))]
+        public void TestEnvelopeCombine(ICollection<Geometry> geoms)
+        {
+            var actual = EnvelopeCombiner.Combine(geoms);
+
+            // JTS doesn't usually bother doing anything special about nulls,
+            // so our ports of their stuff will suffer the same.
+            geoms = geoms?.Where(g => g != null).ToArray() ?? Array.Empty<Geometry>();
+
+            var combinedGeometry = GeometryCombiner.Combine(geoms);
+
+            // JTS also doesn't fear giving us nulls back from its algorithms.
+            var expected = combinedGeometry?.EnvelopeInternal ?? new Envelope();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ NetTopologySuite/Geometries/Utilities/EnvelopeCombiner.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NetTopologySuite.Geometries.Utilities
+{
+    /// <summary>
+    /// Utility to combine just the <see cref="Envelope"/>s of a list of geometries.
+    /// </summary>
+    public sealed class EnvelopeCombiner
+    {
+        /// <summary>
+        /// Gets the smallest <see cref="Envelope"/> within which all input geometries fit, or a
+        /// <see cref="Envelope.IsNull">null</see> envelope if no non-empty geometries were found in
+        /// the input list.
+        /// </summary>
+        /// <param name="geoms">
+        /// The list of input geometries.
+        /// </param>
+        /// <returns>
+        /// The smallest <see cref="Envelope"/> within which all input geometries fit, or a
+        /// <see cref="Envelope.IsNull">null</see> envelope if no non-empty geometries were found in
+        /// the input list.
+        /// </returns>
+        public static Envelope Combine(params Geometry[] geoms) => new EnvelopeCombiner(geoms).Combine();
+
+        /// <summary>
+        /// Gets the smallest <see cref="Envelope"/> within which all input geometries fit, or a
+        /// <see cref="Envelope.IsNull">null</see> envelope if no non-empty geometries were found in
+        /// the input list.
+        /// </summary>
+        /// <param name="geoms">
+        /// The list of input geometries.
+        /// </param>
+        /// <returns>
+        /// The smallest <see cref="Envelope"/> within which all input geometries fit, or a
+        /// <see cref="Envelope.IsNull">null</see> envelope if no non-empty geometries were found in
+        /// the input list.
+        /// </returns>
+        public static Envelope Combine(IEnumerable<Geometry> geoms) => new EnvelopeCombiner(geoms).Combine();
+
+        private readonly Geometry[] _geoms;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnvelopeCombiner"/> class.
+        /// </summary>
+        /// <param name="geoms">
+        /// The <see cref="Geometry"/> instances to combine.
+        /// </param>
+        public EnvelopeCombiner(params Geometry[] geoms)
+        {
+            _geoms = geoms ?? Array.Empty<Geometry>();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnvelopeCombiner"/> class.
+        /// </summary>
+        /// <param name="geoms">
+        /// The <see cref="Geometry"/> instances to combine.
+        /// </param>
+        public EnvelopeCombiner(IEnumerable<Geometry> geoms)
+        {
+            switch (geoms)
+            {
+                case null:
+                    _geoms = Array.Empty<Geometry>();
+                    break;
+
+                case Geometry[] geomArray:
+                    _geoms = geomArray;
+                    break;
+
+                default:
+                    _geoms = geoms.ToArray();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Gets the smallest <see cref="Envelope"/> within which all input geometries fit, or a
+        /// <see cref="Envelope.IsNull">null</see> envelope if no non-empty geometries were found in
+        /// the input list.
+        /// </summary>
+        /// <returns>
+        /// The smallest <see cref="Envelope"/> within which all input geometries fit, or a
+        /// <see cref="Envelope.IsNull">null</see> envelope if no non-empty geometries were found in
+        /// the input list.
+        /// </returns>
+        public Envelope Combine()
+        {
+            var result = new Envelope();
+            foreach (var geom in _geoms)
+            {
+                if (geom is null)
+                {
+                    continue;
+                }
+
+                result.ExpandToInclude(geom.EnvelopeInternal);
+            }
+
+            return result;
+        }
+    }
+}
