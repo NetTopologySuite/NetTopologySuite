3f2f86513cf3d14a8efc5ef9f428bc6600f429d0
Joe Amenta <airbreather@linux.com>
2019-06-12 07:26:49 -0400

    Extract this test case source out.

--- NetTopologySuite.Tests.NUnit/Geometries/Utility/EnvelopeCombinerTests.cs
+++ NetTopologySuite.Tests.NUnit/Geometries/Utility/EnvelopeCombinerTests.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 using NetTopologySuite.Geometries;
 using NetTopologySuite.Geometries.Utilities;
-using NetTopologySuite.Shape.Random;
+using NetTopologySuite.Tests.NUnit.Utilities;
 
 using NUnit.Framework;
 
@@ -12,63 +12,7 @@ namespace NetTopologySuite.Tests.NUnit.Geometries.Utility
 {
     public class EnvelopeCombinerTests
     {
-        private static readonly GeometryFactory Factory = GeometryFactory.Default;
-
-        public static List<Geometry>[] EnvelopeCombinerTestCases =
-        {
-            null,
-
-            new List<Geometry>(),
-
-            new List<Geometry>
-            {
-                null,
-                null,
-                null,
-                null,
-            },
-
-            new List<Geometry>
-            {
-                Factory.CreatePoint(),
-                null,
-                Factory.CreatePolygon(),
-            },
-
-            new List<Geometry>
-            {
-                Factory.CreatePoint(new Coordinate(1, 1)),
-                null,
-                null,
-            },
-
-            new List<Geometry>
-            {
-                null,
-                Factory.ToGeometry(new Envelope(1, 2, 3, 4)),
-                null,
-                null,
-                null,
-                null,
-                null,
-            },
-
-            new List<Geometry>
-            {
-                Factory.ToGeometry(new Envelope(1, 2, 3, 4)),
-                null,
-                Factory.ToGeometry(new Envelope(4, 3, 2, 1)),
-            },
-
-            new List<Geometry>
-            {
-                new RandomPointsBuilder(Factory) { NumPoints = 3 }.GetGeometry(),
-                new RandomPointsBuilder(Factory) { NumPoints = 3 }.GetGeometry(),
-                new RandomPointsBuilder(Factory) { NumPoints = 3 }.GetGeometry(),
-            },
-        };
-
-        [TestCaseSource(nameof(EnvelopeCombinerTestCases))]
+        [TestCaseSource(typeof(PointwiseGeometryAggregationTestCases))]
         public void TestEnvelopeCombine(ICollection<Geometry> geoms)
         {
             var actual = EnvelopeCombiner.Combine(geoms);
---
+++ NetTopologySuite.Tests.NUnit/Utilities/PointwiseGeometryAggregationTestCases.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using NetTopologySuite.Geometries;
+using NetTopologySuite.Shape.Random;
+
+using NUnit.Framework;
+
+namespace NetTopologySuite.Tests.NUnit.Utilities
+{
+    /// <summary>
+    /// Test cases for geometry aggregation methods that depend only on the points of the underlying
+    /// geometries rather than their actual topology.
+    /// </summary>
+    public sealed class PointwiseGeometryAggregationTestCases : IEnumerable
+    {
+        public IEnumerator GetEnumerator() => GetTestCases().Select(tup => new TestCaseData(tup.geoms?.ToList()).SetName($"{{c}}.{{m}}: {tup.testCaseName}")).GetEnumerator();
+
+        private static IEnumerable<(string testCaseName, Geometry[] geoms)> GetTestCases()
+        {
+            var factory = NtsGeometryServices.Instance.CreateGeometryFactory();
+
+            yield return ("Null", null);
+
+            yield return ("Empty", Array.Empty<Geometry>());
+
+            yield return ("Contains only nulls", new Geometry[4]);
+
+            yield return ("Contains empties and nulls", new Geometry[]
+            {
+                factory.CreatePoint(),
+                null,
+                factory.CreatePolygon(),
+            });
+
+            yield return ("One point", new Geometry[]
+            {
+                factory.CreatePoint(new Coordinate(1, 1)),
+                null,
+                null,
+            });
+
+            yield return ("Four points in one polygon", new Geometry[]
+            {
+                null,
+                factory.ToGeometry(new Envelope(1, 2, 3, 4)),
+                null,
+                null,
+                null,
+                null,
+                null,
+            });
+
+            yield return ("Eight points in two polygons", new Geometry[]
+            {
+                factory.ToGeometry(new Envelope(1, 2, 3, 4)),
+                null,
+                factory.ToGeometry(new Envelope(4, 3, 2, 1)),
+            });
+
+            yield return ("Nine random points in three polygons", new Geometry[]
+            {
+                new RandomPointsBuilder(factory) { NumPoints = 3 }.GetGeometry(),
+                new RandomPointsBuilder(factory) { NumPoints = 3 }.GetGeometry(),
+                new RandomPointsBuilder(factory) { NumPoints = 3 }.GetGeometry(),
+            });
+        }
+    }
+}
