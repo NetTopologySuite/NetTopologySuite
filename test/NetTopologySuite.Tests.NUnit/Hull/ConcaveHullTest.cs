0541fc2df4a51c088e273efc0778ebd219a8d86b
Jeroen Bloemscheer <JBloemscheer@tensing.com>
2019-09-06 11:36:28 +0200

    Added testcase TestTurf

--- test/NetTopologySuite.Tests.NUnit/Hull/ConcaveHullTest.cs
+++ test/NetTopologySuite.Tests.NUnit/Hull/ConcaveHullTest.cs
@@ -12,10 +12,21 @@ namespace NetTopologySuite.Tests.NUnit.Hull
         {
             CheckHull(
                 "POLYGON ((100 200, 200 180, 300 200, 200 190, 100 200))",
-                10,
+                150,
                 "POLYGON ((100 200, 200 180, 300 200, 200 190, 100 200))"
                 );
         }
+
+        [Test, Ignore("Not verified test case.")]
+        public void TestTurf()
+        {
+            CheckHull(
+                "LINESTRING (-63.601226 44.642643, -63.591442 44.651436, -63.580799 44.648749, -63.573589 44.641788, -63.587665 44.64533, -63.595218 44.64765)",
+                -1,
+                "POLYGON ((-63.601226 44.642643, -63.587665 44.64533, -63.573589 44.641788, -63.580799 44.648749, -63.591442 44.651436, -63.601226 44.642643))"
+                );
+        }
+
 
         private void CheckHull(string inputWKT, double tolerance, string expectedWKT)
         {
