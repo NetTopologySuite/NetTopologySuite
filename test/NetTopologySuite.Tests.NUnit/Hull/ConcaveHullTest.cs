f4f09b002480d1a04748f17535e0dbe4317cc5ab
Joe Amenta <airbreather@linux.com>
2019-10-04 07:12:03 -0400

    Revert some changes, for the sake of history. - QuadEdgeSubdivision: revert whitespace-only change - Restore the old ConcaveHull, since it's still like that in JTS

---
+++ test/NetTopologySuite.Tests.NUnit/Hull/ConcaveHullTest.cs
@@ -0,0 +1,28 @@
+﻿using NetTopologySuite.Hull;
+using NUnit.Framework;
+
+namespace NetTopologySuite.Tests.NUnit.Hull
+{
+    public class ConcaveHullTest : GeometryTestCase
+    {
+
+        [Test, Ignore("Incomplete")]
+        public void TestSimple()
+        {
+            CheckHull(
+                "POLYGON ((100 200, 200 180, 300 200, 200 190, 100 200))",
+                150,
+                "POLYGON ((100 200, 200 180, 300 200, 200 190, 100 200))"
+                );
+        }
+
+        private void CheckHull(string inputWKT, double tolerance, string expectedWKT)
+        {
+            var input = Read(inputWKT);
+            var expected = Read(expectedWKT);
+            var hull = new ConcaveHull(input, tolerance);
+            var actual = hull.GetResult();
+            CheckEqual(expected, actual);
+        }
+    }
+}
