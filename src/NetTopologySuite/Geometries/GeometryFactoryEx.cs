83a9da559f2264eb291d7dd06c39f200c87cbb5b
FObermaier <felix.obermaier@netcologne.de>
2019-10-30 08:33:18 +0100

    Extended GeometryFactory to enforce polyogn ring orientation
    * Add GeometryFactoryEx
    * Mark OgcCompiantGeometryFactory obsolete
    * Add unit tests
    
    relates to #360

---
+++ src/NetTopologySuite/Geometries/GeometryFactoryEx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace NetTopologySuite.Geometries
+{
+    /// <summary>
+    /// An extended <see cref="GeometryFactory"/> that is capable of enforcing a ring orientation for polygons.
+    /// </summary>
+    public class GeometryFactoryEx : GeometryFactory
+    {
+        /// <summary>
+        /// Gets the default polygon shell ring orientation that is used when nothing else has been set.
+        /// </summary>
+        private const LinearRingOrientation DefaultRingOrientation = LinearRingOrientation.Default;
+
+        /// <summary>
+        /// The polygon shell ring orientation enforced by this factory
+        /// </summary>
+        private LinearRingOrientation? _polygonShellRingOrientation;
+
+        /// <summary>
+        /// Gets or sets a value indicating the ring orientation of the
+        /// <c>Polygon</c>'s exterior rings.<para>
+        /// If its value is <see cref="LinearRingOrientation.DontCare"/>, this
+        /// factory behaves just like the base <see cref="GeometryFactory"/>.
+        /// </para>
+        /// </summary>
+        /// <remarks>
+        /// The setter of this property has to be used prior to any call
+        /// to <c>CreatePolygon</c> or <c>"CreateMultiPolygon</c></remarks>
+        /// <seealso cref="GeometryFactory.CreatePolygon(Coordinate[])"/>
+        /// <seealso cref="GeometryFactory.CreatePolygon(CoordinateSequence)"/>
+        /// <seealso cref="GeometryFactory.CreatePolygon(LinearRing)"/>
+        /// <seealso cref="GeometryFactory.CreatePolygon(LinearRing, LinearRing[])"/>
+        /// <seealso cref="GeometryFactory.CreateMultiPolygon(Polygon[])"/>
+        public LinearRingOrientation OrientationOfExteriorRing
+        {
+            get => _polygonShellRingOrientation ?? (_polygonShellRingOrientation = DefaultRingOrientation).Value;
+            set
+            {
+                if (_polygonShellRingOrientation.HasValue && _polygonShellRingOrientation.Value != value)
+                    throw new InvalidOperationException();
+                if ((int)value < -1 || (int)value > 1)
+                    throw new ArgumentOutOfRangeException();
+
+                _polygonShellRingOrientation = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating the ring orientation for the interior rings
+        /// </summary>
+        /// <remarks>
+        /// This value is always opposite of <see cref="OrientationOfExteriorRing"/>,
+        /// except when its value is <see cref="LinearRingOrientation.DontCare"/>.
+        /// </remarks>
+        private LinearRingOrientation OrientationOfInteriorRings
+        {
+            get
+            {
+                switch (OrientationOfExteriorRing)
+                {
+                    case LinearRingOrientation.CCW:
+                        return LinearRingOrientation.CW;
+                    case LinearRingOrientation.CW:
+                        return LinearRingOrientation.CCW;
+                    default:
+                        return LinearRingOrientation.DontCare;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Constructs a <c>Polygon</c> with the given exterior boundary and
+        /// interior boundaries.
+        /// <para/>
+        /// The <see cref="OrientationOfExteriorRing"/> is enforced on the constructed polygon.
+        /// </summary>
+        /// <param name="shell">
+        /// The outer boundary of the new <c>Polygon</c>, or
+        /// <c>null</c> or an empty <c>LinearRing</c> if
+        /// the empty point is to be created.
+        /// </param>
+        /// <param name="holes">
+        /// The inner boundaries of the new <c>Polygon</c>, or
+        /// <c>null</c> or empty <c>LinearRing</c> s if
+        /// the empty point is to be created.
+        /// </param>
+        /// <returns>A <see cref="Polygon"/> object</returns>
+        public override Polygon CreatePolygon(LinearRing shell, LinearRing[] holes)
+        {
+            shell = EnsureOrientation(shell, OrientationOfExteriorRing);
+            if (holes != null && holes.Length > 0)
+            {
+                var ringOrientation = OrientationOfInteriorRings;
+                for (int i = 0; i < holes.Length; i++)
+                    holes[i] = EnsureOrientation(holes[i], ringOrientation);
+            }
+            return new Polygon(shell, holes, this);
+        }
+
+        /// <summary>
+        /// Creates a <c>MultiPolygon</c> using the given <c>Polygons</c>; a null or empty array
+        /// will create an empty Polygon. The polygons must conform to the
+        /// assertions specified in the <see href="http://www.opengis.org/techno/specs.htm"/> OpenGIS Simple Features
+        /// Specification for SQL.<para/>
+        /// The <see cref="OrientationOfExteriorRing"/> is enforced on each polygon.
+        /// </summary>
+        /// <param name="polygons">Polygons, each of which may be empty but not null.</param>
+        /// <returns>A <see cref="MultiPolygon"/> object</returns>
+        public override MultiPolygon CreateMultiPolygon(Polygon[] polygons)
+        {
+            if (polygons == null || polygons.Length == 0)
+                return CreateMultiPolygon();
+
+            // Check if polygons were created with a ring orientation enforcing factory and that the ring
+            // orientation matches that of this one.
+            if (polygons[0]?.Factory is GeometryFactoryEx gfe && gfe.OrientationOfExteriorRing == OrientationOfExteriorRing)
+                return new MultiPolygon(polygons, this);
+
+            for (int i = 0; i < polygons.Length; i++)
+                polygons[i] = CreatePolygon(polygons[i].Shell, polygons[i].Holes);
+
+            return new MultiPolygon(polygons, this);
+        }
+
+        /// <summary>
+        /// Utility function to enforce a specific ring orientation on a linear ring
+        /// </summary>
+        /// <param name="ring">The ring</param>
+        /// <param name="ringOrientation">The required orientation</param>
+        /// <returns>A ring</returns>
+        private static LinearRing EnsureOrientation(LinearRing ring, LinearRingOrientation ringOrientation)
+        {
+            if (ringOrientation == LinearRingOrientation.DontCare)
+                return ring;
+
+            if (ringOrientation == LinearRingOrientation.CCW && !ring.IsCCW ||
+                ringOrientation == LinearRingOrientation.CW && ring.IsCCW)
+                ring = (LinearRing)ring.Reverse();
+
+            return ring;
+        }
+    }
+}
---
+++ test/NetTopologySuite.Tests.NUnit/Geometries/GeometryFactoryExTest.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using NetTopologySuite.Algorithm;
+using NetTopologySuite.Geometries;
+using NUnit.Framework;
+
+namespace NetTopologySuite.Tests.NUnit.Geometries
+{
+    public class GeometryFactoryExTest : GeometryFactoryTest
+    {
+        [Test]
+        public void TestShellRingOrientationSetting()
+        {
+
+            // check default
+            var gf = new GeometryFactoryEx();
+            Assert.AreEqual(LinearRingOrientation.CounterClockwise, gf.OrientationOfExteriorRing);
+
+            // Check for valid ring orientation values
+            gf = new GeometryFactoryEx();
+            TestShellRingOrientationSetter(gf, LinearRingOrientation.DontCare, 0);
+            gf = new GeometryFactoryEx();
+            TestShellRingOrientationSetter(gf, LinearRingOrientation.Clockwise, 0);
+            gf = new GeometryFactoryEx();
+            TestShellRingOrientationSetter(gf, LinearRingOrientation.RightHandRule, 0);
+            gf = new GeometryFactoryEx();
+            TestShellRingOrientationSetter(gf, LinearRingOrientation.CounterClockwise, 0);
+            gf = new GeometryFactoryEx();
+            TestShellRingOrientationSetter(gf, LinearRingOrientation.LeftHandRule, 0);
+
+            // check for invalid arguments
+            gf = new GeometryFactoryEx();
+            TestShellRingOrientationSetter(gf, (LinearRingOrientation)(-2), 1);
+            TestShellRingOrientationSetter(gf, (LinearRingOrientation)2, 1);
+
+            // check preventation of setting after first usage
+            var ro = gf.OrientationOfExteriorRing;
+            // don't fail if value does not change
+            TestShellRingOrientationSetter(gf, ro, 0);
+            TestShellRingOrientationSetter(gf, LinearRingOrientation.RightHandRule, 2);
+            TestShellRingOrientationSetter(gf, LinearRingOrientation.DontCare, 2);
+        }
+
+        [Test]
+        public void TestShellRingOrientationEnforcement()
+        {
+            Assert.IsTrue(TestShellRingOrientationEnforcement(LinearRingOrientation.DontCare));
+            Assert.IsTrue(TestShellRingOrientationEnforcement(LinearRingOrientation.CounterClockwise));
+            Assert.IsTrue(TestShellRingOrientationEnforcement(LinearRingOrientation.Clockwise));
+        }
+
+        private static bool TestShellRingOrientationEnforcement(LinearRingOrientation ringOrientation)
+        {
+            var gf = new GeometryFactoryEx();
+            gf.OrientationOfExteriorRing = ringOrientation;
+            var cf = gf.CoordinateSequenceFactory;
+
+            var origShellSequence = CreateRing(cf, 0, ringOrientation);
+            var origHolesSequences = new[]
+            {
+                CreateRing(cf, 1, ringOrientation),
+                CreateRing(cf, 2, ringOrientation)
+            };
+
+            var shell = gf.CreateLinearRing(origShellSequence.Copy());
+            var holes = new LinearRing[]
+            {
+                gf.CreateLinearRing(origHolesSequences[0].Copy()),
+                gf.CreateLinearRing(origHolesSequences[1].Copy())
+            };
+
+            var polygon = gf.CreatePolygon(shell, holes);
+            switch (ringOrientation)
+            {
+                case LinearRingOrientation.CW:
+                    if (polygon.Shell.IsCCW)
+                        return false;
+                    if (!polygon.Holes[0].IsCCW)
+                        return false;
+                    if (!polygon.Holes[1].IsCCW)
+                        return false;
+                    break;
+                case LinearRingOrientation.CCW:
+                    if (!polygon.Shell.IsCCW)
+                        return false;
+                    if (polygon.Holes[0].IsCCW)
+                        return false;
+                    if (polygon.Holes[1].IsCCW)
+                        return false;
+                    break;
+                case LinearRingOrientation.DontCare:
+                    if (polygon.Shell.IsCCW != Orientation.IsCCW(origShellSequence))
+                        return false;
+                    if (polygon.Holes[0].IsCCW != Orientation.IsCCW(origHolesSequences[0]))
+                        return false;
+                    if (polygon.Holes[1].IsCCW != Orientation.IsCCW(origHolesSequences[1]))
+                        return false;
+                    break;
+            }
+
+            // Set up a polygon with hole using buffer
+            var pt = new Coordinate(0, 0);
+            var c = gf.CreatePoint(pt);
+            var b = c.Buffer(10d);
+            var s = c.Buffer(6d);
+            var test = (Polygon)b.Difference(s);
+
+            if (ringOrientation == LinearRingOrientation.CW)
+            {
+                if (test.Shell.IsCCW)
+                    return false;
+                if (!test.Holes[0].IsCCW)
+                    return false;
+            }
+            else if (ringOrientation == LinearRingOrientation.CCW)
+            {
+                if (!test.Shell.IsCCW)
+                    return false;
+                if (test.Holes[0].IsCCW)
+                    return false;
+            }
+            return true;
+        }
+
+        private static Coordinate[] CreateCcwRing(int number)
+        {
+            if (number == 0)
+                return new Coordinate[]{
+                    new Coordinate(0, 0), new Coordinate(10, 0),
+                    new Coordinate(10, 10), new Coordinate(0, 10),
+                    new Coordinate(0, 0)};
+            if (number == 1)
+                return new Coordinate[]{
+                    new Coordinate(2, 1), new Coordinate(9, 1),
+                    new Coordinate(9, 8), new Coordinate(2, 1)
+                };
+
+            return new Coordinate[]{
+                new Coordinate(1, 2), new Coordinate(8, 9),
+                new Coordinate(1, 9), new Coordinate(1, 2)
+            };
+        }
+
+        private static bool reverseSequence = false;
+
+        private static CoordinateSequence CreateRing(CoordinateSequenceFactory factory, int ring, LinearRingOrientation orientation)
+        {
+            var coordinates = CreateCcwRing(ring);
+            if (orientation == LinearRingOrientation.Clockwise)
+                CoordinateArrays.Reverse(coordinates);
+            if (orientation == LinearRingOrientation.DontCare)
+            {
+                if (reverseSequence) CoordinateArrays.Reverse(coordinates);
+                reverseSequence = !reverseSequence;
+            }
+
+            return factory.Create(coordinates);
+        }
+
+        private static void TestShellRingOrientationSetter(GeometryFactoryEx gf, LinearRingOrientation ringOrientation, int failKind)
+        {
+            try
+            {
+                gf.OrientationOfExteriorRing = ringOrientation;
+                if (failKind != 0)
+                    Assert.Fail("Setting exterior ring orientation should have failed!");
+                Assert.AreEqual(ringOrientation, gf.OrientationOfExteriorRing);
+            }
+            catch (ArgumentOutOfRangeException iae)
+            {
+                if (failKind != 1)
+                    Assert.Fail("Wrong argument");
+            }
+            catch (InvalidOperationException isa)
+            {
+                if (failKind != 2)
+                    Assert.Fail("Setting exterior ring orientation twice!");
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+    }
+}
