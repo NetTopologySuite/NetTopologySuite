79ab8f85bcf385cd8dc04f9b5c4e5734998e6b96
Felix Obermaier <felix.obermaier@netcologne.de>
2020-03-10 10:14:52 +0100

    Add TestBuilder Each B function
    JTS' commit locationtech/jts@e53855a62712125eb6a18d3dc8ba29502dc53a38

--- test/NetTopologySuite.TestRunner/Functions/SpreaderGeometryFunction.cs
+++ test/NetTopologySuite.TestRunner/Functions/SpreaderGeometryFunction.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 using NetTopologySuite.Geometries;
 using NetTopologySuite.Geometries.Utilities;
 
@@ -8,15 +8,29 @@ namespace Open.Topology.TestRunner.Functions
     public class SpreaderGeometryFunction : IGeometryFunction
     {
         private readonly IGeometryFunction _fun;
+        private readonly bool _isEachA;
+        private readonly bool _isEachB;
 
-        public SpreaderGeometryFunction(IGeometryFunction fun)
+        public SpreaderGeometryFunction(IGeometryFunction fun, bool isEachA, bool isEachB)
         {
             _fun = fun;
+            _isEachA = isEachA;
+            _isEachB = isEachB;
         }
 
         public string Category => _fun.Category;
 
-        public string Name => $"{_fun.Name}-Each";
+        public string Name
+        {
+            get
+            {
+                string name = _fun.Name;
+                if (_isEachA) name += "*A";
+                if (_isEachB) name += "*B";
+
+                return name;
+            }
+        }
 
         public string[] ParameterNames => _fun.ParameterNames;
 
@@ -28,7 +42,7 @@ namespace Open.Topology.TestRunner.Functions
 
         public bool IsBinary => _fun.IsBinary;
 
-        public object Invoke(Geometry geom, object[] args)
+        public object OLDInvoke(Geometry geom, object[] args)
         {
 #if false
             var results = new Geometry[geom.NumGeometries];
@@ -54,5 +68,79 @@ namespace Open.Topology.TestRunner.Functions
             return result;
 #endif
         }
+
+        public object Invoke(Geometry geom, object[] args)
+        {
+            var result = new List<Geometry>();
+            if (_isEachA)
+            {
+                InvokeEachA(geom, args, result);
+            }
+            else
+            {
+                InvokeB(geom, args, result);
+            }
+            return CreateResult(result, geom.Factory);
+        }
+
+        private static object CreateResult(List<Geometry> result, GeometryFactory geometryFactory)
+        {
+            if (result.Count == 1)
+            {
+                return result[0];
+            }
+            var resultGeoms = result.ToArray();
+            return geometryFactory.CreateGeometryCollection(resultGeoms);
+        }
+
+        private void InvokeEachA(Geometry geom, object[] args, List<Geometry> result)
+        {
+            int nElt = geom.NumGeometries;
+            for (int i = 0; i < nElt; i++)
+            {
+                var geomN = geom.GetGeometryN(i);
+                InvokeB(geomN, args, result);
+            }
+        }
+
+        private void InvokeB(Geometry geom, object[] args, List<Geometry> result)
+        {
+            if (HasBGeom(args) && _isEachB)
+            {
+                InvokeEachB(geom, args, result);
+                return;
+            }
+            InvokeFun(geom, args, result);
+        }
+
+        private static bool HasBGeom(object[] args)
+        {
+            if (args.Length <= 0) return false;
+            return args[0] is Geometry;
+        }
+
+        private void InvokeEachB(Geometry geom, object[] args, List<Geometry> result)
+        {
+            var geomB = (Geometry)args[0];
+            object[] argsCopy = (object[])args.Clone();
+            int nElt = geomB.NumGeometries;
+            for (int i = 0; i < nElt; i++)
+            {
+                var geomBN = geomB.GetGeometryN(i);
+                argsCopy[0] = geomBN;
+                InvokeFun(geom, argsCopy, result);
+            }
+        }
+
+        private void InvokeFun(Geometry geom, object[] args, List<Geometry> result)
+        {
+            var resultGeom = (Geometry)_fun.Invoke(geom, args);
+            if (resultGeom == null) return;
+            if (resultGeom.IsEmpty) return;
+            //FunctionsUtil.showIndicator(resultGeom);
+            result.Add(resultGeom);
+        }
+
+
     }
 }
