d4017643e0a8be39b4478b616151bf29f420b31b
Felix Obermaier <felix.obermaier@netcologne.de>
2020-03-12 15:59:33 +0100

    Enhance TestBuilder LinearReferencing function Project to project lines
    JTS' commit locationtech/jts@b8c6e88cd4cf220782c326ae6a48dcdfbc38e068

--- test/NetTopologySuite.TestRunner/Functions/LinearReferencingFunctions.cs
+++ test/NetTopologySuite.TestRunner/Functions/LinearReferencingFunctions.cs
@@ -21,9 +21,21 @@ namespace Open.Topology.TestRunner.Functions
         public static Geometry Project(Geometry g, Geometry g2)
         {
             var ll = new LengthIndexedLine(g);
-            double index = ll.Project(g2.Coordinate);
-            var p = ll.ExtractPoint(index);
-            return g.Factory.CreatePoint(p);
+            if (g2.Dimension == Dimension.Curve)
+            {
+                var line = (LineString)g2.GetGeometryN(0);
+                var pStart = line.GetCoordinateN(0);
+                var pEnd = line.GetCoordinateN(line.NumPoints - 1);
+                double indexStart = ll.Project(pStart);
+                double indexEnd = ll.Project(pEnd);
+                return ll.ExtractLine(indexStart, indexEnd);
+            }
+            else
+            {
+                double index = ll.Project(g2.Coordinate);
+                var p = ll.ExtractPoint(index);
+                return g.Factory.CreatePoint(p);
+            }
         }
         public static double ProjectIndex(Geometry g, Geometry g2)
         {
