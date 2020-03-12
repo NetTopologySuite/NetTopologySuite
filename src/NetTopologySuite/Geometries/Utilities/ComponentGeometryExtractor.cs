ff03a4c6dd598e41818fa6a03b28a1e6e50ad5fa
Felix Obermaier <felix.obermaier@netcologne.de>
2020-03-12 16:03:31 +0100

    Code documentation
    JTS' commit locationtech/jts@05d352741b70c8f454b2ec318d1ba81111915db5

--- src/NetTopologySuite/Geometries/Utilities/ComponentGeometryExtractor.cs
+++ src/NetTopologySuite/Geometries/Utilities/ComponentGeometryExtractor.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 namespace NetTopologySuite.Geometries.Utilities
 {
     /// <summary>
-    /// Extracts a single representative <see cref="Coordinate"/> from each connected component of a <see cref="Geometry"/>.
+    /// Extracts a representative <see cref="Coordinate"/>
+    /// from each connected component of a <see cref="Geometry"/>.
     /// </summary>
     /// <version>1.9</version>
     public class ComponentCoordinateExtracter : IGeometryComponentFilter
     {
         /// <summary>
-        /// Extracts the linear components from a single geometry.
+        /// Extracts a representative <see cref="Coordinate"/>
+        /// from each connected component in a geometry.
+        /// <para/>
         /// If more than one geometry is to be processed, it is more
-        /// efficient to create a single <see cref="ComponentCoordinateExtracter"/> instance
-        /// and pass it to multiple geometries.
+        /// efficient to create a single <see cref="ComponentCoordinateExtracter"/>
+        /// instance and pass it to each geometry.
         /// </summary>
         /// <param name="geom">The Geometry from which to extract</param>
-        /// <returns>A list of Coordinates</returns>
+        /// <returns>A list of representative Coordinates</returns>
         public static List<Coordinate> GetCoordinates(Geometry geom)
         {
             var coords = new List<Coordinate>();
