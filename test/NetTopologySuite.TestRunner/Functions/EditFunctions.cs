a016e6d97a7d5227f5bf4260ccecc1db029bc743
Felix Obermaier <felix.obermaier@netcologne.de>
2020-03-18 10:21:21 +0100

    Add TestBuilder EditFunctions
    JTS' commit locationtech/jts@37a494ca64b76ae3d8725b548b818769e7c798aa

---
+++ test/NetTopologySuite.TestRunner/Functions/EditFunctions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NetTopologySuite.Geometries;
+
+namespace Open.Topology.TestRunner.Functions
+{
+    public class EditFunctions
+    {
+        public static Geometry AddHole(Geometry polyGeom, Geometry hole)
+        {
+            var factory = polyGeom.Factory;
+
+            // input checks
+            if (!(polyGeom is Polygon polygon))
+                throw new ArgumentException("A is not a polygon");
+            if (!(hole is Polygon || hole is LineString))
+             throw new ArgumentException("B must be a polygon or line");
+
+            var holePts = ExtractLine(hole);
+            if (!CoordinateArrays.IsRing(holePts))
+            {
+                throw new ArgumentException("B is not a valid ring");
+            }
+
+            var shell = (LinearRing)polygon.ExteriorRing.Copy();
+
+            var holes = new LinearRing[polygon.NumInteriorRings + 1];
+            for (int i = 0; i < polygon.NumInteriorRings; i++)
+            {
+                holes[i] = (LinearRing)polygon.GetInteriorRingN(i).Copy();
+            }
+            holes[holes.Length - 1] = factory.CreateLinearRing(holePts);
+            return factory.CreatePolygon(shell, holes);
+        }
+
+        private static Coordinate[] ExtractLine(Geometry hole)
+        {
+            if (hole is Polygon phole) {
+                return phole.ExteriorRing.Coordinates;
+            }
+            return hole.Coordinates;
+        }
+    }
+}
