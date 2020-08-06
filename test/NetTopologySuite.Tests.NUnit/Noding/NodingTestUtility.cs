bf7dc7022050f7ba248ca4c6047cdb06a3732814
Felix Obermaier <felix.obermaier@netcologne.de>
2020-08-06 12:08:36 +0200

    Add SnappingNoder unit tests
    JTS' commit dr-jts/jts@d5b7819baa77aa3dc44806c2d4ac6bf09e28459b

---
+++ test/NetTopologySuite.Tests.NUnit/Noding/NodingTestUtility.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.Geometries.Utilities;
+using NetTopologySuite.Noding;
+
+namespace NetTopologySuite.Tests.NUnit.Noding
+{
+    public static class NodingTestUtility
+    {
+        public static MultiLineString ToLines(ICollection<ISegmentString> nodedList,
+            GeometryFactory geomFact)
+        {
+            var lines = new LineString[nodedList.Count];
+            int i = 0;
+            foreach (NodedSegmentString nss in nodedList)
+            {
+                var pts = nss.Coordinates;
+                var line = geomFact.CreateLineString(pts);
+                lines[i++] = line;
+            }
+            return geomFact.CreateMultiLineString(lines);
+        }
+
+        public static IList<ISegmentString> ToSegmentStrings(IEnumerable<Geometry> lines)
+        {
+            var nssList = new List<ISegmentString>();
+            foreach (LineString line in lines)
+            {
+                var nss = new NodedSegmentString(line.Coordinates, line);
+                nssList.Add(nss);
+            }
+            return nssList;
+        }
+
+        /**
+         * Runs a noder on one or two sets of input geometries
+         * and validates that the result is fully noded.
+         * 
+         * @param geom1 a geometry
+         * @param geom2 a geometry, which may be null
+         * @param noder the noder to use
+         * @return the fully noded linework
+         * 
+         * @throws TopologyException
+         */
+        public static Geometry NodeValidated(Geometry geom1, Geometry geom2, INoder noder)
+        {
+            var lines = new List<Geometry>(LineStringExtracter.GetLines(geom1));
+            if (geom2 != null)
+            {
+                lines.AddRange(LineStringExtracter.GetLines(geom2));
+            }
+            var ssList = ToSegmentStrings(lines);
+
+            var noderValid = new ValidatingNoder(noder);
+            noderValid.ComputeNodes(ssList);
+            var nodedList = noder.GetNodedSubstrings();
+
+            var result = ToLines(nodedList, geom1.Factory);
+            return result;
+        }
+    }
+}
---
+++ test/NetTopologySuite.Tests.NUnit/Noding/Snap/SnappingNoderTest.cs
@@ -0,0 +1,55 @@
+﻿using NetTopologySuite.Geometries;
+using NetTopologySuite.Noding.Snap;
+using NUnit.Framework;
+
+namespace NetTopologySuite.Tests.NUnit.Noding.Snap
+{
+    public class SnappingNoderTest : GeometryTestCase
+    {
+        [Test]
+        public void TestOverlappingLinesWithNearVertex()
+        {
+            string wkt1 = "LINESTRING (100 100, 300 100)";
+            string wkt2 = "LINESTRING (200 100.1, 400 100)";
+            string expected =
+                "MULTILINESTRING ((100 100, 200 100.1), (200 100.1, 300 100), (200 100.1, 300 100), (300 100, 400 100))";
+            checkRounding(wkt1, wkt2, 1, expected);
+        }
+
+        [Test]
+        public void TestSnappedVertex()
+        {
+            string wkt1 = "LINESTRING (100 100, 200 100, 300 100)";
+            string wkt2 = "LINESTRING (200 100.3, 400 110)";
+            string expected = "MULTILINESTRING ((100 100, 200 100), (200 100, 300 100), (200 100, 400 110))";
+            checkRounding(wkt1, wkt2, 1, expected);
+        }
+
+        [Test]
+        public void TestSelfSnap()
+        {
+            string wkt1 = "LINESTRING (100 200, 100 100, 300 100, 200 99.3, 200 0)";
+            string expected =
+                "MULTILINESTRING ((100 200, 100 100, 200 99.3), (200 99.3, 300 100), (300 100, 200 99.3), (200 99.3, 200 0))";
+            checkRounding(wkt1, null, 1, expected);
+        }
+
+        void checkRounding(string wkt1, string wkt2, double snapDist, string expectedWKT)
+        {
+            var geom1 = Read(wkt1);
+            Geometry geom2 = null;
+            if (wkt2 != null)
+                geom2 = Read(wkt2);
+
+            var noder = new SnappingNoder(snapDist);
+            var result = NodingTestUtility.NodeValidated(geom1, geom2, noder);
+
+            // only check if expected was provided
+            if (expectedWKT == null) return;
+            var expected = Read(expectedWKT);
+            CheckEqual(expected, result);
+        }
+
+
+    }
+}
