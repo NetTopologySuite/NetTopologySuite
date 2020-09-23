fd03f6586b3e4c82f534edf9fbe7b9eba31fb2ef
Felix Obermaier <felix.obermaier@netcologne.de>
2020-09-23 16:37:42 +0200

    Add KMLReader for reading KML geometry segments.
    JTS' commit locationtech/jts@cd08aebe38f90c40ee4301e417cadcf39fef530b

---
+++ test/NetTopologySuite.Tests.NUnit/IO/KML/KMLReaderTest.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+using NetTopologySuite.IO.KML;
+using NUnit.Framework;
+
+namespace NetTopologySuite.Tests.NUnit.IO.KML
+{
+    public class KMLReaderTest
+    {
+        private readonly KMLReader _kmlReader = new KMLReader(new[] {"altitudeMode", "tesselate", "extrude"});
+
+
+        [Test]
+        public void TestPoint()
+        {
+            CheckParsingResult("<Point><altitudeMode>absolute</altitudeMode><coordinates>1.0,1.0</coordinates></Point>",
+                "POINT (1 1)",
+                new IDictionary<string, string>[] {new Dictionary<string, string>(new[] {new KeyValuePair<string, string>("altitudeMode", "absolute")})});
+        }
+
+        [Test]
+        public void TestLineString()
+        {
+            CheckParsingResult(
+                "<LineString><tesselate>1</tesselate><coordinates>1.0,1.0 2.0,2.0</coordinates></LineString>",
+                "LINESTRING (1 1, 2 2)",
+                new IDictionary<string, string>[] {new Dictionary<string, string>(new[] {new KeyValuePair<string, string>("tesselate", "1")})});
+        }
+
+        [Test]
+        public void TestPolygon()
+        {
+            CheckParsingResult(
+                "<Polygon>" +
+                "   <altitudeMode>relativeToGround</altitudeMode>" +
+                "   <outerBoundaryIs>" +
+                "       <LinearRing>" +
+                "           <coordinates>1.0,1.0 1.0,10.0 10.0,10.0 10.0,1.0 1.0,1.0</coordinates>" +
+                "       </LinearRing>" +
+                "   </outerBoundaryIs>" +
+                "   <innerBoundaryIs>" +
+                "       <LinearRing>" +
+                "           <coordinates>2.0,2.0 2.0,3.0 3.0,3.0 3.0,2.0 2.0,2.0</coordinates>" +
+                "       </LinearRing>" +
+                "   </innerBoundaryIs>" +
+                "   <innerBoundaryIs>" +
+                "       <LinearRing>" +
+                "           <coordinates>6.0,6.0 6.0,7.0 7.0,7.0 7.0,6.0 6.0,6.0</coordinates>" +
+                "       </LinearRing>" +
+                "   </innerBoundaryIs>" +
+                "</Polygon>",
+                "POLYGON ((1 1, 1 10, 10 10, 10 1, 1 1), (2 2, 2 3, 3 3, 3 2, 2 2), (6 6, 6 7, 7 7, 7 6, 6 6))",
+                new IDictionary<string, string>[] {new Dictionary<string, string>(new[]
+                    {new KeyValuePair<string, string>("altitudeMode", "relativeToGround")})});
+        }
+
+        [Test]
+        public void TestMultiGeometry()
+        {
+            CheckParsingResult(
+                "<MultiGeometry>" +
+                "   <Point>" +
+                "       <altitudeMode>absolute</altitudeMode>" +
+                "       <coordinates>1.0,1.0</coordinates>" +
+                "   </Point>" +
+                "   <LineString>" +
+                "       <tesselate>1</tesselate>" +
+                "       <coordinates>1.0,1.0 2.0,2.0</coordinates>" +
+                "   </LineString>" +
+                "   <Polygon>" +
+                "       <altitudeMode>relativeToGround</altitudeMode>" +
+                "       <outerBoundaryIs>" +
+                "           <LinearRing>" +
+                "               <coordinates>1.0,1.0 1.0,10.0 10.0,10.0 10.0,1.0 1.0,1.0</coordinates>" +
+                "           </LinearRing>" +
+                "       </outerBoundaryIs>" +
+                "   </Polygon>" +
+                "</MultiGeometry>",
+                "GEOMETRYCOLLECTION (POINT (1 1), LINESTRING (1 1, 2 2), POLYGON ((1 1, 1 10, 10 10, 10 1, 1 1)))",
+                new IDictionary<string, string>[] {
+                    new Dictionary<string, string>(new[] { new KeyValuePair<string, string>("altitudeMode", "absolute") }),
+                    new Dictionary<string, string>(new[] { new KeyValuePair<string, string>("tesselate", "1") }),
+                    new Dictionary<string, string>(new[] { new KeyValuePair<string, string>("altitudeMode", "relativeToGround") })
+                });
+        }
+
+        [Test]
+        public void TestMultiGeometryWithAllPoints()
+        {
+            CheckParsingResult(
+                "<MultiGeometry>" +
+                "   <Point><coordinates>1.0,1.0</coordinates></Point>" +
+                "   <Point><coordinates>2.0,2.0</coordinates></Point>" +
+                "</MultiGeometry>",
+                "MULTIPOINT ((1 1), (2 2))",
+                new IDictionary<string, string>[] { null, null }
+            );
+        }
+
+        [Test]
+        public void TestMultiGeometryWithAllLines()
+        {
+            CheckParsingResult(
+                "<MultiGeometry>" +
+                "   <LineString><coordinates>1.0,1.0 2.0,2.0</coordinates></LineString>" +
+                "   <LineString><coordinates>5.0,5.0 6.0,6.0</coordinates></LineString>" +
+                "</MultiGeometry>",
+                "MULTILINESTRING ((1 1, 2 2), (5 5, 6 6))",
+                new IDictionary<string, string>[] { null, null }
+            );
+        }
+
+        [Test]
+        public void TestMultiGeometryWithAllPolygons()
+        {
+            CheckParsingResult(
+                "<MultiGeometry>" +
+                "   <Polygon><outerBoundaryIs><LinearRing><coordinates>2.0,2.0 2.0,3.0 3.0,3.0 3.0,2.0 2.0,2.0</coordinates></LinearRing></outerBoundaryIs></Polygon>" +
+                "   <Polygon><outerBoundaryIs><LinearRing><coordinates>6.0,6.0 6.0,7.0 7.0,7.0 7.0,6.0 6.0,6.0</coordinates></LinearRing></outerBoundaryIs></Polygon>" +
+                "</MultiGeometry>",
+                "MULTIPOLYGON (((2 2, 2 3, 3 3, 3 2, 2 2)), ((6 6, 6 7, 7 7, 7 6, 6 6)))",
+                new IDictionary<string, string>[]{ null, null }
+            );
+        }
+
+        [Test]
+        public void TestZ()
+        {
+            string kml = "<Point><coordinates>1.0,1.0,50.0</coordinates></Point>";
+            var kmlReader = new KMLReader();
+            try
+            {
+                var parsedGeometry = kmlReader.Read(kml);
+                Assert.AreEqual(50.0, parsedGeometry.Coordinate.Z, "Wrong Z");
+            }
+            catch (ParseException e)
+            {
+                throw new AssertionException($"ParseException: {e.Message}", e);
+            }
+        }
+
+        [Test]
+        public void TestPrecisionAndSRID()
+        {
+            string kml = "<Point><altitudeMode>absolute</altitudeMode><coordinates>1.385093,1.436456</coordinates></Point>";
+            var geometryFactory = new GeometryFactory(new PrecisionModel(1000.0), 4326);
+            var kmlReader = new KMLReader(geometryFactory);
+            try
+            {
+                var parsedGeometry = kmlReader.Read(kml);
+                Assert.AreEqual(geometryFactory.SRID, parsedGeometry.SRID, "Wrong SRID");
+                Assert.AreEqual("POINT (1.385 1.436)", parsedGeometry.AsText(), "Wrong precision");
+            }
+            catch (ParseException e)
+            {
+                throw new AssertionException($"ParseException: {e.Message}", e);
+            }
+        }
+
+        [Test]
+        public void TestCoordinatesErrors()
+        {
+            CheckExceptionThrown("<Point></Point>", "No element coordinates found in Point");
+            CheckExceptionThrown("<Point><coordinates></coordinates></Point>", "Empty coordinates");
+            CheckExceptionThrown("<Point><coordinates>1.0</coordinates></Point>", "Invalid coordinate format");
+            CheckExceptionThrown("<Point><coordinates>,1.0</coordinates></Point>", "Invalid coordinate format");
+            CheckExceptionThrown("<Point><coordinates>1.0,</coordinates></Point>", "Invalid coordinate format");
+
+            CheckExceptionThrown("<Polygon></Polygon>", "No outer boundary for Polygon");
+            CheckExceptionThrown("<Polygon><outerBoundaryIs><LinearRing></LinearRing></outerBoundaryIs></Polygon>",
+                "No element coordinates found in outerBoundaryIs");
+            CheckExceptionThrown("<Polygon><innerBoundaryIs><LinearRing></LinearRing></innerBoundaryIs></Polygon>",
+                "No element coordinates found in innerBoundaryIs");
+        }
+
+        [Test]
+        public void TestUnknownGeometryType()
+        {
+            CheckExceptionThrown("<StrangePoint></StrangePoint>", "Unknown KML geometry type StrangePoint");
+        }
+
+        private void CheckExceptionThrown(string kmlString, string expectedError)
+        {
+            try
+            {
+                _kmlReader.Read(kmlString);
+                Assert.Fail("Exception must be thrown");
+            }
+            catch (ParseException e)
+            {
+                Assert.AreEqual(expectedError, e.Message, "Exception text differs");
+            }
+        }
+
+        private void CheckParsingResult(string kmlString, string expectedWKT,
+            IDictionary<string, string>[] expectedAttributes)
+        {
+            try
+            {
+                var parsedGeometry = _kmlReader.Read(kmlString);
+                string wkt = parsedGeometry.AsText();
+
+                Assert.AreEqual(expectedWKT, wkt, "WKTs are not equal");
+
+                for (int i = 0; i < parsedGeometry.NumGeometries; i++)
+                {
+                    var geometryN = parsedGeometry.GetGeometryN(i);
+                    Assert.IsTrue(geometryN.UserData != null || expectedAttributes[i] == null, "User data is not filled");
+
+                    if (geometryN.UserData != null)
+                    {
+                        var actualUserData = (IDictionary<string, string>) geometryN.UserData;
+                        Assert.AreEqual(expectedAttributes[i].Count,
+                            actualUserData.Count, "Number of attributes differs in user data");
+
+                        foreach (var entry in expectedAttributes[i])
+                        {
+                            Assert.IsTrue(actualUserData.ContainsKey(entry.Key), $"User data has not attribute {entry.Key}");
+                            Assert.AreEqual(entry.Value, actualUserData[entry.Key], "Attribute value differs");
+                        }
+                    }
+                }
+            }
+            catch (ParseException e)
+            {
+                throw new AssertionException($"ParseException: {e.Message}", e);
+            }
+        }
+    }
+}
