c9261c50ff4a6498f13f5d2e79b9c839bcb7daad
Felix Obermaier <felix.obermaier@netcologne.de>
2021-04-09 13:20:24 +0200

    Add OrientationIndex simple failure test
    JTS' commit locationtech/jts@365701701157abc4e358a07d5d6db2817c073be1

--- test/NetTopologySuite.Tests.NUnit/Algorithm/OrientationIndexFailureTest.cs
+++ test/NetTopologySuite.Tests.NUnit/Algorithm/OrientationIndexFailureTest.cs
@@ -14,7 +14,9 @@ namespace NetTopologySuite.Tests.NUnit.Algorithm
     {
 
         /// <summary>
-        /// This is included to confirm this test is operating correctly
+        /// Tests cases that cause failure in a simple double-precision
+        /// implementation of orientation index, but
+        /// which are fixed by the improved DD algorithm.
         /// </summary>
         [Test]
         public void TestSanity()
@@ -23,39 +25,42 @@ namespace NetTopologySuite.Tests.NUnit.Algorithm
                 OrientationIndexTest.GetCoordinates("LINESTRING ( 0 0, 0 1, 1 1)")));
         }
 
-        [Test/*, ExpectedException(typeof(AssertionException))*/]
+        [Test /*, ExpectedException(typeof(AssertionException))*/]
         public void TestBadCCW()
         {
             // this case fails because subtraction of small from large loses precision
-            Coordinate[] pts = {
-                                   new Coordinate(1.4540766091864998, -7.989685402102996),
-                                   new Coordinate(23.131039116367354, -7.004368924503866),
-                                   new Coordinate(1.4540766091865, -7.989685402102996)
-                               };
+            Coordinate[] pts =
+            {
+                new Coordinate(1.4540766091864998, -7.989685402102996),
+                new Coordinate(23.131039116367354, -7.004368924503866),
+                new Coordinate(1.4540766091865, -7.989685402102996)
+            };
             CheckOrientation(pts);
         }
 
-        [Test/*, ExpectedException(typeof(AssertionException))*/]
+        [Test /*, ExpectedException(typeof(AssertionException))*/]
         public void TestBadCCW2()
         {
             // this case fails because subtraction of small from large loses precision
-            Coordinate[] pts = {
-                                   new Coordinate(219.3649559090992, 140.84159161824724),
-                                   new Coordinate(168.9018919682399, -5.713787599646864),
-                                   new Coordinate(186.80814046338352, 46.28973405831556)
-                               };
+            Coordinate[] pts =
+            {
+                new Coordinate(219.3649559090992, 140.84159161824724),
+                new Coordinate(168.9018919682399, -5.713787599646864),
+                new Coordinate(186.80814046338352, 46.28973405831556)
+            };
             CheckOrientation(pts);
         }
 
-        [Test/*, ExpectedException(typeof(AssertionException))*/]
+        [Test /*, ExpectedException(typeof(AssertionException))*/]
         public void TestBadCCW3()
         {
             // this case fails because subtraction of small from large loses precision
-            Coordinate[] pts = {
-                                   new Coordinate(279.56857838488514, -186.3790522565901),
-                                   new Coordinate(-20.43142161511487, 13.620947743409914),
-                                   new Coordinate(0, 0)
-                               };
+            Coordinate[] pts =
+            {
+                new Coordinate(279.56857838488514, -186.3790522565901),
+                new Coordinate(-20.43142161511487, 13.620947743409914),
+                new Coordinate(0, 0)
+            };
             CheckOrientation(pts);
         }
 
@@ -63,11 +68,12 @@ namespace NetTopologySuite.Tests.NUnit.Algorithm
         public void TestBadCCW4()
         {
             // from JTS list - 5/15/2012  strange case for the GeometryNoder
-            Coordinate[] pts = {
-                                   new Coordinate(-26.2, 188.7),
-                                   new Coordinate(37.0, 290.7),
-                                   new Coordinate(21.2, 265.2)
-                               };
+            Coordinate[] pts =
+            {
+                new Coordinate(-26.2, 188.7),
+                new Coordinate(37.0, 290.7),
+                new Coordinate(21.2, 265.2)
+            };
             CheckOrientation(pts);
         }
 
@@ -75,53 +81,81 @@ namespace NetTopologySuite.Tests.NUnit.Algorithm
         public void TestBadCCW5()
         {
             // from JTS list - 6/15/2012  another case from Tomas Fa
-            Coordinate[] pts = {
-                                   new Coordinate(-5.9, 163.1),
-                                   new Coordinate(76.1, 250.7),
-                                   new Coordinate(14.6, 185)
-                                   //new Coordinate(96.6, 272.6)
-                               };
+            Coordinate[] pts =
+            {
+                new Coordinate(-5.9, 163.1),
+                new Coordinate(76.1, 250.7),
+                new Coordinate(14.6, 185)
+                //new Coordinate(96.6, 272.6)
+            };
             CheckOrientation(pts);
         }
+
         [Test]
         public void TestBadCCW7()
-  {
-    // from JTS list - 6/26/2012  another case from Tomas Fa
-    Coordinate[] pts = {
-        new Coordinate(-0.9575, 0.4511),
-        new Coordinate(-0.9295, 0.3291),
-        new Coordinate(-0.8945, 0.1766)
-    };
-    CheckDD(pts, true);
-    CheckShewchuk(pts, false);
-    CheckOriginalJTS(pts, false);
-  }
+        {
+            // from JTS list - 6/26/2012  another case from Tomas Fa
+            Coordinate[] pts =
+            {
+                new Coordinate(-0.9575, 0.4511),
+                new Coordinate(-0.9295, 0.3291),
+                new Coordinate(-0.8945, 0.1766)
+            };
+            CheckDD(pts, true);
+            CheckShewchuk(pts, false);
+            CheckOriginalJTS(pts, false);
+        }
 
-  [Test]
+        [Test]
         public void TestBadCCW7_2()
-  {
-    // from JTS list - 6/26/2012  another case from Tomas Fa
-    // scale to integers - all methods work on this
-    Coordinate[] pts = {
-        new Coordinate(-9575, 4511),
-        new Coordinate(-9295, 3291),
-        new Coordinate(-8945, 1766)
-    };
-    CheckDD(pts, true);
-    CheckShewchuk(pts, true);
-    CheckOriginalJTS(pts, true);
-  }
+        {
+            // from JTS list - 6/26/2012  another case from Tomas Fa
+            // scale to integers - all methods work on this
+            Coordinate[] pts =
+            {
+                new Coordinate(-9575, 4511),
+                new Coordinate(-9295, 3291),
+                new Coordinate(-8945, 1766)
+            };
+            CheckDD(pts, true);
+            CheckShewchuk(pts, true);
+            CheckOriginalJTS(pts, true);
+        }
 
-  public void TestBadCCW6()
-  {
-    // from JTS Convex Hull "Almost collinear" unit test
-    Coordinate[] pts = {
-        new Coordinate(-140.8859438214298, 140.88594382142983),
-        new Coordinate(-57.309236848216706, 57.30923684821671),
-        new Coordinate(-190.9188309203678, 190.91883092036784)
-    };
-    CheckOrientation(pts);
-  }
+        [Test]
+        public void TestBadCCW6()
+        {
+            // from JTS Convex Hull "Almost collinear" unit test
+            Coordinate[] pts =
+            {
+                new Coordinate(-140.8859438214298, 140.88594382142983),
+                new Coordinate(-57.309236848216706, 57.30923684821671),
+                new Coordinate(-190.9188309203678, 190.91883092036784)
+            };
+            CheckOrientation(pts);
+        }
+
+        /**
+         * Tests a simple case in which a point exactly lies on a line, 
+         * but whose orientation can't be computed accurately in double precision or DD.
+         */
+        [Test]
+        public void TestSimpleFail()
+        {
+            var p1 = new Coordinate(1, 1);
+            var p2 = new Coordinate(3, 3.5);
+            // 2.6 is not exactly representable in DD, or in double
+            var q = new Coordinate(2.6, 3);
+
+            int indexDD = CGAlgorithmsDD.OrientationIndex(p1, p2, q);
+            int index = NonRobustCGAlgorithms.OrientationIndex(p1, p2, q);
+
+            //System.out.println("indexDD = " + indexDD);
+            //System.out.println("index = " + index);
+
+            Assert.That(indexDD != 0, "orientationIndex DD is not expected to be correct");
+            Assert.That(index != 0, "orientationIndex in DP is not expected to be correct");
+        }
 
         private static void CheckOrientation(Coordinate[] pts)
         {
@@ -153,11 +187,12 @@ namespace NetTopologySuite.Tests.NUnit.Algorithm
             double p1x, double p1y,
             double p2x, double p2y)
         {
-            Coordinate[] pts = {
-                                   new Coordinate(p0x, p0y),
-                                   new Coordinate(p1x, p1y),
-                                   new Coordinate(p2x, p2y)
-                               };
+            Coordinate[] pts =
+            {
+                new Coordinate(p0x, p0y),
+                new Coordinate(p1x, p1y),
+                new Coordinate(p2x, p2y)
+            };
             if (!IsAllOrientationsEqualDD(pts))
                 throw new InvalidOperationException("High-precision orientation computation FAILED");
             return OrientationIndexTest.IsAllOrientationsEqual(pts);
