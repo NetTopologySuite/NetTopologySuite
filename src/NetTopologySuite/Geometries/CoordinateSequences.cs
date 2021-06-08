560232376067598ba47f7cab78f0a17331504741
Felix Obermaier <felix.obermaier@netcologne.de>
2021-06-08 15:41:31 +0200

    Add Xml code documentation

--- src/NetTopologySuite/Geometries/CoordinateSequences.cs
+++ src/NetTopologySuite/Geometries/CoordinateSequences.cs
@@ -394,22 +394,45 @@ namespace NetTopologySuite.Geometries
             return -1;
         }
 
+        /// <summary>
+        /// Gets a value indicating the index of the first coordinate in the sequence.
+        /// </summary>
+        /// <remarks>The returned value is either <c>0</c> or, in case the sequence is empty, <c>-1</c></remarks>.
+        /// <param name="sequence">The sequence</param>
+        /// <returns>An index</returns>
         public static int FirstIndex(this CoordinateSequence sequence)
         {
             return sequence.Count == 0 ? -1 : 0;
         }
 
+        /// <summary>
+        /// Gets a value indicating the index of the last coordinate in the sequence.
+        /// </summary>
+        /// <remarks>The returned value is either <c><see cref="CoordinateSequence.Count"/> - 1</c> or, in case the sequence is empty, <c>-1</c></remarks>.
+        /// <param name="sequence">The sequence</param>
+        /// <returns>An index</returns>
         public static int LastIndex(this CoordinateSequence sequence)
         {
             return sequence.Count - 1;
         }
 
+        /// <summary>
+        /// Gets the first coordinate in the sequence.
+        /// </summary>
+        /// <remarks>The returned value is either a <see cref="Coordinate"/> or, in case the sequence is empty, <c>null</c></remarks>.
+        /// <param name="sequence">The sequence</param>
+        /// <returns>A coordinate</returns>
         public static Coordinate First(this CoordinateSequence sequence)
         {
             int index = sequence.FirstIndex();
             return index < 0 ? null : sequence.GetCoordinate(index);
         }
 
+        /// <summary>
+        /// Gets the last coordinate in the sequence.
+        /// </summary>
+        /// <remarks>The returned value is either a <see cref="Coordinate"/> or, in case the sequence is empty, <c>null</c></remarks>.
+        /// <returns>A coordinate</returns>
         public static Coordinate Last(this CoordinateSequence sequence)
         {
             int index = sequence.LastIndex();
