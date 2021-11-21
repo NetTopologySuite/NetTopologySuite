18b6bd7f8c363bb4251c24e7f666cee3e31c67a8
Olof Lagerkvist <ol@ltr-data.se>
2021-11-22 00:18:54 +0100

    Update AssemblyInfo.cs

--- src/NetTopologySuite/AssemblyInfo.cs
+++ src/NetTopologySuite/AssemblyInfo.cs
@@ -2,10 +2,13 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
 
 [assembly: AssemblyDelaySign(false)]
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 [assembly: Guid("6B7EB658-792E-4178-B853-8AEB851513A9")]
+
+[assembly: SecurityTransparent]
 
 [assembly: InternalsVisibleTo("NetTopologySuite.Tests.NUnit, PublicKey=" + Consts.PublicKeyToken)]
