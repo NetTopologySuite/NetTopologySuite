fe9e945164d65d8bd61fa7986a79f8057e57d2c7
Joe Amenta <airbreather@linux.com>
2021-11-23 09:52:23 -0500

    what I've got so far for partial trust

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using System.Security;
+using System.Security.Permissions;
+
+using NetTopologySuite.Operation.OverlayNG;
+
+public sealed class MyObject : MarshalByRefObject
+{
+    private static readonly Assembly MyAssembly = typeof(MyObject).Assembly;
+
+    public double GetValue()
+    {
+        return PrecisionUtility.InherentScale(100);
+    }
+
+    private static void Main()
+    {
+        var appDomainSetup = new AppDomainSetup { ApplicationBase = "." };
+
+        var permissionSet = new PermissionSet(PermissionState.None);
+        permissionSet.AddPermission(new SecurityPermission(SecurityPermissionFlag.Execution | SecurityPermissionFlag.SkipVerification));
+
+        var appDomain = AppDomain.CreateDomain("Sandbox", null, appDomainSetup, permissionSet);
+        try
+        {
+            var myObject = (MyObject)appDomain.CreateInstanceAndUnwrap(MyAssembly.FullName, typeof(MyObject).FullName);
+            Console.WriteLine(myObject.GetValue());
+        }
+        finally
+        {
+            AppDomain.Unload(appDomain);
+        }
+    }
+}
--- src/NetTopologySuite/AssemblyInfo.cs
+++ src/NetTopologySuite/AssemblyInfo.cs
@@ -2,10 +2,12 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
 
 [assembly: AssemblyDelaySign(false)]
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 [assembly: Guid("6B7EB658-792E-4178-B853-8AEB851513A9")]
+[assembly: SecurityRules(SecurityRuleSet.Level2, SkipVerificationInFullTrust = true)]
 
 [assembly: InternalsVisibleTo("NetTopologySuite.Tests.NUnit, PublicKey=" + Consts.PublicKeyToken)]
