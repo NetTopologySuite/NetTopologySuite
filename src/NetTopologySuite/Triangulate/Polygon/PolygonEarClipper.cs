5a40fd645d7c510f765c2bbe0ab0578da044bead
Felix Obermaier <felix.obermaier@netcologne.de>
2022-01-10 09:23:17 +0100

    Improve code documentation
    JTS' commit locationtech/jts@3a532d27ab7b23071d7d406c75d14ce7b1a8002e

--- src/NetTopologySuite/Triangulate/Polygon/PolygonEarClipper.cs
+++ src/NetTopologySuite/Triangulate/Polygon/PolygonEarClipper.cs
@@ -57,8 +57,10 @@ namespace NetTopologySuite.Triangulate.Polygon
         private int[] _cornerIndex;
 
         /// <summary>
-        /// Indexing vertices improves ear intersection testing performance a lot.
+        /// Indexing vertices improves ear intersection testing performance.
         /// The polyShell vertices are contiguous, so are suitable for an SPRtree.
+        /// Note that a KDtree cannot be used because the vertex indices must be stored
+        /// and duplicates must be stored.
         /// </summary>
         private readonly VertexSequencePackedRtree _vertexCoordIndex;
 
@@ -189,15 +191,16 @@ namespace NetTopologySuite.Triangulate.Polygon
                 //--- a duplicate corner vertex requires a full scan
                 return IsValidEarScan(cornerIndex, corner);
             }
+            //-- vertex is contained in corner, so it is not a valid ear
             return false;
         }
 
         /// <summary>
-        /// Finds another vertex intersecting the corner triangle, if any.
+        /// Finds a vertex contained in the corner triangle, if any.
         /// Uses the vertex spatial index for efficiency.
         /// <para/>
-        /// Also finds any vertex which is a duplicate of the corner apex vertex,
-        /// which then requires a full scan of the vertices to confirm ear is valid.
+        /// Also finds any vertex which is a duplicate of the corner apex vertex.
+        /// This requires a full scan of the vertices to confirm ear is valid.
         /// This is usually a rare situation, so has little impact on performance.
         /// </summary>
         /// <param name="cornerIndex">The index of the corner apex vertex</param>
@@ -221,11 +224,11 @@ namespace NetTopologySuite.Triangulate.Polygon
 
                 var v = _vertex[vertIndex];
                 /*
-                 * If another vertex at the corner is found,
-                 * need to do a full scan to check the incident segments.
-                 * This happens when the polygon ring self-intersects,
-                 * usually due to hold joining.
-                 * But only report this if no properly intersecting vertex is found,
+                 * If the vertex is equal to the corner apex, record it.
+                 * This can happen where the polygon ring self-touches,
+                 * usually due to hole joining.
+                 * This will require a full scan to check the incident segments.
+                 * So only report this if no properly intersecting vertex is found,
                  * for efficiency.
                  */
                 if (v.Equals2D(corner[1]))
