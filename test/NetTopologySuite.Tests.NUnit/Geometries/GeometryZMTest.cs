399b8d0ed152f54531a8f89192bbb37e3fdaa59e
Felix Obermaier <felix.obermaier@netcologne.de>
2022-01-13 11:37:45 +0100

    Ensure some algorithms handling of XYZM coordinates
    JTS' commit locationtech/jts@e0a932c1f82793af0da3b11391a83590b103a2fc

---
+++ test/NetTopologySuite.Tests.NUnit/Geometries/GeometryZMTest.cs
@@ -0,0 +1,29 @@
+﻿using NetTopologySuite.Geometries;
+using NetTopologySuite.Geometries.Implementation;
+using NUnit.Framework;
+
+namespace NetTopologySuite.Tests.NUnit.Geometries
+{
+    internal class GeometryZMTest : GeometryTestCase
+    {
+        public GeometryZMTest()
+            : base(PackedCoordinateSequenceFactory.DoubleFactory)
+        { }
+
+        [Test]
+        public void TestArea()
+        {
+            var geom = (Polygon)Read("POLYGON ZM ((1 9 2 3, 9 9 2 3, 9 1 2 3, 1 1 2 3, 1 9 2 3))");
+            double area = geom.Area;
+            Assert.That(area, Is.EqualTo(64.0));
+        }
+
+        [Test]
+        public void TestLength()
+        {
+            var geom = Read("POLYGON ZM ((1 9 2 3, 9 9 2 3, 9 1 2 3, 1 1 2 3, 1 9 2 3))");
+            double len = geom.Length;
+            Assert.That(len, Is.EqualTo(32.0));
+        }
+    }
+}
