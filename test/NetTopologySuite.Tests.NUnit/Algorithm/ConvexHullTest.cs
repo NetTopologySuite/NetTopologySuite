4944c5f855fc34bfd2e08e3d7e2f56810b012348
Felix Obermaier <felix.obermaier@netcologne.de>
2023-01-17 09:05:46 +0100

    ConvexHull better unit tests
    JTS' commit locationtech/jts@3c847676d39fe64e91ca8e6311abedcabc039182

--- test/NetTopologySuite.Tests.NUnit/Algorithm/ConvexHullTest.cs
+++ test/NetTopologySuite.Tests.NUnit/Algorithm/ConvexHullTest.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using NetTopologySuite.Algorithm;
+﻿using NetTopologySuite.Algorithm;
 using NetTopologySuite.Geometries;
-using NetTopologySuite.Geometries.Utilities;
 using NetTopologySuite.IO;
-using NetTopologySuite.Tests.NUnit.Utilities;
 using NUnit.Framework;
 
 namespace NetTopologySuite.Tests.NUnit.Algorithm
@@ -49,114 +44,59 @@ namespace NetTopologySuite.Tests.NUnit.Algorithm
         }
 
         [Test]
-        public void Test1()
+        public void TestLineCollinear()
         {
-            var reader = new WKTReader(new NtsGeometryServices(new PrecisionModel(1), 0));
-            var lineString = (LineString)reader.Read("LINESTRING (30 220, 240 220, 240 220)");
-            var convexHull = (LineString)reader.Read("LINESTRING (30 220, 240 220)");
-            Assert.IsTrue(convexHull.EqualsExact(lineString.ConvexHull()));
+            CheckConvexHull(
+                "LINESTRING (30 220, 240 220, 240 220)",
+                "LINESTRING (30 220, 240 220)");
         }
 
         [Test]
-        public void Test2()
+        public void TestLineCollinear2()
         {
-            var reader = new WKTReader(new NtsGeometryServices(new PrecisionModel(1), 0));
-            var geometry = reader.Read("MULTIPOINT (130 240, 130 240, 130 240, 570 240, 570 240, 570 240, 650 240)");
-            var convexHull = (LineString)reader.Read("LINESTRING (130 240, 650 240)");
-            Assert.IsTrue(convexHull.EqualsExact(geometry.ConvexHull()));
+            CheckConvexHull(
+                "MULTIPOINT (130 240, 130 240, 130 240, 570 240, 570 240, 570 240, 650 240)",
+                "LINESTRING (130 240, 650 240)");
         }
 
         [Test]
-        public void Test3()
+        public void TestMultiCollinearEqual12()
         {
-            var reader = new WKTReader(new NtsGeometryServices(new PrecisionModel(1), 0));
-            var geometry = reader.Read("MULTIPOINT (0 0, 0 0, 10 0)");
-            var convexHull = (LineString)reader.Read("LINESTRING (0 0, 10 0)");
-            Assert.IsTrue(convexHull.EqualsExact(geometry.ConvexHull()));
+            CheckConvexHull(
+                "MULTIPOINT (0 0, 0 0, 10 0)",
+                "LINESTRING (0 0, 10 0)");
         }
 
         [Test]
-        public void Test4()
+        public void TestMultiPointCollinearEqual23()
         {
-            var reader = new WKTReader(new NtsGeometryServices(new PrecisionModel(1000), 0));
-            var geometry = reader.Read("MULTIPOINT (0 0, 10 0, 10 0)");
-            var convexHull = (LineString)reader.Read("LINESTRING (0 0, 10 0)");
-            Assert.IsTrue(convexHull.EqualsExact(geometry.ConvexHull()));
+            CheckConvexHull(
+                "MULTIPOINT (0 0, 10 0, 10 0)",
+                "LINESTRING (0 0, 10 0)");
         }
 
         [Test]
-        public void Test5()
+        public void TestMultiPointCollinearEqualNone()
         {
-            var reader = new WKTReader(new NtsGeometryServices(new PrecisionModel(1000), 0));
-            var geometry = reader.Read("MULTIPOINT (0 0, 5 0, 10 0)");
-            var convexHull = (LineString)reader.Read("LINESTRING (0 0, 10 0)");
-            Assert.IsTrue(convexHull.EqualsExact(geometry.ConvexHull()));
+            CheckConvexHull(
+                "MULTIPOINT (0 0, 5 0, 10 0)",
+                "LINESTRING (0 0, 10 0)");
         }
 
         [Test]
-        public void Test6()
+        public void TestMultiPoint()
         {
-            var reader = new WKTReader(new NtsGeometryServices(new PrecisionModel(1000), 0));
-            var actualGeometry = reader.Read("MULTIPOINT (0 0, 5 1, 10 0)").ConvexHull();
-            var expectedGeometry = reader.Read("POLYGON ((0 0, 5 1, 10 0, 0 0))");
-            Assert.IsTrue(actualGeometry.EqualsTopologically(expectedGeometry));
+            CheckConvexHull(
+                "MULTIPOINT (0 0, 5 1, 10 0)",
+                "POLYGON ((0 0, 5 1, 10 0, 0 0))");
         }
-
-        // TJackson - Not included in NTS because there is no longer a ToCoordinateArray method on ConvexHull
-        //public void testToArray() throws Exception {
-        //    TestConvexHull convexHull = new TestConvexHull(geometryFactory.createGeometryCollection(null));
-        //    Stack stack = new Stack();
-        //    stack.push(new Coordinate(0, 0));
-        //    stack.push(new Coordinate(1, 1));
-        //    stack.push(new Coordinate(2, 2));
-        //    Object[] array1 = convexHull.toCoordinateArray(stack);
-        //    assertEquals(3, array1.length);
-        //    assertEquals(new Coordinate(0, 0), array1[0]);
-        //    assertEquals(new Coordinate(1, 1), array1[1]);
-        //    assertEquals(new Coordinate(2, 2), array1[2]);
-        //    assertTrue(!array1[0].equals(array1[1]));
-        //}
-
-        //private static class TestConvexHull extends ConvexHull {
-        //    protected Coordinate[] toCoordinateArray(Stack stack) {
-        //        return super.toCoordinateArray(stack);
-        //    }
-        //    public TestConvexHull(Geometry geometry) {
-        //        super(geometry);
-        //    }
-        //}
 
         [Test]
-        public void Test7()
+        public void TestMultiPointLinear()
         {
-            var reader = new WKTReader(new NtsGeometryServices(PrecisionModel.Fixed.Value, 0));
-            var geometry = reader.Read("MULTIPOINT (0 0, 0 0, 5 0, 5 0, 10 0, 10 0)");
-            var convexHull = (LineString)reader.Read("LINESTRING (0 0, 10 0)");
-            Assert.IsTrue(convexHull.EqualsExact(geometry.ConvexHull()));
-        }
-
-        [TestCaseSource(typeof(PointwiseGeometryAggregationTestCases))]
-        public void TestStaticAggregation(ICollection<Geometry> geoms)
-        {
-            var actual = ConvexHull.Create(geoms);
-
-            // JTS doesn't usually bother doing anything special about nulls,
-            // so our ports of their stuff will suffer the same.
-            geoms = geoms?.Where(g => g != null).ToArray() ?? Array.Empty<Geometry>();
-
-            var combinedGeometry = GeometryCombiner.Combine(geoms);
-
-            // JTS also doesn't fear giving us nulls back from its algorithms.
-            var expected = combinedGeometry?.ConvexHull();
-
-            if (expected?.IsEmpty == false)
-            {
-                Assert.That(expected.EqualsTopologically(actual));
-            }
-            else
-            {
-                Assert.That(actual.IsEmpty);
-            }
+            CheckConvexHull(
+                "MULTIPOINT (0 0, 0 0, 5 0, 5 0, 10 0, 10 0)",
+                "LINESTRING (0 0, 10 0)");
         }
 
         [Test]
