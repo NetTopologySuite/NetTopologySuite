589df86c0dede6a3584f33ace0a204a7acc5828e
Felix Obermaier <felix.obermaier@netcologne.de>
2023-01-17 08:58:04 +0100

    Add ConvexHull unit tests
    JTS' commit locationtech/jts@f96699d950aac98f61cabec969e3508b38a906bf

--- test/NetTopologySuite.Tests.NUnit/Algorithm/ConvexHullTest.cs
+++ test/NetTopologySuite.Tests.NUnit/Algorithm/ConvexHullTest.cs
@@ -166,7 +166,21 @@ namespace NetTopologySuite.Tests.NUnit.Algorithm
                 "MULTIPOINT ((-0.2 -0.1), (0 -0.1), (0.2 -0.1), (0 -0.1), (-0.2 0.1), (0 0.1), (0.2 0.1), (0 0.1))",
                 "POLYGON ((-0.2 -0.1, -0.2 0.1, 0.2 0.1, 0.2 -0.1, -0.2 -0.1))");
         }
+        /**
+         * See https://trac.osgeo.org/geos/ticket/850
+         */
+        [Test]
+        public void TestGEOS_850()
+        {
+            CheckConvexHull("01040000001100000001010000002bd3a24002bcb0417ff59d2051e25c4101010000003aebcec70a8b3cbfdb123fe713a2e8be0101000000afa0bb8638b770bf7fc1d77d0dda1cbf01010000009519cb944ce070bf1a46cd7df4201dbf010100000079444b4cd1937cbfa6ca29ada6a928bf010100000083323f09e16c7cbfd36d07ee0b8828bf01010000009081b8f066967ebf915fbc9ebe652abf0101000000134cf280633bc1bf37b754972dbe6dbf0101000000ea992c094df585bf1bbabc8a42f332bf0101000000c0a13c7fb31186bf9af7b10cc50b33bf0101000000a0bba15a0a7188bf8fba7870e91735bf01010000000fc8701903db93bf93bdbe93b52241bf01010000007701a73b29cc90bfb770bc3732fe3cbf010100000036fa45b75b8b8cbf1cfca5bf59a238bf0101000000a54e773f7f287ebf910d4621e5062abf01010000004b5b5dc4196f55bfa51f0579717f02bf01010000007e549489513a5fbfa57bacea34f30abf",
+                "POLYGON ((-0.1346248988744213 -0.0036307230426677, -0.0019059940589774 -0.0000514030956167, 280756800.63603467 7571780.50964105, -0.1346248988744213 -0.0036307230426677))",
+                0.000000000001);
+        }
 
+        /**
+         * Tests robustness issue in radial sort.
+         * See https://github.com/libgeos/geos/issues/722
+         */
         [Test]
         public void TestCollinearPointsTinyX()
         {
@@ -176,7 +190,7 @@ namespace NetTopologySuite.Tests.NUnit.Algorithm
         }
 
         [Test]
-        public void TestCollinearPointsTinyXAlt()
+        public void TestCollinearPointsLessTinyX()
         {
             CheckConvexHull(
                 "MULTIPOINT (-0.2 -0.1, 1.38777878e-7 -0.1, 0.2 -0.1, -1.38777878e-7 -0.1, -0.2 0.1, 1.38777878e-7 0.1, 0.2 0.1, -1.38777878e-7 0.1)",
@@ -189,6 +203,13 @@ namespace NetTopologySuite.Tests.NUnit.Algorithm
             var actual = geom.ConvexHull();
             var expected = Read(wktExpected);
             CheckEqual(expected, actual);
+        }
+        private void CheckConvexHull(string wkt, string wktExpected, double tolerance)
+        {
+            var geom = Read(wkt);
+            var actual = geom.ConvexHull();
+            var expected = Read(wktExpected);
+            CheckEqual(expected, actual, tolerance);
         }
 
     }
