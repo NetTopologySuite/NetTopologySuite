03b1db7ebf87413fb56697fe7c6024ff5ba2c1e1
Felix Obermaier <felix.obermaier@netcologne.de>
2023-03-14 16:53:11 +0100

    Improve Javadoc for GeometryComponentFilter
    JTS' commit locationtech/jts@436f1e8c91ebdfe9e8f76f3dc35c2aa65fa453e7

--- src/NetTopologySuite/Geometries/IGeometryComponentFilter.cs
+++ src/NetTopologySuite/Geometries/IGeometryComponentFilter.cs
@@ -2,28 +2,31 @@ namespace NetTopologySuite.Geometries
 {
     /// <summary>
     /// <c>Geometry</c> classes support the concept of applying
-    /// an <c>IGeometryComponentFilter</c> filter to the <c>Geometry</c>.
+    /// an <c>IGeometryComponentFilter</c> filter to a geometry.
     /// </summary>
     /// <remarks>
-    /// The filter is applied to every component of the <c>Geometry</c>
-    /// which is itself a <c>Geometry</c>
-    /// and which does not itself contain any components.
-    /// (For instance, all the LinearRings in Polygons are visited,
-    /// but in a MultiPolygon the Polygons themselves are not visited.)
-    /// Thus the only classes of Geometry which must be 
-    /// handled as arguments to <see cref="Filter"/>
-    /// are <see cref="LineString"/>s, <see cref="LinearRing"/>s and <see cref="Point"/>s.
+    /// The filter is applied to every component of a geometry
+    /// as well as to the geometry itself.
+    /// For instance, in a <see cref="Polygon"/>,
+    /// all the <see cref="LinearRing"/>
+    /// components for the shell and holes are visited,
+    /// as well as the polygon itself.
+    /// In order to process only atomic components,
+    /// the <see cref="Filter"/> method code must
+    /// explicitly handle only <see cref="LineString"/>s, <see cref="LinearRing"/>s and <see cref="Point"/>s.
+    /// <para/>
     /// An <c>IGeometryComponentFilter</c> filter can either
     /// record information about the <c>Geometry</c>
     /// or change the <c>Geometry</c> in some way.
+    /// <para/>
     /// <c>IGeometryComponentFilter</c> is an example of the Gang-of-Four Visitor pattern.
-    /// </remarks>>    
+    /// </remarks>
     public interface IGeometryComponentFilter
     {
         /// <summary>
-        /// Performs an operation with or on <c>geom</c>.
+        /// Performs an operation with or on a geometry component.
         /// </summary>
-        /// <param name="geom">A <c>Geometry</c> to which the filter is applied.</param>
+        /// <param name="geom">A component of the geometry to which the filter is applied.</param>
         void Filter(Geometry geom);
     }
 }
