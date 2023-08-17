7fc0c6ffdb875528e19cae4a2dc3faa559a8472b
Felix Obermaier <felix.obermaier@netcologne.de>
2023-08-17 13:33:48 +0200

    Refactor OverlayNG unit tests
    JTS' commits:
    * locationtech/jts@9830c1bb1478c23fcb694fb98a4dd1a11327d57f
    * locationtech/jts@78feb6c98c332eb1d1c6da2c584bbc51aae47dc7
    * locationtech/jts@2867127264c30cd15681736cb35378ba41ea2b2f

--- test/NetTopologySuite.Tests.NUnit/Operation/OverlayNG/OverlayNGTestCase.cs
+++ test/NetTopologySuite.Tests.NUnit/Operation/OverlayNG/OverlayNGTestCase.cs
@@ -3,6 +3,8 @@ using NetTopologySuite.Operation.Overlay;
 
 namespace NetTopologySuite.Tests.NUnit.Operation.OverlayNG
 {
+    using OverlayNG = NetTopologySuite.Operation.OverlayNG.OverlayNG;
+
     public class OverlayNGTestCase : GeometryTestCase
     {
         protected void CheckIntersection(string wktA, string wktB, string wktExpected)
@@ -20,9 +22,57 @@ namespace NetTopologySuite.Tests.NUnit.Operation.OverlayNG
             var a = Read(wktA);
             var b = Read(wktB);
             var pm = new PrecisionModel();
-            var actual = NetTopologySuite.Operation.OverlayNG.OverlayNG.Overlay(a, b, overlayOp, pm);
+            var actual = OverlayNG.Overlay(a, b, overlayOp, pm);
             var expected = Read(wktExpected);
             CheckEqual(expected, actual);
+        }
+
+        public static Geometry Difference(Geometry a, Geometry b)
+        {
+            var pm = new PrecisionModel();
+            return OverlayNG.Overlay(a, b, SpatialFunction.Difference, pm);
+        }
+
+        public static Geometry SymDifference(Geometry a, Geometry b)
+        {
+            var pm = new PrecisionModel();
+            return OverlayNG.Overlay(a, b, SpatialFunction.SymDifference, pm);
+        }
+
+        public static Geometry Intersection(Geometry a, Geometry b)
+        {
+            var pm = new PrecisionModel();
+            return OverlayNG.Overlay(a, b, SpatialFunction.Intersection, pm);
+        }
+
+        public static Geometry Union(Geometry a, Geometry b)
+        {
+            var pm = new PrecisionModel();
+            return OverlayNG.Overlay(a, b, SpatialFunction.Union, pm);
+        }
+
+        public static Geometry Difference(Geometry a, Geometry b, double scaleFactor)
+        {
+            var pm = new PrecisionModel(scaleFactor);
+            return OverlayNG.Overlay(a, b, SpatialFunction.Difference, pm);
+        }
+
+        public static Geometry SymDifference(Geometry a, Geometry b, double scaleFactor)
+        {
+            var pm = new PrecisionModel(scaleFactor);
+            return OverlayNG.Overlay(a, b, SpatialFunction.SymDifference, pm);
+        }
+
+        public static Geometry Intersection(Geometry a, Geometry b, double scaleFactor)
+        {
+            var pm = new PrecisionModel(scaleFactor);
+            return OverlayNG.Overlay(a, b, SpatialFunction.Intersection, pm);
+        }
+
+        public static Geometry Union(Geometry a, Geometry b, double scaleFactor)
+        {
+            var pm = new PrecisionModel(scaleFactor);
+            return OverlayNG.Overlay(a, b, SpatialFunction.Union, pm);
         }
     }
 }
