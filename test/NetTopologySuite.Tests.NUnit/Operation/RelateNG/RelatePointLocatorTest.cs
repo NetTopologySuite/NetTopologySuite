b4c9a7e24ab703b7f12db4eea74bc4c64f509ddb
Felix Obermaier <felix.obermaier@netcologne.de>
2024-08-12 09:10:18 +0200

    Rename functions for clarity
    JTS' commit locationtech/jts@8c71ae7b56f600ed05b87c9584ae04fe33d154dd

--- test/NetTopologySuite.Tests.NUnit/Operation/RelateNG/RelatePointLocatorTest.cs
+++ test/NetTopologySuite.Tests.NUnit/Operation/RelateNG/RelatePointLocatorTest.cs
@@ -14,73 +14,73 @@ namespace NetTopologySuite.Tests.NUnit.Operation.RelateNG
         public void TestPoint()
         {
             //string wkt = "GEOMETRYCOLLECTION (POINT(0 0), POINT(1 1))";
-            CheckLocation(gcPLA, 1, 1, DimensionLocation.POINT_INTERIOR);
-            CheckLocation(gcPLA, 0, 1, (int)Location.Exterior);
+            CheckDimLocation(gcPLA, 1, 1, DimensionLocation.POINT_INTERIOR);
+            CheckDimLocation(gcPLA, 0, 1, DimensionLocation.EXTERIOR);
         }
 
         [Test]
         public void TestPointInLine()
         {
-            CheckLocation(gcPLA, 3, 8, DimensionLocation.LINE_INTERIOR);
+            CheckDimLocation(gcPLA, 3, 8, DimensionLocation.LINE_INTERIOR);
         }
 
         [Test]
         public void TestPointInArea()
         {
-            CheckLocation(gcPLA, 8, 8, DimensionLocation.AREA_INTERIOR);
+            CheckDimLocation(gcPLA, 8, 8, DimensionLocation.AREA_INTERIOR);
         }
 
         [Test]
         public void TestLine()
         {
-            CheckLocation(gcPLA, 3, 3, DimensionLocation.LINE_INTERIOR);
-            CheckLocation(gcPLA, 3, 1, DimensionLocation.LINE_BOUNDARY);
+            CheckDimLocation(gcPLA, 3, 3, DimensionLocation.LINE_INTERIOR);
+            CheckDimLocation(gcPLA, 3, 1, DimensionLocation.LINE_BOUNDARY);
         }
 
         [Test]
         public void TestLineInArea()
         {
-            CheckLocation(gcPLA, 11, 11, DimensionLocation.AREA_INTERIOR);
-            CheckLocation(gcPLA, 14, 14, DimensionLocation.AREA_INTERIOR);
+            CheckDimLocation(gcPLA, 11, 11, DimensionLocation.AREA_INTERIOR);
+            CheckDimLocation(gcPLA, 14, 14, DimensionLocation.AREA_INTERIOR);
         }
 
         [Test]
         public void TestArea()
         {
-            CheckLocation(gcPLA, 8, 8, DimensionLocation.AREA_INTERIOR);
-            CheckLocation(gcPLA, 9, 9, DimensionLocation.AREA_BOUNDARY);
+            CheckDimLocation(gcPLA, 8, 8, DimensionLocation.AREA_INTERIOR);
+            CheckDimLocation(gcPLA, 9, 9, DimensionLocation.AREA_BOUNDARY);
         }
 
         [Test]
         public void TestAreaInArea()
         {
-            CheckLocation(gcPLA, 11, 11, DimensionLocation.AREA_INTERIOR);
-            CheckLocation(gcPLA, 12, 12, DimensionLocation.AREA_INTERIOR);
-            CheckLocation(gcPLA, 10, 10, DimensionLocation.AREA_BOUNDARY);
-            CheckLocation(gcPLA, 16, 16, DimensionLocation.AREA_INTERIOR);
+            CheckDimLocation(gcPLA, 11, 11, DimensionLocation.AREA_INTERIOR);
+            CheckDimLocation(gcPLA, 12, 12, DimensionLocation.AREA_INTERIOR);
+            CheckDimLocation(gcPLA, 10, 10, DimensionLocation.AREA_BOUNDARY);
+            CheckDimLocation(gcPLA, 16, 16, DimensionLocation.AREA_INTERIOR);
         }
 
         [Test]
         public void TestLineNode()
         {
-            //checkNodeLocation(gcPLA, 12.1, 12.2, Location.INTERIOR);
+            //CheckNodeLocation(gcPLA, 12.1, 12.2, Location.Interior);
             CheckNodeLocation(gcPLA, 3, 1, Location.Boundary);
         }
 
-        private void CheckLocation(string wkt, double i, double j, int expected)
+        private void CheckDimLocation(string wkt, double x, double y, int expectedDimLoc)
         {
             var geom = Read(wkt);
             var locator = new RelatePointLocator(geom);
-            int actual = locator.LocateWithDim(new Coordinate(i, j));
-            Assert.That(actual, Is.EqualTo(expected));
+            int actual = locator.LocateWithDim(new Coordinate(x, y));
+            Assert.That(actual, Is.EqualTo(expectedDimLoc));
         }
 
-        private void CheckNodeLocation(string wkt, double i, double j, Location expected)
+        private void CheckNodeLocation(string wkt, double x, double y, Location expectedLoc)
         {
             var geom = Read(wkt);
             var locator = new RelatePointLocator(geom);
-            var actual = locator.LocateNode(new Coordinate(i, j), null);
-            Assert.That(actual, Is.EqualTo(expected));
+            var actual = locator.LocateNode(new Coordinate(x, y), null);
+            Assert.That(actual, Is.EqualTo(expectedLoc));
         }
     }
 
