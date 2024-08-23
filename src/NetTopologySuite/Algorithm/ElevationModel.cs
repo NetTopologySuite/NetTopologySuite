b98ab0ffda42c3f04037c3e133524901d5e9f667
Felix Obermaier <felix.obermaier@netcologne.de>
2024-08-23 15:32:22 +0200

    Add (basic) ElevationModel class
    ... and unit test

---
+++ src/NetTopologySuite/Algorithm/ElevationModel.cs
@@ -0,0 +1,132 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace NetTopologySuite.Algorithm
+{
+    /// <summary>
+    /// A base elevation model class.
+    /// </summary>
+    public class ElevationModel
+    {
+        private static ElevationModel _default = new ElevationModel();
+
+        private readonly double _z;
+        private readonly Envelope _extent;
+        private readonly int _srid;
+
+        /// <summary>
+        /// Creates an elevation model that always returns <see cref="Coordinate.NullOrdinate"/> as result.
+        /// </summary>
+        public ElevationModel() : this(Coordinate.NullOrdinate)
+        { }
+
+        /// <summary>
+        /// Creates an elevation model that always returns <paramref name="z"/> as result.
+        /// </summary>
+        /// <param name="z">The result value for <see cref="GetZ(Coordinate)"/> or its overloads.</param>
+        internal ElevationModel(double z)
+        {
+            _z = z;
+        }
+
+        /// <summary>
+        /// Creates an elevation model that always returns <paramref name="z"/> as result.
+        /// </summary>
+        /// <param name="z">The result value for <see cref="GetZ(Coordinate)"/> or its overloads.</param>
+        /// <param name="extent">The extent where this elevation model is valid</param>
+        /// <param name="srid">The spatial reference id</param>
+        public ElevationModel(double z, Envelope extent, int srid)
+        {
+            _z = z;
+            _extent = extent;
+            _srid = srid;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating the default <see cref="ElevationModel"/>
+        /// </summary>
+        /// <remarks>The value <c>null</c> cannot be assigned to this property, it will be converted to a no-op elevation model.</remarks>
+        public static ElevationModel Default
+        {
+            get => _default;
+            set
+            {
+                if (value == null)
+                    value = new ElevationModel();
+                _default = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating the extent where this elevation model is valid.
+        /// </summary>
+        /// <remarks>
+        /// If this value is <c>null</c>, no check for the validity of the
+        /// input arguments for <see cref="GetZ(Coordinate)"/> and its overload is made.
+        /// </remarks>
+        public Envelope Extent { get => _extent; }
+
+        /// <summary>
+        /// Gets a value indicating the spatial reference system this elevation model belongs to.
+        /// </summary>
+        public int SRID { get => _srid; }
+
+        /// <summary>
+        /// Gets the z-ordinate value for a given <paramref name="coordinate"/>.
+        /// </summary>
+        /// <param name="coordinate">A coordinate to get the z-ordinate value.</param>
+        /// <returns>The z-ordinate value</returns>
+        public virtual double GetZ(Coordinate coordinate)
+        {
+            if (_extent != null && !_extent.Contains(coordinate))
+                return Coordinate.NullOrdinate;
+
+            return _z;
+        }
+
+        /// <summary>
+        /// Gets the z-ordinate value for a given pair of <paramref name="x"/> and <paramref name="y"/> ordinates.
+        /// </summary>
+        /// <param name="x">A x-ordinate value.</param>
+        /// <param name="y">A y-ordinate value.</param>
+        /// <returns>The z-ordinate value</returns>
+        public double GetZ(double x, double y) => GetZ(new Coordinate(x, y));
+
+
+        /// <summary>
+        /// Creates a copy of <paramref name="c"/> that has the z-ordinate value at <paramref name="c"/>.
+        /// If the elevation model can't retrieve a 
+        /// </summary>
+        /// <param name="c">A coordinate</param>
+        /// <returns>A copy of <paramref name="c"/> with the z-ordinate value.</returns>
+        public Coordinate CopyWithZ(Coordinate c)
+        {
+            // If it already has a z-ordinate value, return a copy
+            if (!double.IsNaN(c.Z)) return c.Copy();
+
+            // Get the z-ordinate value for c. If no z-ordinate value was supplied, return a copy of c
+            double z = GetZ(c);
+            if (double.IsNaN(z)) return c.Copy();
+
+            // Depending on the type return specific coordinate classes.
+            if (c.GetType() == typeof(CoordinateM))
+                return new CoordinateZM(c.X, c.Y, z, c.M);
+
+            if (c is ExtraDimensionalCoordinate edc)
+            {
+                ExtraDimensionalCoordinate edcRes = null;
+                if (edc.Dimension - edc.Measures > 2)
+                    edcRes = (ExtraDimensionalCoordinate)edc.Copy();
+                else 
+                    edcRes = new ExtraDimensionalCoordinate(edc.Dimension + 1, edc.Measures) {
+                        CoordinateValue = edc
+                    };
+                edcRes.Z = z;
+                return edcRes;
+            }
+
+            // Last resort
+            return new CoordinateZ(c.X, c.Y, z);
+        }
+    }
+
+}
---
+++ test/NetTopologySuite.Tests.NUnit/Algorithm/ElevationModelTest.cs
@@ -0,0 +1,122 @@
+﻿using NetTopologySuite.Algorithm;
+using NetTopologySuite.Geometries;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace NetTopologySuite.Tests.NUnit.Algorithm
+{
+    internal class ElevationModelTest
+    {
+        [Test]
+        public void TestDefaultConstructor()
+        {
+            var emdl = new ElevationModel();
+            Assert.That(emdl, Is.Not.Null);
+            Assert.That(emdl.Extent, Is.Null);
+            Assert.That(emdl.SRID, Is.EqualTo(0));
+
+            foreach ((var pos, double z) in SamplePosZs(double.NaN))
+                Assert.That(emdl.GetZ(pos), Is.EqualTo(double.NaN));
+        }
+
+        [Test]
+        public void TestConstructorWithDefaultZValue()
+        {
+            var emdl = new ElevationModel(1d);
+            Assert.That(emdl, Is.Not.Null);
+            Assert.That(emdl.Extent, Is.Null);
+            Assert.That(emdl.SRID, Is.EqualTo(0));
+
+            foreach((var pos, double z) in SamplePosZs(1d))
+                Assert.That(emdl.GetZ(pos), Is.EqualTo(1d));
+        }
+
+        [TestCase(4326, -10, 10, 30, 60, 10)]
+        public void TestConstructorWithAllArguments(int srid, double minX, double maxX, double minY, double maxY, double defaultZ)
+        {
+            var emdl = new ElevationModel(defaultZ, new Envelope(minX, maxX, minY, maxY), srid);
+
+            Assert.That(emdl, Is.Not.Null);
+            Assert.That(emdl.Extent, Is.Not.Null);
+            Assert.That(emdl.SRID, Is.EqualTo(srid));
+
+            int i = 0;
+            foreach ((var pos, double z) in SamplePosZs(emdl.Extent, defaultZ))
+            {
+                Assert.That(emdl.GetZ(pos), Is.EqualTo(z), $"Assertion failed for test position {i} at {pos}.");
+                i++;
+            }
+        }
+
+        [Test]
+        public void TestCopyWithZ()
+        {
+            var emdl = new ElevationModel(10);
+
+            var pos = new Coordinate(1, 1);
+            var posZ = emdl.CopyWithZ(pos);
+            Assert.That(posZ, Is.Not.Null);
+            Assert.That(posZ, Is.TypeOf<CoordinateZ>());
+            Assert.That(posZ.Z, Is.EqualTo(10));
+
+            pos = new CoordinateZ(1, 1, 9);
+            posZ = emdl.CopyWithZ(pos);
+            Assert.That(posZ, Is.Not.Null);
+            Assert.That(posZ, Is.TypeOf<CoordinateZ>());
+            Assert.That(posZ.Z, Is.EqualTo(9));
+
+            pos = new CoordinateM(1, 1, 8);
+            posZ = emdl.CopyWithZ(pos);
+            Assert.That(posZ, Is.Not.Null);
+            Assert.That(posZ, Is.TypeOf<CoordinateZM>());
+            Assert.That(posZ.Z, Is.EqualTo(10));
+            Assert.That(posZ.M, Is.EqualTo(8));
+        }
+
+        private static IEnumerable<(Coordinate pos, double z)> SamplePosZs(double defaultZ)
+        {
+            yield return (new Coordinate(), defaultZ);
+        }
+
+
+        private static IEnumerable<(Coordinate pos, double z)> SamplePosZs(Envelope extent, double defaultZ)
+        {
+            if (extent == null || extent.Area == 0d)
+            {
+                Assert.Ignore("extent is null or empty");
+                yield break;
+            }
+
+            double testDX = 0.1 * extent.Width;
+            double testDY = 0.1 * extent.Height;
+
+            yield return (extent.Centre, defaultZ);
+
+            // left bottom
+            yield return (new Coordinate(extent.MinX, extent.MinY), defaultZ);
+            yield return (new Coordinate(extent.MinX - testDX, extent.MinY), Coordinate.NullOrdinate);
+            yield return (new Coordinate(extent.MinX + testDX, extent.MinY), defaultZ);
+            yield return (new Coordinate(extent.MinX, extent.MinY - testDY), Coordinate.NullOrdinate);
+            yield return (new Coordinate(extent.MinX, extent.MinY + testDY), defaultZ);
+            //left top
+            yield return (new Coordinate(extent.MinX, extent.MaxY), defaultZ);
+            yield return (new Coordinate(extent.MinX - testDX, extent.MaxY), Coordinate.NullOrdinate);
+            yield return (new Coordinate(extent.MinX + testDX, extent.MaxY), defaultZ);
+            yield return (new Coordinate(extent.MinX, extent.MaxY - testDY), defaultZ);
+            yield return (new Coordinate(extent.MinX, extent.MaxY + testDY), Coordinate.NullOrdinate);
+            //right top
+            yield return (new Coordinate(extent.MaxX, extent.MaxY), defaultZ);
+            yield return (new Coordinate(extent.MaxX - testDX, extent.MaxY), defaultZ);
+            yield return (new Coordinate(extent.MaxX + testDX, extent.MaxY), Coordinate.NullOrdinate);
+            yield return (new Coordinate(extent.MaxX, extent.MaxY - testDY), defaultZ);
+            yield return (new Coordinate(extent.MaxX, extent.MaxY + testDY), Coordinate.NullOrdinate);
+            //right bottom
+            yield return (new Coordinate(extent.MaxX, extent.MinY), defaultZ);
+            yield return (new Coordinate(extent.MaxX - testDX, extent.MinY), defaultZ);
+            yield return (new Coordinate(extent.MaxX + testDX, extent.MinY), Coordinate.NullOrdinate);
+            yield return (new Coordinate(extent.MaxX, extent.MinY - testDY), Coordinate.NullOrdinate);
+            yield return (new Coordinate(extent.MaxX, extent.MinY + testDY), defaultZ);
+        }
+
+    }
+}
